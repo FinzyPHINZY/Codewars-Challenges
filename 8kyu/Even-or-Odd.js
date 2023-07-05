51849601f1d3079f61d6b5bfbba55ff1a145e27f
FinzyPHINZY <finzyphinzy@gmail.com>
2023-07-05 12:59:37 +0100

    Add files via upload

---
+++ 8kyu/Even-or-Odd.js
@@ -0,0 +1,13 @@
+function evenOrOdd(number) {
+  //Takes in 'number' arguement
+  //checks if the number is divisible by 2 without a remainder
+  //returns odd or even
+
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(evenOrOdd(13));
