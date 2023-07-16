03fded55d435c54ddbf1f84b52a52a794ff6ee22
FinzyPHINZY <finzyphinzy@gmail.com>
2023-07-16 21:40:33 +0100

    Add files via upload

---
+++ 8kyu/You only need one.js
@@ -0,0 +1,14 @@
+// function check(a, x) {
+//   // your code here
+//   // if a includes x, return true
+//   // if not, return false
+//   //   if (a.includes(x)) {
+//   //     return true;
+//   //   } else {
+//   //     return false;
+//   //   }
+// }
+
+const check = (a, x) => a.includes(x);
+
+console.log(check(["bolu", "mabel", "samuel", "ernest"], "bolua"));
