3acc7df9747dd59951029a5f929912115f71e0ac
FinzyPHINZY <finzyphinzy@gmail.com>
2023-07-18 13:59:18 +0100

    Add files via upload

---
+++ 8kyu/Calculate average.js
@@ -0,0 +1,15 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+  if (array.length == 0) return 0;
+  let result = 0;
+  for (num of array) {
+    result += num;
+  }
+  return result / array.length;
+}
+console.log(findAverage([1, 2, 3, 4, 5]));
+
+// console.log(findAverage([]));
