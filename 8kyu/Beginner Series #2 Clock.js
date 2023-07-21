026e25cc09dfcc3f1ad6dfe31494bd3af721f450
FinzyPHINZY <finzyphinzy@gmail.com>
2023-07-21 18:40:05 +0100

    Update Beginner Series #2 Clock.js

--- 8kyu/Beginner Series #2 Clock.js
+++ 8kyu/Beginner Series #2 Clock.js
@@ -31,4 +31,4 @@ past(1, 0, 0);
 // assert.strictEqual(past(1, 1, 1), 3661000);
 // assert.strictEqual(past(0, 0, 0), 0);
 // assert.strictEqual(past(1, 0, 1), 3601000);
-// assert.strictEqual(past(1, 0, 0), 3600000);
+
