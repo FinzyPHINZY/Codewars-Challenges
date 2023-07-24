dd1cdcb3c8e652dd427aaaedad12e327dd31eec6
FinzyPHINZY <finzyphinzy@gmail.com>
2023-07-24 11:28:02 +0100

    Add files via upload

---
+++ 8kyu/Beginner - Reduce but Grow.js
@@ -0,0 +1,15 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x) {
+  //result =1
+  //loop through x
+  //result *= x[i]
+
+  let result = 1;
+  for (i = 0; i < x.length; i++) {
+    result = result * x[i];
+  }
+  return result;
+}
