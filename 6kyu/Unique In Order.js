5308da8a097b45b33b3edcd795bff92329ebead2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-08-02 15:31:18 +0100

    added solution to challenge

--- 6kyu/Unique In Order.js
+++ 6kyu/Unique In Order.js
@@ -7,3 +7,18 @@
 // uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 // uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 // uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder = function (iterable) {
+  if (!Array.isArray(iterable)) {
+    iterable = iterable.split("");
+  }
+
+  const uniqueList = [];
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i + 1]) {
+      uniqueList.push(iterable[i]);
+    }
+  }
+
+  return uniqueList;
+};
