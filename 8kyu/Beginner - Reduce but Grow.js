edcd8c6ee8e8269fc3c6632a24dd2d57ef896ebe
FinzyPHINZY <finzyphinzy@gmail.com>
2023-08-05 21:05:46 +0100

    Update Beginner - Reduce but Grow.js

--- 8kyu/Beginner - Reduce but Grow.js
+++ 8kyu/Beginner - Reduce but Grow.js
@@ -1,6 +1,6 @@
 // Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
 
-// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24;
 
 function grow(x) {
   //result =1
