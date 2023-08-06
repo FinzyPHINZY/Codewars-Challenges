a256a732064e92ca7a10dde805bb557d9b5f2073
FinzyPHINZY <finzyphinzy@gmail.com>
2023-08-06 22:00:37 +0100

    Update Unique In Order.js

--- 6kyu/Unique In Order.js
+++ 6kyu/Unique In Order.js
@@ -4,9 +4,9 @@
 
 // For example:
 
-// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
-// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
-// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B'];
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D'];
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3];
 
 var uniqueInOrder = function (iterable) {
   if (!Array.isArray(iterable)) {
