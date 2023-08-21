bbf5d71e9810bdf8f794616ca6ce0ccc07871aec
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-08-21 17:04:21 +0100

    added another way to solve

--- 6kyu/Replace With Alphabet Position.js
+++ 6kyu/Replace With Alphabet Position.js
@@ -24,6 +24,16 @@ function alphabetPosition(text) {
   return result.join(" "); // Join the positions with spaces and return as a string
 }
 
+// function alphabetPosition(text) {
+//   var result = "";
+//   for (var i = 0; i < text.length; i++) {
+//     var code = text.toUpperCase().charCodeAt(i);
+//     if (code > 64 && code < 91) result += code - 64 + " ";
+//   }
+
+//   return result.slice(0, result.length - 1);
+// }
+
 const input = "The sunset sets at twelve o' clock.";
 const output = alphabetPosition(input);
 console.log(output); // Output: "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
