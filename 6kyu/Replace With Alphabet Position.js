cc7acf1146f1bba0b9311f8f9ccd8ade39a101a3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-08-21 17:00:58 +0100

    created function

--- 6kyu/Replace With Alphabet Position.js
+++ 6kyu/Replace With Alphabet Position.js
@@ -8,3 +8,22 @@
 // alphabetPosition("The sunset sets at twelve o' clock.")
 
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string
+
+function alphabetPosition(text) {
+  const result = [];
+
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i].toLowerCase(); // Convert to lowercase for consistent comparison
+
+    if (char >= "a" && char <= "z") {
+      const position = char.charCodeAt(0) - "a".charCodeAt(0) + 1;
+      result.push(position);
+    }
+  }
+
+  return result.join(" "); // Join the positions with spaces and return as a string
+}
+
+const input = "The sunset sets at twelve o' clock.";
+const output = alphabetPosition(input);
+console.log(output); // Output: "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
