a97cea94b1b465a8b0c53978b5cc8d7a334c1cb8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-09-13 12:02:50 +0100

    added solution to kata

---
+++ 8kyu/Fake Binary.js
@@ -0,0 +1,34 @@
+// Description:
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests",() =>{
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(fakeBin('45385593107843568'), '01011110001100111');
+//     assert.strictEqual(fakeBin('509321967506747'), '101000111101101');
+//     assert.strictEqual(fakeBin('366058562030849490134388085'), '011011110000101010000011011');
+//   })
+// });
+
+function fakeBin(x) {
+  const xsplit = x.split("");
+
+  let result = "";
+  xsplit.forEach((xs) => {
+    if (xs < 5) {
+      result += 0;
+    } else {
+      result += 1;
+    }
+  });
+
+  return result;
+}
+
+console.log(fakeBin("45385593107843568"));
