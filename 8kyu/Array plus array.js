031e2272687dad5b31123401efd5a4989a818c1d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-09-13 12:42:19 +0100

    added solution to kata

--- 8kyu/Array plus array.js
+++ 8kyu/Array plus array.js
@@ -4,15 +4,31 @@
 
 // P.S. Each array includes only integer numbers. Output is a number too.
 
-describe("Basic tests", () => {
-  it("Testing for fixed tests", () => {
-    assert.strictEqual(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
-    assert.strictEqual(arrayPlusArray([-1, -2, -3], [-4, -5, -6]), -21);
-    assert.strictEqual(arrayPlusArray([0, 0, 0], [4, 5, 6]), 15);
-    assert.strictEqual(arrayPlusArray([100, 200, 300], [400, 500, 600]), 2100);
-  });
-});
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
+//     assert.strictEqual(arrayPlusArray([-1, -2, -3], [-4, -5, -6]), -21);
+//     assert.strictEqual(arrayPlusArray([0, 0, 0], [4, 5, 6]), 15);
+//     assert.strictEqual(arrayPlusArray([100, 200, 300], [400, 500, 600]), 2100);
+//   });
+// });
 
 function arrayPlusArray(arr1, arr2) {
-  return arr1 + arr2; //something went wrong
+  let result = 0;
+
+  arr1.forEach((num) => {
+    result += num;
+  });
+
+  arr2.forEach((num) => {
+    result += num;
+  });
+  return result;
 }
+
+console.log(arrayPlusArray([100, 200, 300], [400, 500, 600]));
+console.log(arrayPlusArray([0, 0, 0], [4, 5, 6]));
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]));
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
+
+console.log(typeof arrayPlusArray([100, 200, 300], [400, 500, 600]));
