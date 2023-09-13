24b434e7f92a3414fea8c04f531bad6987d7c888
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-09-13 11:52:57 +0100

    added solution

--- 8kyu/Count of positives or sum of negatives.js
+++ 8kyu/Count of positives or sum of negatives.js
@@ -8,3 +8,32 @@
 // Example
 
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  // your code here
+
+  if (!input || input.length === 0) {
+    return [];
+  }
+
+  let positiveCount = 0;
+  let negativeSum = 0;
+
+  input.forEach((num) => {
+    if (num > 0) {
+      positiveCount++;
+    } else if (num < 0) {
+      negativeSum += num;
+    }
+  });
+
+  return [positiveCount, negativeSum];
+}
+
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+  ])
+);
+
+console.log(countPositivesSumNegatives([]));
