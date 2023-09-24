a266d1e7f3ca225ef080bab296090fd861f96f46
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-09-24 06:54:59 +0100

    added editsmto solution

--- Remove the minimum.js
+++ Remove the minimum.js
@@ -26,6 +26,6 @@ function removeSmallest(numbers) {
   return array;
 }
 
-console.log(removeSmallest([1, 2, 3, 4, 5]));
-console.log(removeSmallest([5, 3, 2, 1, 4]));
-console.log(removeSmallest([2, 2, 1, 2, 1]));
+console.log(removeSmallest([1, 2, 3, 4, 5])); //[ 2, 3, 4, 5 ]
+console.log(removeSmallest([5, 3, 2, 1, 4])); //[ 5, 3, 2, 4 ]
+console.log(removeSmallest([2, 2, 1, 2, 1])); //[ 2, 2, 2, 1 ]
