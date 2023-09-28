9cecd70cc386adbef5741347c960fac73daf69ed
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-09-28 08:01:01 +0100

    added test cases

--- String incrementer.js
+++ String incrementer.js
@@ -16,3 +16,15 @@
 // foo099 -> foo100
 
 // Attention: If the number has leading zeros the amount of digits should be considered.
+
+// it("sample tests", () => {
+//   doTest("foobar000", "foobar001");
+//   doTest("foobar999", "foobar1000");
+//   doTest("foobar00999", "foobar01000");
+//   doTest("foo", "foo1");
+//   doTest("foobar001", "foobar002");
+//   doTest("foobar1", "foobar2");
+//   doTest("1", "2");
+//   doTest("009", "010");
+//   doTest("fo99obar99", "fo99obar100");
+// });
