075e039984660c226dadb10d70675dc9f7a3032d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-01 08:29:16 +0100

    added solution

--- Switch it Up!.js
+++ Switch it Up!.js
@@ -7,3 +7,48 @@
 // Output :: "One".
 
 // If your language supports it, try using a switch statement.
+
+function switchItUp(number) {
+  //Write your own Code!
+
+  switch (number) {
+    case (number = 1):
+      return "One";
+      break;
+    case (number = 2):
+      return "Two";
+      break;
+    case (number = 3):
+      return "Three";
+      break;
+    case (number = 4):
+      return "Four";
+      break;
+    case (number = 5):
+      return "Five";
+      break;
+    case (number = 6):
+      return "Six";
+      break;
+    case (number = 7):
+      return "Seven";
+      break;
+    case (number = 8):
+      return "Eight";
+      break;
+    case (number = 9):
+      return "Nine";
+      break;
+
+    default:
+      return "Zero";
+      break;
+  }
+}
+
+console.log(switchItUp(1));
+console.log(switchItUp(2));
+console.log(switchItUp(3));
+console.log(switchItUp(4));
+console.log(switchItUp(5));
+console.log(switchItUp());
