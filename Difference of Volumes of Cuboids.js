e74cb0b4d517ae638ede23eb493507a54327b4e3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-03 15:21:17 +0100

    added solution attempt

--- Difference of Volumes of Cuboids.js
+++ Difference of Volumes of Cuboids.js
@@ -4,17 +4,21 @@
 
 // If you can, try writing it in one line of code.
 
-describe("Tests", function () {
-  it("Basic tests", function () {
-    assert.strictEqual(findDifference([3, 2, 5], [1, 4, 4]), 14);
-    assert.strictEqual(findDifference([9, 7, 2], [5, 2, 2]), 106);
-    assert.strictEqual(findDifference([11, 2, 5], [1, 10, 8]), 30);
-    assert.strictEqual(findDifference([4, 4, 7], [3, 9, 3]), 31);
-    assert.strictEqual(findDifference([15, 20, 25], [10, 30, 25]), 0);
-  });
-});
+// describe("Tests", function () {
+//   it("Basic tests", function () {
+//     assert.strictEqual(findDifference([3, 2, 5], [1, 4, 4]), 14);
+//     assert.strictEqual(findDifference([9, 7, 2], [5, 2, 2]), 106);
+//     assert.strictEqual(findDifference([11, 2, 5], [1, 10, 8]), 30);
+//     assert.strictEqual(findDifference([4, 4, 7], [3, 9, 3]), 31);
+//     assert.strictEqual(findDifference([15, 20, 25], [10, 30, 25]), 0);
+//   });
+// });
 
 function findDifference(a, b) {
-  const aTimes = 0;
-  const bTimes = 0;
+  let aTimes = a[0] * a[1] * a[2];
+  let bTimes = b[0] * b[1] * b[2];
+
+  return aTimes - bTimes;
 }
+
+console.log(findDifference([4, 4, 7], [3, 9, 3]));
