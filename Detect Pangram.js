30decc2ccdd21704533554d1d5df4b44d001aa21
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-09 09:45:58 +0100

    added alternative kata solution

--- Detect Pangram.js
+++ Detect Pangram.js
@@ -13,6 +13,29 @@
 //     });
 //   });
 
+// function isPangram(str) {
+//   // Convert the string to lowercase
+//   str = str.toLowerCase();
+
+//   // Create an object to store the occurrence of each letter
+//   const letterCounts = {};
+
+//   // Iterate through the string and update the letterCounts object
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i];
+//     if (/[a-z]/.test(char)) {
+//       letterCounts[char] = (letterCounts[char] || 0) + 1;
+//     }
+//   }
+
+//   // Check if all 26 letters of the alphabet are present
+//   return Object.keys(letterCounts).length === 26;
+// }
+
+// Test cases
+console.log(isPangram("The quick brown fox jumps over the lazy dog")); // true
+console.log(isPangram("Hello, World!")); // false
+
 function isPangram(string) {
   string = string.toLowerCase();
   return "abcdefghijklmnopqrstuvwxyz".split("").every(function (x) {
