59f894cddf44083b5fc750e98b97aaf8b9f6d1a7
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-11 06:20:41 +0100

    added kata solution

--- Grasshopper - Grade book.js
+++ Grasshopper - Grade book.js
@@ -9,3 +9,21 @@
 // 0 <= score < 60 	'F'
 
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+function getGrade(s1, s2, s3) {
+  // Code here
+
+  //   get the average of the values
+  const averageValue = (s1 + s2 + s3) / 3;
+
+  if (averageValue >= 90 && averageValue <= 100) {
+    return "A";
+  } else if (averageValue >= 80 && averageValue < 90) {
+    return "B";
+  } else if (averageValue >= 70 && averageValue < 80) {
+    return "C";
+  } else if (averageValue >= 60 && averageValue < 70) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
