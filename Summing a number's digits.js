1b532083135b49159dc555725060fa7283489cfc
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-11 06:30:55 +0100

    added kata solution

--- Summing a number's digits.js
+++ Summing a number's digits.js
@@ -5,3 +5,18 @@
 // 10 --> 1
 // 99 --> 18
 // -32 --> 5
+
+function sumDigits(number) {
+  // turn number into a string and split
+  number = String(99).split("");
+
+  let result = 0;
+
+  number.forEach((num) => {
+    result += Number(num);
+  });
+
+  return Math.abs(result);
+}
+
+console.log(sumDigits(99));
