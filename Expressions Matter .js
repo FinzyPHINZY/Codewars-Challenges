2bce59f329f37bfe97e2151026ad9cde00c410d3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-12 07:02:29 +0100

    added updates to kata to pass all tests

--- Expressions Matter .js
+++ Expressions Matter .js
@@ -85,10 +85,11 @@ function expressionMatter(a, b, c) {
   let calc1 = a + b + c;
   let calc2 = a + b * c;
   let calc3 = (a + b) * c;
-  let calc4 = a + b * c;
-  let calcl5 = a * b * c;
+  let calc4 = a * b + c;
+  let calc5 = a * b * c;
+  let calc6 = a * (b + c);
 
-  const arr = [calc1, calc2, calc3, calc4, calcl5];
+  const arr = [calc1, calc2, calc3, calc4, calc5, calc6];
 
   highestValue = Math.max(...arr);
 
@@ -97,3 +98,18 @@ function expressionMatter(a, b, c) {
 
 console.log(expressionMatter(1, 2, 3)); //9
 console.log(expressionMatter(1, 1, 1)); //3
+
+console.log(expressionMatter(5, 1, 3)); // 20);
+console.log(expressionMatter(3, 5, 7)); // 105);
+console.log(expressionMatter(5, 6, 1)); // 35);
+console.log(expressionMatter(1, 6, 1)); // 8);
+console.log(expressionMatter(2, 6, 1)); // 14);
+console.log(expressionMatter(6, 7, 1)); // 48);
+
+console.log(expressionMatter(2, 10, 3)); //60);
+console.log(expressionMatter(1, 8, 3)); // 27);
+console.log(expressionMatter(9, 7, 2)); // 126);
+console.log(expressionMatter(1, 1, 10)); //  20);
+console.log(expressionMatter(9, 1, 1)); // 18);
+console.log(expressionMatter(10, 5, 6)); //  300);
+console.log(expressionMatter(1, 10, 1)); //  12);
