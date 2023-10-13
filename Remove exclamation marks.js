d1698edb88305a1815b7aa6457bf12dc8e6d19ec
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-13 09:43:38 +0100

    added correct solution

--- Remove exclamation marks.js
+++ Remove exclamation marks.js
@@ -1,5 +1,12 @@
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 function removeExclamationMarks(s) {
-  return "";
+  s = s.split("");
+  let result = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== "!") {
+      result.push(s[i]);
+    }
+  }
+  return result.join("");
 }
