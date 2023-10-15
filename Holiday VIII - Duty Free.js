c5c822a0c1063caefc3e768dd927a12f7d2e994e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-15 14:51:58 +0100

    added solution to kata

--- Holiday VIII - Duty Free.js
+++ Holiday VIII - Duty Free.js
@@ -10,4 +10,10 @@
 
 // All inputs will be integers. Please return an integer. Round down.
 
-function dutyFree(normPrice, discount, hol) {}
+function dutyFree(normPrice, discount, hol) {
+  return Math.floor(hol / (normPrice * (discount / 100)));
+}
+
+console.log(dutyFree(12, 50, 1000));
+console.log(dutyFree(17, 10, 500));
+console.log(dutyFree(24, 35, 3000));
