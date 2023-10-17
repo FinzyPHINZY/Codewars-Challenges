c08fe34bb6d07838496061b28b961979182e9ab8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-17 14:17:00 +0100

    added function

--- Super Duper Easy.js
+++ Super Duper Easy.js
@@ -1,3 +1,7 @@
 // Description:
 
 // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(x) {
+  //your code here
+}
