0cc26e9434bfd4ed8087d7bc0cefe7b76f15ec34
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-19 09:15:55 +0100

    added solution to kata

--- Break camelCase.js
+++ Break camelCase.js
@@ -6,4 +6,21 @@
 // ""             =>  ""
 
 // complete the function
-function solution(string) {}
+
+// Loop through the arguement and get the index of the letters in uppercase and put them in an array
+
+function solution(string) {
+  let result = "";
+
+  for (let i = 0; i < string.length; i++) {
+    const currentChar = string[i];
+    if (currentChar == currentChar.toUpperCase()) {
+      result += " ";
+    }
+    result += currentChar;
+  }
+  return result;
+}
+
+console.log(solution("camelCasing"));
+console.log(solution("camelCasingTest"));
