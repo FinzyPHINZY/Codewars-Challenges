4864683561bba27c0ad2d367a50ef0c535e4a69e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-23 21:03:42 +0100

    Update Fake Binary.js

--- Fake Binary.js
+++ Fake Binary.js
@@ -2,10 +2,6 @@
 // Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 // Note: input will never be an empty string
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
-
 // describe("Basic tests",() =>{
 //   it("Testing for fixed tests", () => {
 //     assert.strictEqual(fakeBin('45385593107843568'), '01011110001100111');
