9ed6565c85ca00423b9665dc674f3b93949bab28
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-23 21:03:04 +0100

    Update Array plus array.js

--- Array plus array.js
+++ Array plus array.js
@@ -1,5 +1,5 @@
 // Description:
-// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate your help.
 // P.S. Each array includes only integer numbers. Output is a number too.
 
 // describe("Basic tests", () => {
