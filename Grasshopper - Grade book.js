cb64b930dea772d963c031250192b20aa879a5ca
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-25 18:40:05 +0100

    Update Grasshopper - Grade book.js

--- Grasshopper - Grade book.js
+++ Grasshopper - Grade book.js
@@ -10,7 +10,6 @@
 
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 function getGrade(s1, s2, s3) {
-  // Code here
 
   //   get the average of the values
   const averageValue = (s1 + s2 + s3) / 3;
