4a02c023c66a72341c88ed6bb86e65699bab9361
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-27 18:36:41 +0100

    added solution boilerplate

--- String cleaning.js
+++ String cleaning.js
@@ -6,3 +6,7 @@
 // 'This looks5 grea8t!' -> 'This looks great!'
 
 // Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+function stringClean(s) {
+  // Function will return the cleaned string
+}
