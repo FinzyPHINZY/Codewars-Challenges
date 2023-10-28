f91d7b59745909278c9634c2f0dc156d78ede18d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-28 03:45:51 +0100

    added solution to kata

--- Sort Numbers.js
+++ Sort Numbers.js
@@ -17,4 +17,15 @@
 //   });
 // });
 
-function solution(nums) {}
+function solution(nums) {
+  if (nums === null) {
+    return [];
+  } else {
+    return nums.sort((a, b) => a - b);
+  }
+}
+
+console.log(solution([1, 2, 3, 10, 5]));
+
+console.log(solution([20, 2, 10]));
+console.log(solution(null));
