7e63e084044aee94ccfae4c1372baf0926dd0a82
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-28 03:24:54 +0100

    added test cases

--- Simple Fun #176 Reverse Letter.js
+++ Simple Fun #176 Reverse Letter.js
@@ -14,6 +14,18 @@
 
 //     [output] a string
 
+// describe("Basic Tests", function () {
+//   it("It should works for basic tests.", function () {
+//     Test.assertEquals(reverseLetter("krishan"), "nahsirk");
+
+//     Test.assertEquals(reverseLetter("ultr53o?n"), "nortlu");
+
+//     Test.assertEquals(reverseLetter("ab23c"), "cba");
+
+//     Test.assertEquals(reverseLetter("krish21an"), "nahsirk");
+//   });
+// });
+
 function reverseLetter(str) {
   //coding and coding..
 }
