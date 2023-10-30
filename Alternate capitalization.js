46a2bbbe8686efe8c924a56147e9989aed364bf7
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-30 17:02:18 +0100

    added kata sollution

--- Alternate capitalization.js
+++ Alternate capitalization.js
@@ -15,5 +15,19 @@
 // Even-odd disparity
 
 function capitalize(s) {
-  return [];
+  let evenStr = "";
+  let oddStr = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (i % 2 === 0) {
+      evenStr += s[i].toUpperCase();
+      oddStr += s[i];
+    } else {
+      evenStr += s[i];
+      oddStr += s[i].toUpperCase();
+    }
+  }
+
+  return [evenStr, oddStr];
 }
+console.log(capitalize("abracadabra"));
