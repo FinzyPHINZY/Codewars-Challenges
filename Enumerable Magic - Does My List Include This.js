f0a2d2dec0b209756e62a65ffae513829e535bc0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-31 09:54:39 +0100

    added kata solution template

--- Enumerable Magic - Does My List Include This.js
+++ Enumerable Magic - Does My List Include This.js
@@ -1,3 +1,7 @@
 // Description:
 
 // Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+function include(arr, item) {
+  // ...
+}
