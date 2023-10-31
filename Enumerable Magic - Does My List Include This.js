0e8994484c45e4c1573d08af8be19dfd93c8f9e6
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-10-31 09:55:43 +0100

    added test cases

--- Enumerable Magic - Does My List Include This.js
+++ Enumerable Magic - Does My List Include This.js
@@ -2,6 +2,15 @@
 
 // Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
 
+// TEST CASES
+// describe("Tests", () => {
+//     it("test", () => {
+//       assert.strictEqual(include([1,2,3,4], 3), true )
+//       assert.strictEqual(include([1,2,4,5], 3), false)
+//       assert.strictEqual(include([], 3),        false)
+//     });
+//   });
+
 function include(arr, item) {
   // ...
 }
