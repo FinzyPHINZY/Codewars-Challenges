e56c81b9b12238f30ff48d244e02db270e7dede6
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-02 09:24:35 +0100

    added solution first attempt

--- Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe.js
+++ Coding Meetup #1 - Higher-Order Functions Series - Count the number of JavaScript developers coming from Europe.js
@@ -21,3 +21,74 @@
 
 //     The format of the strings will always be Europe and JavaScript.
 //     All data will always be valid and uniform as in the example above.
+
+// const assert = require('chai').assert;
+// describe("Sample tests", () => {
+//   const list1 = [
+//     { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+//     { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+//     { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+//     { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+//   ];
+
+//   it("should find JavaScript developers from Europe", () => {
+//     assert.strictEqual(countDevelopers(list1), 1, "Noah is the only JavaScript developer from Europe");
+//   });
+
+//   const list2 = [
+//     { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 19, language: 'HTML' },
+//     { firstName: 'Lukas', lastName: 'R.', country: 'Austria', continent: 'Europe', age: 89, language: 'HTML' }
+//   ];
+
+//   it("should not find HTML developers from Europe", () => {
+//     assert.strictEqual(countDevelopers(list2), 0, "Lukas is not a JS devolper");
+//   });
+// });
+
+function countDevelopers(list) {
+  // your awesome code here :)
+  let count = 0;
+  for (let item = 0; item < list.length; item++) {
+    if (list[item].continent === "Europe") {
+      count++;
+      return `${count}, ${list[item].firstName} is the only ${list[item].language} developer`;
+    }
+  }
+}
+
+console.log(
+  countDevelopers([
+    {
+      firstName: "Noah",
+      lastName: "M.",
+      country: "Switzerland",
+      continent: "Europe",
+      age: 19,
+      language: "JavaScript",
+    },
+    {
+      firstName: "Maia",
+      lastName: "S.",
+      country: "Tahiti",
+      continent: "Oceania",
+      age: 28,
+      language: "JavaScript",
+    },
+    {
+      firstName: "Shufen",
+      lastName: "L.",
+      country: "Taiwan",
+      continent: "Asia",
+      age: 35,
+      language: "HTML",
+    },
+    {
+      firstName: "Sumayah",
+      lastName: "M.",
+      country: "Tajikistan",
+      continent: "Asia",
+      age: 30,
+      language: "CSS",
+    },
+  ])
+);
