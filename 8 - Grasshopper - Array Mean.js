aa6f7aaeffb4a4b1dab650b6bb4a69404299f43a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-07 13:28:11 +0100

    added refactored code

--- 8 - Grasshopper - Array Mean.js
+++ 8 - Grasshopper - Array Mean.js
@@ -21,3 +21,7 @@ var findAverage = function (nums) {
   nums.forEach((num) => (result += num));
   return result / nums.length;
 };
+
+const findAverage = (nums) => nums.reduce((a, b) => a + b) / nums.length;
+
+console.log(findAverage([1, 3, 5, 7]));
