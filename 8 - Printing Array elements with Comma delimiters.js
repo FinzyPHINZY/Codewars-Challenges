5a5fcdf36a61c2538f6a9ec251e0242362830630
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-07 13:43:46 +0100

    added new kata description

---
+++ 8 - Printing Array elements with Comma delimiters.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// Note: if this see
