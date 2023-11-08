385653f7ea85816f66dfc0e2817b8ba6d1ab6951
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-08 10:22:00 +0100

    added first solution attempt

--- 8 - Sum of differences in array.js
+++ 8 - Sum of differences in array.js
@@ -8,4 +8,15 @@
 
 // If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
 
-function sumOfDifferences(arr) {}
+function sumOfDifferences(arr) {
+  //Sort the array in descending order of the elements
+  arr = arr.sort((a, b) => b - a);
+
+  //Bind calculations in variables
+  let output1 = arr[0] - arr[1];
+  let output2 = arr[1] - arr[2];
+
+  return output1 + output2;
+}
+
+console.log(sumOfDifferences([2, 1, 10]));
