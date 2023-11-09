27e9f76b9896d0bdec0ceeb9002d2aa0e36c7646
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-09 10:46:37 +0100

    added kata description and kata soolution boilerplate

---
+++ 8 - Find the position.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+function position(letter) {
+  //Write your own Code!
+}
