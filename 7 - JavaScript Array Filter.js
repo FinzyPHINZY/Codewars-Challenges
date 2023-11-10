2e0c25a16c685ef88749e168b6a8fd83a127c198
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-10 09:41:02 +0100

    added solution boilerplate and test cases

--- 7 - JavaScript Array Filter.js
+++ 7 - JavaScript Array Filter.js
@@ -3,3 +3,17 @@
 // The solution would work like the following:
 
 // getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+// describe("Fixed tests", () => {
+//   it("should return an empty array if there are no even numbers", function () {
+//     assert.deepEqual(getEvenNumbers([1, 2, 3, 6, 8, 10]), [2, 6, 8, 10]);
+//     assert.deepEqual(getEvenNumbers([1, 2]), [2]);
+//     assert.deepEqual(getEvenNumbers([12, 14, 15]), [12, 14]);
+//     assert.deepEqual(getEvenNumbers([13, 15]), []);
+//     assert.deepEqual(getEvenNumbers([1, 3, 9]), []);
+//   });
+// });
+
+function getEvenNumbers(numbersArray) {
+  // filter out the odd numbers
+}
