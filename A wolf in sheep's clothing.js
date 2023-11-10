3b35b94ebe8738c0d67a5eb63448d610af76d0c7
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-10 10:52:21 +0100

    added solution to kata

--- A wolf in sheep's clothing.js
+++ A wolf in sheep's clothing.js
@@ -16,4 +16,13 @@
 // Input: ["sheep", "sheep", "wolf"]
 // Output: "Pls go away and stop eating my sheep"
 
-function warnTheSheep(queue) {}
+function warnTheSheep(queue) {
+  const wolfIndex = queue.findIndex((animal) => animal === "wolf");
+
+  if (wolfIndex === queue.length - 1) {
+    return "Pls go away and stop eating my sheep";
+  } else {
+    const sheepNumber = queue.length - wolfIndex - 1;
+    return `Oi! Sheep number ${sheepNumber}! You are about to be eaten by a wolf!`;
+  }
+}
