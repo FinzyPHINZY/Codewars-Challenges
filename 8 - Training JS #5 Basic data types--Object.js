f8946c0c341df53de8f2fd95dd34f7b6cbc66c67
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-10 10:40:00 +0100

    added solution to kata

--- 8 - Training JS #5 Basic data types--Object.js
+++ 8 - Training JS #5 Basic data types--Object.js
@@ -18,3 +18,9 @@
 // When you have finished the work, click "Run Tests" to see if your code is working properly.
 
 // In the end, click "Submit" to submit your code pass this kata.
+
+function animal(obj) {
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
+
+console.log(animal({ name: "dog", legs: 4, color: "white" }));
