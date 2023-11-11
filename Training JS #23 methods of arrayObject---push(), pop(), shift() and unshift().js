119416ccdc0f34f16b735adb2f9af3650269467c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-11 11:42:06 +0100

    added solution boiierplate

--- Training JS #23 methods of arrayObject---push(), pop(), shift() and unshift().js
+++ Training JS #23 methods of arrayObject---push(), pop(), shift() and unshift().js
@@ -4,3 +4,7 @@
 // You can think of arr as a moat, the elements of arr like water constantly flow in. The direction of flow is controlled by the parameter d. The value of d can be "left" or "right". "left" means the "river" moves to the left. All elements in the 1D array are to the left moving n position, if beyond the bounds of the array and the element is moved to the tail on the left side of the array; if it exceeds the left boundary element would be moved to the tail of 3rd array(like a circle). Right is also similar to the operation, but it is moving to the right.
 
 // Finally, return arr.
+
+function infiniteLoop(arr, d, n) {
+  //coding here...
+}
