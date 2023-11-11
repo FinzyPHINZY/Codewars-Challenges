2eb392347ee23b02247f078c019886c1b950ca33
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-11 10:05:42 +0100

    added kata description

---
+++ Training JS #23 methods of arrayObject---push(), pop(), shift() and unshift().js
@@ -0,0 +1,6 @@
+// Task
+// Coding in function infiniteLoop. function accept 3 parameters. The 1st parameter is arr, it's a 2D array, it contains three 1D array. The 2nd parameter is d ，it's a string. The 3rd parameter is n, it's a number.
+
+// You can think of arr as a moat, the elements of arr like water constantly flow in. The direction of flow is controlled by the parameter d. The value of d can be "left" or "right". "left" means the "river" moves to the left. All elements in the 1D array are to the left moving n position, if beyond the bounds of the array and the element is moved to the tail on the left side of the array; if it exceeds the left boundary element would be moved to the tail of 3rd array(like a circle). Right is also similar to the operation, but it is moving to the right.
+
+// Finally, return arr.
