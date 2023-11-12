95c6b39cd66f1c3fe3bc71a22b995542ea26bef0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-12 14:48:11 +0100

    added alternative solution

--- 8 - How good are you really.js
+++ 8 - How good are you really.js
@@ -44,4 +44,10 @@ function betterThanAverage(classPoints, yourPoints) {
   return classAverage < yourPoints ? true : false;
 }
 
+function betterThanAverage(classPoints, yourPoints) {
+  return (
+    yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length
+  );
+}
+
 console.log(betterThanAverage([2, 3], 5));
