1413a0017c1698a457bb42ed148434e6ed8b8a1a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-13 10:22:00 +0100

    added solutin boilerplate

--- 7 - Sorted-yes-no-how.js
+++ 7 - Sorted-yes-no-how.js
@@ -5,3 +5,5 @@
 // "yes, descending" - if the numbers in the array are sorted in a descending order
 // "no" - otherwise
 // You can assume the array will always be valid, and there will always be one correct answer.
+
+function isSortedAndHow(array) {}
