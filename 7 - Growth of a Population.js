87a0d2a8b3185b9cc2a641193c015d6a3d9fdb0c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-17 08:18:21 +0100

    added solution starter code

--- 7 - Growth of a Population.js
+++ 7 - Growth of a Population.js
@@ -24,3 +24,7 @@
 // nb_year(1500000, 2.5, 10000, 2000000) -> 10
 // Note:
 // Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you have to convert it to 0.02.
+
+function nbYear(p0, percent, aug, p) {
+  // your code
+}
