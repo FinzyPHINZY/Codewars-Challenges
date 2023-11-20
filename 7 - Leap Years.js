3f37a23d834f811255ecb4dbd72585dd7b5e61fa
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-20 09:01:39 +0100

    added solution starter code

--- 7 - Leap Years.js
+++ 7 - Leap Years.js
@@ -5,3 +5,7 @@
 // but years divisible by 100 are not leap years,
 // but years divisible by 400 are leap years.
 // Tested years are in range 1600 ≤ year ≤ 4000.
+
+function isLeapYear(year) {
+  // TODO
+}
