0c981355ef4c1121a6d6aa80be727eedc2bbd675
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-22 09:02:33 +0100

    added test cases

--- 6 - Title Case.js
+++ 6 - Title Case.js
@@ -15,4 +15,19 @@
 // titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
 // titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
 
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertEquals(titleCase(""), "");
+//     Test.assertEquals(
+//       titleCase("a clash of KINGS", "a an the of"),
+//       "A Clash of Kings"
+//     );
+//     Test.assertEquals(
+//       titleCase("THE WIND IN THE WILLOWS", "The In"),
+//       "The Wind in the Willows"
+//     );
+//     Test.assertEquals(titleCase("the quick brown fox"), "The Quick Brown Fox");
+//   });
+// });
+
 function titleCase(title, minorWords) {}
