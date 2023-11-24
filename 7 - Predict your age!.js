f781b3e5e732ba9654aa48de5dbdd9bf4aea6a8c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-24 12:17:54 +0100

    added started code for solution

--- 7 - Predict your age!.js
+++ 7 - Predict your age!.js
@@ -15,3 +15,7 @@
 // Note: the result should be rounded down to the nearest integer.
 
 // Some random tests might fail due to a bug in the JavaScript implementation. Simply resubmit if that happens to you.
+
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+  // your code
+}
