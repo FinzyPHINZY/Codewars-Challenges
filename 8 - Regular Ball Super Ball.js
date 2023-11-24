40c9dfa71ab7a1b9f11e60996b46d7b4a53a2149
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-24 12:55:37 +0100

    added solutiion to kata

--- 8 - Regular Ball Super Ball.js
+++ 8 - Regular Ball Super Ball.js
@@ -3,7 +3,7 @@
 // DESCRIPTION:
 // Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
 
-// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular.
 
 // ball1 = new Ball();
 // ball2 = new Ball("super");
@@ -11,6 +11,14 @@
 // ball1.ballType     //=> "regular"
 // ball2.ballType     //=> "super"
 
+// Test Case:
+// Test.assertEquals(new Ball().ballType, "regular");
+// Test.assertEquals(new Ball("super").ballType, "super");
+
 var Ball = function (ballType) {
   // your code goes here
+  this.ballType = ballType || "regular";
 };
+
+console.log(Ball("regular"));
+console.log(Ball("super"));
