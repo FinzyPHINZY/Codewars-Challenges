7ef2e7ebf94e4cebe737e53b982e17cee319c354
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-25 21:55:41 +0100

    added test cases

--- 8 - Surface Area and Volume of a Box.js
+++ 8 - Surface Area and Volume of a Box.js
@@ -3,6 +3,16 @@
 // DESCRIPTION:
 // Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 
+//  Test Cases
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.deepEqual(getSize(4, 2, 6), [88, 48]);
+//     assert.deepEqual(getSize(10, 10, 10), [600, 1000]);
+//     assert.deepEqual(getSize(4, 2, 6)[0], 88);
+//     assert.deepEqual(getSize(4, 2, 6)[1], 48);
+//   });
+// });
+
 function getSize(width, height, depth) {
   return Array;
 }
