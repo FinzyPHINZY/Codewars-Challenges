dbafd0bce5f6067587d438a34292f6de45f05179
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-26 07:15:50 +0100

    added test cases

--- 8 - Sort and Star.js
+++ 8 - Sort and Star.js
@@ -7,4 +7,13 @@
 
 // You should not remove or add elements from/to the array.
 
+// Test Cases:
+
+// describe("Basic tests",() =>{
+//     it("Testing for fixed tests", () => {
+//       assert.strictEqual(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' );
+//       assert.strictEqual(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e');
+//     });
+//   });
+
 function twoSort(s) {}
