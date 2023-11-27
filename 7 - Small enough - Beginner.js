102dcdc184c727405d050788ef04c9fe2f4c8753
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-27 09:16:44 +0100

    added tst cases

--- 7 - Small enough - Beginner.js
+++ 7 - Small enough - Beginner.js
@@ -5,4 +5,19 @@
 
 // You can assume all values in the array are numbers.
 
+// Test Cases:
+// describe("Basic tests", () => {
+//     it("Testing for fixed tests", () => {
+//       assert.strictEqual(smallEnough([66, 101], 200), true);
+//       assert.strictEqual(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100), false);
+//       assert.strictEqual(smallEnough([101, 45, 75, 105, 99, 107], 107), true);
+//       assert.strictEqual(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120), true);
+//     })
+//   })
+
 function smallEnough(a, limit) {}
+
+console.log(smallEnough([66, 101], 200));
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100));
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107));
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120));
