de41c09838c028d4e656667bc8ea3e501cea0ad3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-27 09:01:17 +0100

    added kata solution

--- 6 - Simple Encryption #1 - Alternating Split.js
+++ 6 - Simple Encryption #1 - Alternating Split.js
@@ -22,3 +22,71 @@
 // Simple Encryption #3 - Turn The Bits Around
 // Simple Encryption #4 - Qwerty
 // Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+function encrypt(text, n) {
+  if (text === "" || n <= 0) {
+    return text;
+  }
+
+  for (let i = 0; i < n; i++) {
+    let oddChars = "";
+    let evenChars = "";
+
+    for (let j = 0; j < text.length; j++) {
+      if (j % 2 === 1) {
+        oddChars += text[j];
+      } else {
+        evenChars += text[j];
+      }
+    }
+
+    text = oddChars + evenChars;
+  }
+
+  return text;
+}
+
+function decrypt(encryptedText, n) {
+  if (encryptedText === "" || n <= 0) {
+    return encryptedText;
+  }
+
+  const mid = Math.floor(encryptedText.length / 2);
+
+  for (let i = 0; i < n; i++) {
+    const oddChars = encryptedText.slice(0, mid);
+    const evenChars = encryptedText.slice(mid);
+
+    let decryptedText = "";
+
+    for (let j = 0; j < mid; j++) {
+      decryptedText += evenChars[j] + oddChars[j];
+    }
+
+    if (encryptedText.length % 2 !== 0) {
+      decryptedText += evenChars[mid];
+    }
+
+    encryptedText = decryptedText;
+  }
+
+  return encryptedText;
+}
+
+// Examples
+console.log(encrypt("012345", 1)); // "135024"
+console.log(encrypt("012345", 2)); // "304152"
+console.log(encrypt("012345", 3)); // "012345"
+
+console.log(encrypt("01234", 1)); // "13024"
+console.log(encrypt("01234", 2)); // "32104"
+console.log(encrypt("01234", 3)); // "20314"
+
+// Decryption examples
+console.log(decrypt("135024", 1)); // "012345"
+console.log(decrypt("304152", 2)); // "012345"
+console.log(decrypt("012345", 3)); // "012345"
+
+console.log(decrypt("13024", 1)); // "01234"
+console.log(decrypt("32104", 2)); // "01234"
+console.log(decrypt("20314", 3)); // "01234"
