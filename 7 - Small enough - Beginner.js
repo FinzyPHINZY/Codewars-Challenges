f549d95f25c5496141cda7b6518a270e7b44e479
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-27 09:30:31 +0100

    added kata solution

--- 7 - Small enough - Beginner.js
+++ 7 - Small enough - Beginner.js
@@ -15,7 +15,7 @@
 //     })
 //   })
 
-function smallEnough(a, limit) {}
+const smallEnough = (a, limit) => (Math.max(...a) > limit ? false : true);
 
 console.log(smallEnough([66, 101], 200));
 console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100));
