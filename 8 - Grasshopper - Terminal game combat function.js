3aa0b8af63feb86d30334c15c844b0c391bfb60b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-28 10:27:07 +0100

    added kata description

--- 8 - Grasshopper - Terminal game combat function.js
+++ 8 - Grasshopper - Terminal game combat function.js
@@ -1,1 +1,4 @@
 "use strict";
+
+// DESCRIPTION:
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0
