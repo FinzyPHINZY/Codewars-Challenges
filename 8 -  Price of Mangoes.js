0c0f27731cf91d23c5b3de83aa0f67cbcf15b1b1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-28 09:35:59 +0100

    added solution to kata'

--- 8 -  Price of Mangoes.js
+++ 8 -  Price of Mangoes.js
@@ -16,3 +16,10 @@
 // mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
 // mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
 // mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for free
+
+function mango(quantity, price) {}
+
+console.log(mango(2, 3));
+console.log(mango(3, 3));
+console.log(mango(5, 3));
+console.log(mango(2, 3));
