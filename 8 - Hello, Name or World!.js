6f9f96f4de20f0d712b4aaafd1ac17bb6f78df97
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-29 12:15:56 +0100

    added kata solution

--- 8 - Hello, Name or World!.js
+++ 8 - Hello, Name or World!.js
@@ -11,6 +11,26 @@
 // * With `name` not given
 //   or `name` = ""        => return "Hello, World!"
 
+// Test Cases:
+// describe("Sample Tests", () => {
+//     it("should work for sample inputs", () => {
+//       let actualJohn = hello('johN');
+//       assert.isDefined(actualJohn, "You have to return a string instead of logging it to the console");
+//       assert.strictEqual(actualJohn, 'Hello, John!', "returns 'Hello, John!' when given 'johN'");
+//       assert.strictEqual(hello('alice'), 'Hello, Alice!', "returns 'Hello, Alice!' when given 'alice'");
+//       assert.strictEqual(hello(), 'Hello, World!', "returns 'Hello, World!' when name is not given");
+//       assert.strictEqual(hello(''), 'Hello, World!', "returns 'Hello, World!' when name is an empty string");
+//     });
+//   });
+
 function hello(name) {
-  return "";
+  if (name) {
+    name = name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
+    return `Hello, ${name}!`;
+  } else {
+    return "Hello, World!";
+  }
 }
+
+console.log(hello("joHn"));
+console.log(hello());
