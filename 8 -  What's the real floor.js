54a16bb0bf039fe93840728b90a9b8b7b365841a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-29 11:59:26 +0100

    updated kata

--- 8 -  What's the real floor.js
+++ 8 -  What's the real floor.js
@@ -28,9 +28,18 @@
 //   });
 // });
 
-function getRealFloor(n) {}
+function getRealFloor(n) {
+  if (n > 0 && n < 13) {
+    return n - 1;
+  } else if (n > 13) {
+    return n - 2;
+  } else {
+    return n;
+  }
+}
 
 console.log(getRealFloor(1));
 console.log(getRealFloor(5));
 console.log(getRealFloor(15));
 console.log(getRealFloor(-2));
+console.log(getRealFloor(0));
