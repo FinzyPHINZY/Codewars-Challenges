66799dbc579068716b33b301ea0db020797448a1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-30 09:14:20 +0100

    added test cases

--- 8 - Formatting decimal places #0.js
+++ 8 - Formatting decimal places #0.js
@@ -5,3 +5,17 @@
 // Example:
 // 5.5589 is rounded 5.56
 // 3.3424 is rounded 3.34
+
+// Test Cases:
+
+// describe("twoDecimalPlaces", function() {
+//     it("works for some examples", function() {
+//       Test.assertEquals(twoDecimalPlaces(4.659725356), 4.66, "didn't work for 4.659725356");
+//       Test.assertEquals(twoDecimalPlaces(173735326.3783732637948948), 173735326.38, "didn't work for 173735326.3783732637948948");
+//       Test.assertEquals(twoDecimalPlaces(4.653725356), 4.65, "didn't work for 4.653725356");
+//     });
+//   });
+
+console.log(twoDecimalPlaces(4.659725356));
+console.log(twoDecimalPlaces(173735326.3783732637948948));
+console.log(twoDecimalPlaces(4.653725356));
