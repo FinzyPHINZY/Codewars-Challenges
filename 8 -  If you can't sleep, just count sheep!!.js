77023f9aef6c2149f1888c51bef6450f99372622
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-30 15:11:59 +0100

    added kata test cases

--- 8 -  If you can't sleep, just count sheep!!.js
+++ 8 -  If you can't sleep, just count sheep!!.js
@@ -5,6 +5,21 @@
 // Task:
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
+// Test Cases:
+// describe("Fixed tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(countSheep(0), "");
+//     assert.strictEqual(countSheep(1), "1 sheep...");
+//     assert.strictEqual(countSheep(2), "1 sheep...2 sheep...");
+//     assert.strictEqual(countSheep(3), "1 sheep...2 sheep...3 sheep...");
+//   });
+// });
+
 var countSheep = function (num) {
   //your code here
 };
+
+console.log(countSheep(0));
+console.log(countSheep(1));
+console.log(countSheep(2));
+console.log(countSheep(3));
