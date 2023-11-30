509932ca27925ce8ccb9f1b09a45d922ecd591ed
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-11-30 09:15:01 +0100

    added starter function for solution

--- 8 - Formatting decimal places #0.js
+++ 8 - Formatting decimal places #0.js
@@ -16,6 +16,10 @@
 //     });
 //   });
 
+function twoDecimalPlaces(n) {
+  // Your code here
+}
+
 console.log(twoDecimalPlaces(4.659725356));
 console.log(twoDecimalPlaces(173735326.3783732637948948));
 console.log(twoDecimalPlaces(4.653725356));
