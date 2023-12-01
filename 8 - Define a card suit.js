b3f3f008d7329644c5020b9b3bfbbbc822a7201e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-01 09:15:53 +0100

    added test case

--- 8 - Define a card suit.js
+++ 8 - Define a card suit.js
@@ -1,1 +1,7 @@
 "use strict";
+
+// Test Cases:
+
+// it('Testing №2 for "Q♠"', () => assert.strictEqual(defineSuit('Q♠'), 'spades'));
+// it('Testing №3 for "9♦"', () => assert.strictEqual(defineSuit('9♦'), 'diamonds'));
+// it('Testing №4 for "J♥"', () => assert.strictEqual(defineSuit('J♥'), 'hearts'));
