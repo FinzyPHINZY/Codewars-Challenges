185c03230505a8723db7a48b40b1c884ea91c6b3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-01 09:16:28 +0100

    added kata description

--- 8 - Define a card suit.js
+++ 8 - Define a card suit.js
@@ -1,7 +1,15 @@
 "use strict";
 
-// Test Cases:
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
 
+// Our deck (is preloaded):
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+// Test Cases:
 // it('Testing №2 for "Q♠"', () => assert.strictEqual(defineSuit('Q♠'), 'spades'));
 // it('Testing №3 for "9♦"', () => assert.strictEqual(defineSuit('9♦'), 'diamonds'));
 // it('Testing №4 for "J♥"', () => assert.strictEqual(defineSuit('J♥'), 'hearts'));
