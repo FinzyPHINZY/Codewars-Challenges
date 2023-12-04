3dd30fc63dece13d3ad0f31cba4937cef53abe8b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-04 09:41:01 +0100

    added solution starter code

--- 7 - Largest pair sum in array.js
+++ 7 - Largest pair sum in array.js
@@ -8,3 +8,7 @@
 // [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
 // [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
 // Input sequence contains minimum two elements and every element is an integer.
+
+function largestPairSum(numbers) {
+  //TODO: Write your Code here
+}
