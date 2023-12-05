283f5c112a549c7550570fb4636a08d346078ff9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-05 10:01:42 +0100

    added test cases

--- 8 - Lario and Muigi Pipe Problem.js
+++ 8 - Lario and Muigi Pipe Problem.js
@@ -32,3 +32,9 @@
 // });
 
 function pipeFix(numbers) {}
+
+console.log(pipeFix([1, 2, 3, 5, 6, 8, 9]));
+console.log(pipeFix([1, 2, 3, 12]));
+console.log(pipeFix([6, 9]));
+console.log(pipeFix([-1, 4]));
+console.log(pipeFix([1, 2, 3]));
