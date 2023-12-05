d3d462395eea1fd82126fcf67e499d0320470fc4
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-05 09:50:11 +0100

    added test cases and solution to kata

--- 8 -  The 'if' function.js
+++ 8 -  The 'if' function.js
@@ -8,6 +8,37 @@
 // _if(true, function(){console.log("True")}, function(){console.log("false")})
 // // Logs 'True' to the console.
 
+// Test Cases:
+
+// describe("Tests", () => {
+//   const unexpectedTrue = () =>
+//     assert.fail("'true' function unexpectedly called");
+//   const unexpectedFalse = () =>
+//     assert.fail("'false' function unexpectedly called");
+
+//   it("test true function", () => {
+//     let testvar = 0;
+//     _if(true, () => (testvar = 42), unexpectedFalse);
+//     assert.strictEqual(testvar, 42, "'true' function has not been called");
+//   });
+//   it("test false function", () => {
+//     let testvar = 0;
+//     _if(false, unexpectedTrue, () => (testvar = 42));
+//     assert.strictEqual(testvar, 42, "'false' function has not been called");
+//   });
+// });
+
+const trueFunc = function () {
+  return true;
+};
+
+const falseFunc = function () {
+  return false;
+};
+
 function _if(bool, func1, func2) {
   // ...
+  return bool ? func1() : func2();
 }
+
+console.log(_if(true, trueFunc, falseFunc));
