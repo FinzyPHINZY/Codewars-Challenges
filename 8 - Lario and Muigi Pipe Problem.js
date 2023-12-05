0da10e497457a7d882c617d9e97e0924b80eb933
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-05 09:59:11 +0100

    added starter code for solution

--- 8 - Lario and Muigi Pipe Problem.js
+++ 8 - Lario and Muigi Pipe Problem.js
@@ -14,4 +14,21 @@
 // Example
 // Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
 
+// Test Cases:
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.deepEqual(
+//       pipeFix([1, 2, 3, 5, 6, 8, 9]),
+//       [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//     );
+//     assert.deepEqual(
+//       pipeFix([1, 2, 3, 12]),
+//       [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+//     );
+//     assert.deepEqual(pipeFix([6, 9]), [6, 7, 8, 9]);
+//     assert.deepEqual(pipeFix([-1, 4]), [-1, 0, 1, 2, 3, 4]);
+//     assert.deepEqual(pipeFix([1, 2, 3]), [1, 2, 3]);
+//   });
+// });
+
 function pipeFix(numbers) {}
