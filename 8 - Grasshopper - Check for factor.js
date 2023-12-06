cd02e9c5678b75c6db5d2d4f679556e81c25b710
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-06 09:47:18 +0100

    added kata test case

--- 8 - Grasshopper - Check for factor.js
+++ 8 - Grasshopper - Check for factor.js
@@ -15,6 +15,32 @@
 
 // Note: base is a non-negative number, factor is a positive number.
 
+// Test Cases:
+
+// describe("check for factor", function () {
+//   it("should return true", function () {
+//     Test.assertEquals(checkForFactor(10, 2), true);
+//     Test.assertEquals(checkForFactor(63, 7), true);
+//     Test.assertEquals(checkForFactor(2450, 5), true);
+//     Test.assertEquals(checkForFactor(24612, 3), true);
+//   });
+//   it("should return false", function () {
+//     Test.assertEquals(checkForFactor(9, 2), false);
+//     Test.assertEquals(checkForFactor(653, 7), false);
+//     Test.assertEquals(checkForFactor(2453, 5), false);
+//     Test.assertEquals(checkForFactor(24617, 3), false);
+//   });
+// });
+
 function checkForFactor(base, factor) {
   // code here
 }
+
+console.log(checkForFactor(10, 2));
+console.log(checkForFactor(63, 7));
+console.log(checkForFactor(2450, 5));
+console.log(checkForFactor(24612, 3));
+console.log(checkForFactor(9, 2));
+console.log(checkForFactor(653, 7));
+console.log(checkForFactor(2453, 5));
+console.log(checkForFactor(24617, 3));
