4ee149bc040fae258b03aab6489ca35d39a4d6e2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-06 10:52:42 +0100

    added kata solution

--- 8 - Simple validation of a username with regex.js
+++ 8 - Simple validation of a username with regex.js
@@ -14,22 +14,16 @@
 //     assert.strictEqual(validateUsr("asddsa"), true);
 //     assert.strictEqual(validateUsr("a"), false);
 //     assert.strictEqual(validateUsr("Hass"), false);
-//     assert.strictEqual(
-//       validateUsr("Hasd_12assssssasasasasasaasasasasas"),
-//       false
-//     );
 //     assert.strictEqual(validateUsr(""), false);
 //     assert.strictEqual(validateUsr("____"), true);
 //     assert.strictEqual(validateUsr("012"), false);
 //     assert.strictEqual(validateUsr("p1pp1"), true);
 //     assert.strictEqual(validateUsr("asd43 34"), false);
-//     assert.strictEqual(validateUsr("asd43_34"), true);
 //   });
 // });
 
 function validateUsr(username) {
-  res = /^[a-z]+$/.test(username);
-  return res;
+  return /^[a-z0-9_]{4,16}$/i.test(username);
 }
 
 console.log(validateUsr("asddsa"));
