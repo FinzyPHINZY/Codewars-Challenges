075ea3e28ada3767cc1d4c597556cab41c0e3769
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-06 10:38:54 +0100

    added test cases

--- 8 - Simple validation of a username with regex.js
+++ 8 - Simple validation of a username with regex.js
@@ -8,7 +8,35 @@
 // underscore
 // Length should be between 4 and 16 characters (both included).
 
+//Test Cases:
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(validateUsr("asddsa"), true);
+//     assert.strictEqual(validateUsr("a"), false);
+//     assert.strictEqual(validateUsr("Hass"), false);
+//     assert.strictEqual(
+//       validateUsr("Hasd_12assssssasasasasasaasasasasas"),
+//       false
+//     );
+//     assert.strictEqual(validateUsr(""), false);
+//     assert.strictEqual(validateUsr("____"), true);
+//     assert.strictEqual(validateUsr("012"), false);
+//     assert.strictEqual(validateUsr("p1pp1"), true);
+//     assert.strictEqual(validateUsr("asd43 34"), false);
+//     assert.strictEqual(validateUsr("asd43_34"), true);
+//   });
+// });
+
 function validateUsr(username) {
   res = /^[a-z]+$/.test(username);
   return res;
 }
+
+console.log(validateUsr("asddsa"));
+console.log(validateUsr("a"));
+console.log(validateUsr("Hass"));
+console.log(validateUsr(""));
+console.log(validateUsr("____"));
+console.log(validateUsr("012"));
+console.log(validateUsr("p1pp1"));
+console.log(validateUsr("asd43 34"));
