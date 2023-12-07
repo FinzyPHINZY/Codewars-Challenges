bb278767bc5226ecda9deca21dcbb0bbc08d4a8b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-07 08:33:28 +0100

    added test cases

--- 7 -  Row Weights.js
+++ 7 -  Row Weights.js
@@ -27,6 +27,34 @@
 // Playing With Lists/Arrays Series
 // For More Enjoyable Katas
 
+// Test Cases:
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.deepEqual(rowWeights([80]), [80, 0]);
+//     assert.deepEqual(rowWeights([100, 50]), [100, 50]);
+//     assert.deepEqual(rowWeights([50, 60, 70, 80]), [120, 140]);
+//     assert.deepEqual(rowWeights([13, 27, 49]), [62, 27]);
+//     assert.deepEqual(rowWeights([70, 58, 75, 34, 91]), [236, 92]);
+//     assert.deepEqual(rowWeights([29, 83, 67, 53, 19, 28, 96]), [211, 164]);
+//     assert.deepEqual(rowWeights([0]), [0, 0]);
+//     assert.deepEqual(rowWeights([100, 51, 50, 100]), [150, 151]);
+//     assert.deepEqual(rowWeights([39, 84, 74, 18, 59, 72, 35, 61]), [207, 235]);
+//     assert.deepEqual(rowWeights([0, 1, 0]), [0, 1]);
+//   });
+// });
+
 function rowWeights(array) {
   //your code here
 }
+
+console.log(rowWeights([80]));
+console.log(rowWeights([100, 50]));
+console.log(rowWeights([50, 60, 70, 80]));
+console.log(rowWeights([13, 27, 49]));
+console.log(rowWeights([70, 58, 75, 34, 91]));
+console.log(rowWeights([29, 83, 67, 53, 19, 28, 96]));
+console.log(rowWeights([0]));
+console.log(rowWeights([100, 51, 50, 100]));
+console.log(rowWeights([39, 84, 74, 18, 59, 72, 35, 61]));
+console.log(rowWeights([0, 1, 0]));
