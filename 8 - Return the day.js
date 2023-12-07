706ca7ca0fe32b0d083c7872f650221a7f5124a6
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-07 10:26:45 +0100

    added test cases

--- 8 - Return the day.js
+++ 8 - Return the day.js
@@ -13,6 +13,29 @@
 // 7 returns "Saturday"
 // Otherwise returns "Wrong, please enter a number between 1 and 7"
 
+// Test cases:
+// describe("whatday", function () {
+//   it("should work for the examples", function () {
+//     Test.assertEquals(whatday(1), "Sunday");
+//     Test.assertEquals(whatday(2), "Monday");
+//     Test.assertEquals(whatday(3), "Tuesday");
+//     Test.assertEquals(
+//       whatday(8),
+//       "Wrong, please enter a number between 1 and 7"
+//     );
+//     Test.assertEquals(
+//       whatday(20),
+//       "Wrong, please enter a number between 1 and 7"
+//     );
+//   });
+// });
+
 function whatday(num) {
   // put your code here
 }
+
+console.log(whatday(1));
+console.log(whatday(2));
+console.log(whatday(3));
+console.log(whatday(8));
+console.log(whatday(20));
