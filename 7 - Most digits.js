66ec4e1ce03c195ce53a295823b5ee386c43f628
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-07 09:34:13 +0100

    added test cases

--- 7 - Most digits.js
+++ 7 - Most digits.js
@@ -5,6 +5,19 @@
 
 // If two numbers in the argument array have the same number of digits, return the first one in the array.
 
+// Test Cases:
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertEquals(findLongest([1, 10, 100]), 100);
+//     Test.assertEquals(findLongest([9000, 8, 800]), 9000);
+//     Test.assertEquals(findLongest([8, 900, 500]), 900);
+//   });
+// });
+
 function findLongest(array) {
   // code here
 }
+
+console.log(findLongest([1, 10, 100]));
+console.log(findLongest([9000, 8, 800]));
+console.log(findLongest([8, 900, 500]));
