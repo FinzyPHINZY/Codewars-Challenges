a861cb131bd1c629d44cbffb6090122c5de08432
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-09 18:12:49 +0100

    added test cases

--- 8 -  Reversed sequence.js
+++ 8 -  Reversed sequence.js
@@ -3,7 +3,16 @@
 // Build a function that returns an array of integers from n to 1 where n>0.
 
 // Example : n=5 --> [5,4,3,2,1]
+// Test Cases:
+
+// describe("reverseSeq", function () {
+//   it("Sample Test", function () {
+//     assert.deepEqual(reverseSeq(5), [5, 4, 3, 2, 1]);
+//   });
+// });
 
 const reverseSeq = (n) => {
   return [];
 };
+
+console.log(reverseSeq(5));
