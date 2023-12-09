8a506f842b31de84e7b5d458973234a656b2e2c2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-09 18:26:45 +0100

    added starter code and test cases

--- 7 - Find the divisors!.js
+++ 7 - Find the divisors!.js
@@ -7,3 +7,19 @@
 // divisors(12); // should return [2,3,4,6]
 // divisors(25); // should return [5]
 // divisors(13); // should return "13 is prime"
+
+// Test Cases:
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertDeepEquals(divisors(15), [3, 5]);
+//     Test.assertDeepEquals(divisors(12), [2, 3, 4, 6]);
+//     Test.assertDeepEquals(divisors(13), "13 is prime");
+//   });
+// });
+
+function divisors(integer) {}
+
+console.log(divisors(15));
+console.log(divisors(12));
+console.log(divisors(13));
