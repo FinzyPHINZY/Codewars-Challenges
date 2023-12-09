055f930ff7c08725b2605678e926ddec3b726d44
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-09 18:38:12 +0100

    added test cases and solution to kata

--- 8 - Reversed Words.js
+++ 8 - Reversed Words.js
@@ -10,5 +10,12 @@
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
 function reverseWords(str) {
-  return str; // reverse those words
+  return str.split(" ").reverse().join(" "); // reverse those words
 }
+
+console.log(reverseWords("hello world!"));
+console.log(reverseWords("yoda doesn't speak like this"));
+console.log(reverseWords("foobar"));
+console.log(reverseWords("kata editor"));
+console.log(reverseWords("row row row your boat"));
+console.log(reverseWords(""));
