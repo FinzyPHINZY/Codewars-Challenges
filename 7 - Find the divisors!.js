2dc292752fdef01e78ffb1dc43982035912af5ba
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-09 18:32:37 +0100

    added solution to kata

--- 7 - Find the divisors!.js
+++ 7 - Find the divisors!.js
@@ -18,7 +18,19 @@
 //   });
 // });
 
-function divisors(integer) {}
+function divisors(integer) {
+  let result = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) {
+      result.push(i);
+    }
+  }
+  if (result.length > 0) {
+    return result;
+  } else {
+    return `${integer} is prime`;
+  }
+}
 
 console.log(divisors(15));
 console.log(divisors(12));
