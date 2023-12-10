8c54986ce5bfa6815883cdfad2a9020045234a08
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-10 01:51:05 +0100

    added solution to kata

--- 8 - Powers of 2.js
+++ 8 - Powers of 2.js
@@ -18,7 +18,11 @@
 // });
 
 function powersOfTwo(n) {
-  return [];
+  const result = [];
+  for (let i = 0; i <= n; i++) {
+    result.push(2 ** i);
+  }
+  return result;
 }
 
 console.log(powersOfTwo(0));
