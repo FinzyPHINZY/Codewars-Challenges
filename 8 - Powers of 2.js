d76b35b774896a9fef58ca45e52d25b00abd1b86
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-10 01:42:57 +0100

    added starter code and test cases

--- 8 - Powers of 2.js
+++ 8 - Powers of 2.js
@@ -7,3 +7,20 @@
 // n = 0  ==> [1]        # [2^0]
 // n = 1  ==> [1, 2]     # [2^0, 2^1]
 // n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+// Test Cases:
+// describe("Basic Tests", function () {
+//   it("Testing for fixed tests", () => {
+//     assert.deepEqual(powersOfTwo(0), [1]);
+//     assert.deepEqual(powersOfTwo(1), [1, 2]);
+//     assert.deepEqual(powersOfTwo(4), [1, 2, 4, 8, 16]);
+//   });
+// });
+
+function powersOfTwo(n) {
+  return [];
+}
+
+console.log(powersOfTwo(0));
+console.log(powersOfTwo(1));
+console.log(powersOfTwo(4));
