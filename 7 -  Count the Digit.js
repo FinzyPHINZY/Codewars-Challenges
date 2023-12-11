03bce7c66105b09ccfbc4301677a65e8ae482bbc
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-11 10:06:06 +0100

    added test cases

--- 7 -  Count the Digit.js
+++ 7 -  Count the Digit.js
@@ -20,6 +20,20 @@
 // So there are 11 digits 1 for the squares of numbers between 0 and 25.
 // Note that 121 has twice the digit 1.
 
+describe("nbDig", () => {
+  it("Basic tests", () => {
+    assert.strictEqual(nbDig(5750, 0), 4700, "n = 5750, d = 0");
+    assert.strictEqual(nbDig(11011, 2), 9481, "n = 11011, d = 2");
+    assert.strictEqual(nbDig(12224, 8), 7733, "n = 12224, d = 8");
+    assert.strictEqual(nbDig(11549, 1), 11905, "n = 11549, d = 1");
+  });
+});
+
 function nbDig(n, d) {
   // your code
 }
+
+console.log(nbDig(5750, 0));
+console.log(nbDig(11011, 2));
+console.log(nbDig(12224, 8));
+console.log(nbDig(11549, 1));
