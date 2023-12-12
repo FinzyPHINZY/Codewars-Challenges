b30c8be334eade817b0814a02fcabc3e133d96d7
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-12 09:16:32 +0100

    added starter code

--- 7 - Don't give me five!.js
+++ 7 - Don't give me five!.js
@@ -16,3 +16,7 @@
 // Have fun coding it and please don't forget to vote and rank this kata! :-)
 
 // I have also created other katas. Take a look if you enjoyed this kata!
+
+function dontGiveMeFive(start, end) {
+  return 0;
+}
