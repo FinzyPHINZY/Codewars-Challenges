9297d8274512277a26c3719d6377790a67bc870b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-12 09:05:57 +0100

    added test cases

--- 8 - Short Long Short.js
+++ 8 - Short Long Short.js
@@ -11,6 +11,21 @@
 // ("1", "22") --> "1221"
 // ("22", "1") --> "1221"
 
+// Test Cases:
+// describe("Sample tests", function () {
+//   it("Should pass sample tests", function () {
+//     Test.assertEquals(solution("45", "1"), "1451");
+//     Test.assertEquals(solution("13", "200"), "1320013");
+//     Test.assertEquals(solution("Soon", "Me"), "MeSoonMe");
+//     Test.assertEquals(solution("U", "False"), "UFalseU");
+//   });
+// });
+
 function solution(a, b) {
   // your code here
 }
+
+console.log(solution("45", "1"));
+console.log(solution("13", "200"));
+console.log(solution("Soon", "Me"));
+console.log(solution("U", "False"));
