c33c72525c8614b7513faaef97821e510064648f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-12 09:01:38 +0100

    added kata solution

--- 7 - Sum of a sequence.js
+++ 7 - Sum of a sequence.js
@@ -29,6 +29,12 @@
 
 const sequenceSum = (begin, end, step) => {
   // May the Force be with you
+  let sum = 0;
+  for (let i = begin; i <= end; i += step) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 console.log(sequenceSum(2, 6, 2));
