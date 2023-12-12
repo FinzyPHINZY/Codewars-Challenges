8ccaaf91b1a77c0d0e3c7c400e3b593eab90c0fb
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-12 10:42:22 +0100

    added kata description,test cases and solution

---
+++ 8 - Capitalization and Mutability.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// DESCRIPTION:
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. it must make the first character in the string upper case).
+
+// The string will always start with a letter and will never be empty.
+
+// Examples:
+
+// "hello" --> "Hello"
+// "Hello" --> "Hello" (the first letter was already capitalized)
+// "a"     --> "A"
+
+// Test Cases:
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertSimilar(capitalizeWord("word"), "Word");
+//     Test.assertSimilar(capitalizeWord("i"), "I");
+//     Test.assertSimilar(capitalizeWord("glasswear"), "Glasswear");
+//   });
+// });
+
+// function capitalizeWord(word) {
+
+//   return word.toUpperCase();
+// }
+function capitalizeWord(word) {
+  return word.slice(0, 1).toUpperCase() + word.slice(1);
+}
+
+capitalizeWord("word");
+capitalizeWord("i");
+capitalizeWord("glasswear");
