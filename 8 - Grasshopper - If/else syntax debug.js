1e5adbe50b2435d3efbff86939c40c7fab1e0eca
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-14 08:07:14 +0100

    added test cases and starter code

--- 8 - Grasshopper - If/else syntax debug.js
+++ 8 - Grasshopper - If/else syntax debug.js
@@ -6,3 +6,27 @@
 // checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
 
 // The function receives one parameter health which will always be a whole number between -10 and 10.
+
+// Test Cases:
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertEquals(checkAlive(5), true)
+//     Test.assertEquals(checkAlive(0), false)
+//   });
+// });
+
+
+
+function checkAlive (health) {
+    if () {
+      health < 0
+      return false
+    } else () {
+      return true
+    }
+  }
+
+
+  console.log(checkAlive(5))
+  console.log(checkAlive(0))
