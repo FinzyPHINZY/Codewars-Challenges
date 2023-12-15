5a421412db7bcee5a71871940310c034f7f0fc2f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-15 16:35:48 +0100

    added solution to kata

--- 8 - get character from ASCII Value.js
+++ 8 - get character from ASCII Value.js
@@ -10,24 +10,25 @@
 // For ASCII table, you can refer to http://www.asciitable.com/
 
 // Test Cases:
-describe("Tests", () => {
-  it("test", () => {
-    Test.assertEquals(getChar(55), "7");
-    Test.assertEquals(getChar(56), "8");
-    Test.assertEquals(getChar(57), "9");
-    Test.assertEquals(getChar(58), ":");
-    Test.assertEquals(getChar(59), ";");
-    Test.assertEquals(getChar(60), "<");
-    Test.assertEquals(getChar(61), "=");
-    Test.assertEquals(getChar(62), ">");
-    Test.assertEquals(getChar(63), "?");
-    Test.assertEquals(getChar(64), "@");
-    Test.assertEquals(getChar(65), "A");
-  });
-});
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertEquals(getChar(55), "7");
+//     Test.assertEquals(getChar(56), "8");
+//     Test.assertEquals(getChar(57), "9");
+//     Test.assertEquals(getChar(58), ":");
+//     Test.assertEquals(getChar(59), ";");
+//     Test.assertEquals(getChar(60), "<");
+//     Test.assertEquals(getChar(61), "=");
+//     Test.assertEquals(getChar(62), ">");
+//     Test.assertEquals(getChar(63), "?");
+//     Test.assertEquals(getChar(64), "@");
+//     Test.assertEquals(getChar(65), "A");
+//   });
+// });
 
 function getChar(c) {
   // ...
+  return String.fromCharCode(c);
 }
 
 console.log(getChar(55));
