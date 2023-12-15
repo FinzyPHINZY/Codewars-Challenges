ddba91c59b10e6bdd083c22729a1002d75fc87c2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-15 19:46:11 +0100

    added test cases

--- 8 - Stringy Strings.js
+++ 8 - Stringy Strings.js
@@ -12,6 +12,28 @@
 
 // The size will always be positive and will only use whole numbers.
 
+// Test Cases:
+
+// it("Should return a string", function () {
+//   Test.assertEquals(typeof stringy(3),"string","Make sure it's a string not a number (the function is called stringy after all!)"
+//   );
+// });
+
+// it("Should start with a 1", function () {
+//   Test.assertEquals( stringy(3)[0],"1", "Whoops your string doesnt start with a 1?"
+//   );
+// });
+
+// it("Should have the correct length", function () {
+//   for (let i = 0; i < 10; ++i) {
+//     let parameter = (Math.random() * 50 + 1) | 0;
+//     Test.assertEquals(  stringy(parameter).length,parameter,"Wrong length on " + parameter
+//     );
+//   }
+// });
+
 function stringy(size) {
   // your code here
 }
+
+console.log(string(3));
