75524bc12b532fe77ba19d4aeb7076e395e550fb
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-16 20:01:17 +0100

    added test cases

--- 8 - Exclusive or (xor) Logical Operator.js
+++ 8 - Exclusive or (xor) Logical Operator.js
@@ -12,6 +12,23 @@
 // Task
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 
+// Test cases:
+// describe("Your 'xor' function/operator", () => {
+//     it("should work for the four basic cases described above", () => {
+//       assert.strictEqual(xor(false, false), false, "false xor false");
+//       assert.strictEqual(xor(true, false), true, "true xor false");
+//       assert.strictEqual(xor(false, true), true, "false xor true");
+//       assert.strictEqual(xor(true, true), false, "true xor true");
+//       assert.notStrictEqual(xor(true, true), true, "true xor true: 'xor' is NOT identical to 'or'");
+//     });
+//   });
+
 function xor(a, b) {
   // TODO: Program Me
 }
+
+console.log(xor(false, false));
+console.log(xor(true, false));
+console.log(xor(false, true));
+console.log(xor(true, true));
+console.log(xor(true, true));
