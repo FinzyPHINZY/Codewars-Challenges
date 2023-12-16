60482685620a8b54fab507eba726d3dd4bc12e04
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-16 20:10:09 +0100

    added test cases

--- 7 - Sum of angles.js
+++ 7 - Sum of angles.js
@@ -3,6 +3,18 @@
 // DESCRIPTION:
 // Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
 
+// Test Cases:
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(angle(3), 180);
+//     assert.strictEqual(angle(4), 360);
+//   });
+// });
+
 function angle(n) {
   //code here
 }
+
+console.log(angle(3));
+console.log(angle(4));
