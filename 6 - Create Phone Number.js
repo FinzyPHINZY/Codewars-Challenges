ff0103d629a122dbce361534db0e7d6a2fb57483
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-19 11:02:00 +0100

    added solution using the replace method of array instances

--- 6 - Create Phone Number.js
+++ 6 - Create Phone Number.js
@@ -34,6 +34,16 @@ function createPhoneNumber(numbers) {
   return `(${code.join("")}) ${numDir.join("")}-${last_4.join("")}`;
 }
 
+function createPhoneNumber(numbers) {
+  var format = "(xxx) xxx-xxxx";
+
+  for (var i = 0; i < numbers.length; i++) {
+    format = format.replace("x", numbers[i]);
+  }
+
+  return format;
+}
+
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); //  "(123) 456-7890"
 // console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); //  "(111) 111-1111"
 // console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); //  "(123) 456-7890"
