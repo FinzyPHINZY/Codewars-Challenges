580956506a765f66641509558fddd240ca78d1b0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-19 11:05:03 +0100

    added test cases

--- 6 - Array.diff.js
+++ 6 - Array.diff.js
@@ -10,4 +10,23 @@
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
+// Test Cases:
+// describe("Basic tests", () => {
+//     it("Should pass Basic tests", () => {
+//       assert.deepEqual(arrayDiff([1,2], [1]), [2], "a was [1,2], b was [1]");
+//       assert.deepEqual(arrayDiff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1]");
+//       assert.deepEqual(arrayDiff([1,2,2], [2]), [1], "a was [1,2,2], b was [2]");
+//       assert.deepEqual(arrayDiff([1,2,2], []), [1,2,2], "a was [1,2,2], b was []");
+//       assert.deepEqual(arrayDiff([], [1,2]), [], "a was [], b was [1,2]");
+//       assert.deepEqual(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
+//     });
+//   });
+
 function arrayDiff(a, b) {}
+
+console.log(arrayDiff([1, 2], [1]));
+console.log(arrayDiff([1, 2, 2], [1]));
+console.log(arrayDiff([1, 2, 2], [2]));
+console.log(arrayDiff([1, 2, 2], []));
+console.log(arrayDiff([], [1, 2]));
+console.log(arrayDiff([1, 2, 3], [1, 2]));
