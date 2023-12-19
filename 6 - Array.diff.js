1efe387ede987fd74b215dcea2cd91d6a260fa0d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-19 11:53:03 +0100

    added solution to kata

--- 6 - Array.diff.js
+++ 6 - Array.diff.js
@@ -22,11 +22,19 @@
 //     });
 //   });
 
-function arrayDiff(a, b) {}
+function arrayDiff(a, b) {
+  let result = [];
+  for (let i = 0; i < a.length; i++) {
+    if (!b.includes(a[i])) {
+      result.push(a[i]);
+    }
+  }
+  return result;
+}
 
-console.log(arrayDiff([1, 2], [1]));
-console.log(arrayDiff([1, 2, 2], [1]));
-console.log(arrayDiff([1, 2, 2], [2]));
-console.log(arrayDiff([1, 2, 2], []));
-console.log(arrayDiff([], [1, 2]));
-console.log(arrayDiff([1, 2, 3], [1, 2]));
+console.log(arrayDiff([1, 2], [1])); // [2]
+console.log(arrayDiff([1, 2, 2], [1])); // [2,2]
+console.log(arrayDiff([1, 2, 2], [2])); // [1]
+console.log(arrayDiff([1, 2, 2], [])); // [1,2,2]
+console.log(arrayDiff([], [1, 2])); // []
+console.log(arrayDiff([1, 2, 3], [1, 2])); // [3]
