e60516b99042f8210e3c6d2d79beff52c7da35d8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-20 11:34:35 +0100

    added starter code

--- 6 -  Stop gninnipS My sdroW!.js
+++ 6 -  Stop gninnipS My sdroW!.js
@@ -8,3 +8,7 @@
 // spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
 // spinWords( "This is a test") => returns "This is a test"
 // spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(string) {
+  //TODO Have fun :)
+}
