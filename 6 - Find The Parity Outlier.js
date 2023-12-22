e103e9cc58791152c585f462c7a997cfb3421a04
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-22 09:10:51 +0100

    added starter code

--- 6 - Find The Parity Outlier.js
+++ 6 - Find The Parity Outlier.js
@@ -7,3 +7,7 @@
 // [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
 
 // [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+
+function findOutlier(integers) {
+  //your code here
+}
