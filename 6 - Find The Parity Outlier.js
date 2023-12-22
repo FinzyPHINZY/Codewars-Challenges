d3014e16dcfa4261ed578344cfee3fdc507da93c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-22 09:12:53 +0100

    added kata test cases

--- 6 - Find The Parity Outlier.js
+++ 6 - Find The Parity Outlier.js
@@ -8,6 +8,24 @@
 
 // [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
 
+// Test cases:
+
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(findOutlier([0, 1, 2]), 1);
+    Test.assertEquals(findOutlier([1, 2, 3]), 2);
+    Test.assertEquals(findOutlier([2, 6, 8, 10, 3]), 3);
+    Test.assertEquals(findOutlier([0, 0, 3, 0, 0]), 3);
+    Test.assertEquals(findOutlier([1, 1, 0, 1, 1]), 0);
+  });
+});
+
 function findOutlier(integers) {
   //your code here
 }
+
+console.log(findOutlier([0, 1, 2]));
+console.log(findOutlier([1, 2, 3]));
+console.log(findOutlier([2, 6, 8, 10, 3]));
+console.log(findOutlier([0, 0, 3, 0, 0]));
+console.log(findOutlier([1, 1, 0, 1, 1]));
