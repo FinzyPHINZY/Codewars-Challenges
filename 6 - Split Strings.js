cb9a22eb1bd696af6482f1282fe8e393fa594839
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-22 10:18:53 +0100

    added solution to kata

--- 6 - Split Strings.js
+++ 6 - Split Strings.js
@@ -18,7 +18,28 @@
 //   });
 // });
 
-function solution(str) {}
+function solution(str) {
+  // Check if the string is empty
+  if (str.length === 0) {
+    return [];
+  }
+
+  // Create an array to store the pairs
+  const pairs = [];
+
+  // Loop through the string with a step of 2
+  for (let i = 0; i < str.length; i += 2) {
+    // Extract two characters and add them to the pairs array
+    pairs.push(str.slice(i, i + 2));
+  }
+
+  // If the string length is odd, replace the last pair's second character with '_'
+  if (str.length % 2 !== 0) {
+    pairs[pairs.length - 1] += "_";
+  }
+
+  return pairs;
+}
 
 console.log(solution("abcdef"));
 console.log(solution("abcdefg"));
