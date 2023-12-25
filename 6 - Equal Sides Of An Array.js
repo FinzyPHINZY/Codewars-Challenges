743e8afb91dca66cee52cd9442fecdc344d5f476
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-25 09:03:47 +0100

    added solution to kata

--- 6 - Equal Sides Of An Array.js
+++ 6 - Equal Sides Of An Array.js
@@ -78,7 +78,21 @@ describe("FindEvenIndex", function () {
 });
 
 */
-
 function findEvenIndex(arr) {
-  //Code goes here!
+  for (let i = 0; i < arr.length; i++) {
+    const leftSum = arr.slice(0, i).reduce((acc, num) => acc + num, 0);
+    const rightSum = arr.slice(i + 1).reduce((acc, num) => acc + num, 0);
+    // console.log(`${i}: left-[${leftSum}] || right-[${rightSum}`);
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+
+  return -1;
 }
+
+// Examples
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1])); // Output: 3
+// console.log(findEvenIndex([1, 100, 50, -51, 1, 1])); // Output: 1
+// console.log(findEvenIndex([20, 10, -80, 10, 10, 15, 35])); // Output: 0
