c4b8e84b14571b4e436a9377bb837d3eefbdf554
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-27 15:23:50 +0100

    added test cases" git push

--- 6 - Tribonacci Sequence.js
+++ 6 - Tribonacci Sequence.js
@@ -15,6 +15,31 @@
 
 // Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
 
+// Test Cases:
+
+// doTest([[1, 1, 1], 10], [1, 1, 1, 3, 5, 9, 17, 31, 57, 105]);
+// doTest([[0, 0, 1], 10], [0, 0, 1, 1, 2, 4, 7, 13, 24, 44]);
+// doTest([[0, 1, 1], 10], [0, 1, 1, 2, 4, 7, 13, 24, 44, 81]);
+// doTest([[1, 0, 0], 10], [1, 0, 0, 1, 1, 2, 4, 7, 13, 24]);
+// doTest([[0, 0, 0], 10], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+// doTest([[1, 2, 3], 10], [1, 2, 3, 6, 11, 20, 37, 68, 125, 230]);
+// doTest([[3, 2, 1], 10], [3, 2, 1, 6, 9, 16, 31, 56, 103, 190]);
+// doTest([[1, 1, 1], 1], [1]);
+// doTest([[300, 200, 100], 0], []);
+
 function tribonacci(signature, n) {
   //your code here
+  if (n === 0) {
+    return [];
+  }
 }
+
+console.log(tribonacci([1, 1, 1], 10));
+console.log(tribonacci([0, 0, 1], 10));
+console.log(tribonacci([0, 1, 1], 10));
+console.log(tribonacci([1, 0, 0], 10));
+console.log(tribonacci([0, 0, 0], 10));
+console.log(tribonacci([1, 2, 3], 10));
+console.log(tribonacci([3, 2, 1], 10));
+console.log(tribonacci([1, 1, 1], 1));
+console.log(tribonacci([300, 200, 100], 0));
