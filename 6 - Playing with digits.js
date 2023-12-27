de8c628e1119c6c7cd0e579e5405b7112f121236
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-27 15:06:45 +0100

    added solutiont to kata

--- 6 - Playing with digits.js
+++ 6 - Playing with digits.js
@@ -30,6 +30,20 @@
 
 function digPow(n, p) {
   // ...
+  const digits = n.toString().split("");
+  let sum = 0;
+
+  for (let i = 0; i < digits.length; i++) {
+    sum += Math.pow(parseInt(digits[i]), p + i);
+  }
+
+  const k = sum / n;
+
+  if (Number.isInteger(k)) {
+    return k;
+  } else {
+    return -1;
+  }
 }
 
 console.log(digPow(89, 1));
