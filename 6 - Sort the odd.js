8130f07bb6f7371fcb356fe21d88f18297d6afb0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-29 13:22:08 +0100

    added test cases;

--- 6 - Sort the odd.js
+++ 6 - Sort the odd.js
@@ -9,6 +9,19 @@
 // [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
+// Test Cases:
+// describe("Sample Tests", () => {
+//   it("should pass some sample tests", () => {
+//     assert.deepEqual(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+//     assert.deepEqual(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+//     assert.deepEqual(sortArray([]), []);
+//   });
+// });
+
 function sortArray(array) {
   // Return a sorted array.
 }
+
+console.log(sortArray([5, 3, 2, 8, 1, 4]));
+console.log(sortArray([5, 3, 1, 8, 0]));
+console.log(sortArray([]));
