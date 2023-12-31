bb9b8a44568a02b8398782a0d99795f0f76168e6
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2023-12-31 13:13:46 +0100

    added test cases

--- 6 - Find the missing letter.js
+++ 6 - Find the missing letter.js
@@ -17,6 +17,17 @@
 
 // I have also created other katas. Take a look if you enjoyed this kata!
 
+// Test Cases:
+// escribe("KataTests", function () {
+//   it("exampleTests", function () {
+//     Test.assertEquals(findMissingLetter(["a", "b", "c", "d", "f"]), "e");
+//     Test.assertEquals(findMissingLetter(["O", "Q", "R", "S"]), "P");
+//   });
+// });
+
 function findMissingLetter(array) {
   return " ";
 }
+
+console.log(findMissingLetter(["a", "b", "c", "d", "f"]));
+console.log(findMissingLetter(["O", "Q", "R", "S"]));
