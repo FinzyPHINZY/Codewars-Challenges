365cc479586358d881316b4d28ccbaf3bd64e3de
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-04 10:47:50 +0100

    added kata solution

--- 7 - Check the exam.js
+++ 7 - Check the exam.js
@@ -26,9 +26,21 @@
 
 function checkExam(array1, array2) {
   // good luck
+  let score = 0;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      score += 4;
+    } else if (array1[i] === "" || array2[i] === "") {
+      score += 0;
+    } else {
+      score -= 1;
+    }
+  }
+
+  return score > 0 ? score : 0;
 }
 
 console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]));
 console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]));
 console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]));
-console.log(checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"])));
+console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]));
