e4d579c16c9fbb9fd351eae2993958d3c19f749b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-05 08:40:45 +0100

    added test cases and starter code

--- 7 - Greet Me.js
+++ 7 - Greet Me.js
@@ -7,3 +7,12 @@
 
 // "riley" --> "Hello Riley!"
 // "JACK"  --> "Hello Jack!"
+
+// Test Cases:
+Test.assertEquals(greet("riley"), "Hello Riley!");
+Test.assertEquals(greet("Jack"), "Hello Jack!");
+
+var greet = function (name) {};
+
+console.log(greet("riley"));
+console.log(greet("Jack"));
