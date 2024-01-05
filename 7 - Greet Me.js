6ab8e1c26d2daac6f4a8e5f55cbb05693d9dd07d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-05 08:37:58 +0100

    added kata description

--- 7 - Greet Me.js
+++ 7 - Greet Me.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// DESCRIPTION:
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
