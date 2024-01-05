68fbc3b92bc525cae061e6af5088f52fda4018f1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-05 08:23:50 +0100

    added test cases

--- 7 - Fizz Buzz.js
+++ 7 - Fizz Buzz.js
@@ -14,7 +14,20 @@
 
 // fizzbuzz(3) -->  [1, 2, "Fizz"]
 
+describe("Fizzbuzz", function () {
+  it("Should fizzify 10 numbers correctly", function () {
+    var expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"];
+    Test.assertEquals(
+      JSON.stringify(fizzbuzz(10)),
+      JSON.stringify(expected),
+      "Fails with 10 numbers!"
+    );
+  });
+});
+
 // Return an array
 function fizzbuzz(n) {
   //
 }
+
+console.log(fizzbuzz(10));
