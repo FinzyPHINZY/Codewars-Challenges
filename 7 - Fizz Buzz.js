fcab5a490d9c4ad960e28310f3c862e2cc9eade1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-05 08:35:48 +0100

    added solution to kata

--- 7 - Fizz Buzz.js
+++ 7 - Fizz Buzz.js
@@ -14,20 +14,33 @@
 
 // fizzbuzz(3) -->  [1, 2, "Fizz"]
 
-describe("Fizzbuzz", function () {
-  it("Should fizzify 10 numbers correctly", function () {
-    var expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"];
-    Test.assertEquals(
-      JSON.stringify(fizzbuzz(10)),
-      JSON.stringify(expected),
-      "Fails with 10 numbers!"
-    );
-  });
-});
+// describe("Fizzbuzz", function () {
+//   it("Should fizzify 10 numbers correctly", function () {
+//     var expected = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"];
+//     Test.assertEquals(
+//       JSON.stringify(fizzbuzz(10)),
+//       JSON.stringify(expected),
+//       "Fails with 10 numbers!"
+//     );
+//   });
+// });
 
 // Return an array
 function fizzbuzz(n) {
   //
+  let output = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      output.push("Fizz");
+    } else if (i % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(i);
+    }
+  }
+  return output;
 }
 
 console.log(fizzbuzz(10));
