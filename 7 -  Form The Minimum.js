56a786f3289581a01997fa1d4590ccec4ae46841
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-05 08:07:08 +0100

    added test cases

--- 7 -  Form The Minimum.js
+++ 7 -  Form The Minimum.js
@@ -19,6 +19,32 @@
 // Explanation:
 // (134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6, 7} , Without duplications
 
+// Test Cases:
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(minValue([1, 3, 1]), 13);
+//     assert.strictEqual(minValue([4, 7, 5, 7]), 457);
+//     assert.strictEqual(minValue([4, 8, 1, 4]), 148);
+//     assert.strictEqual(minValue([5, 7, 9, 5, 7]), 579);
+//     assert.strictEqual(minValue([6, 7, 8, 7, 6, 6]), 678);
+//     assert.strictEqual(minValue([5, 6, 9, 9, 7, 6, 4]), 45679);
+//     assert.strictEqual(minValue([1, 9, 1, 3, 7, 4, 6, 6, 7]), 134679);
+//     assert.strictEqual(minValue([3, 6, 5, 5, 9, 8, 7, 6, 3, 5, 9]), 356789);
+//     assert.strictEqual(minValue([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), 1);
+//   });
+// });
+
 function minValue(values) {
   //your code here
 }
+
+console.log(minValue([1, 3, 1]));
+console.log(minValue([4, 7, 5, 7]));
+console.log(minValue([4, 8, 1, 4]));
+console.log(minValue([5, 7, 9, 5, 7]));
+console.log(minValue([6, 7, 8, 7, 6, 6]));
+console.log(minValue([5, 6, 9, 9, 7, 6, 4]));
+console.log(minValue([1, 9, 1, 3, 7, 4, 6, 6, 7]));
+console.log(minValue([3, 6, 5, 5, 9, 8, 7, 6, 3, 5, 9]));
+console.log(minValue([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]));
