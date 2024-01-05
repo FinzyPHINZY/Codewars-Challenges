0bf6bd1298559f7bb8a5b2830dd17897be2d70f5
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-05 08:04:28 +0100

    added starter code

--- 7 -  Form The Minimum.js
+++ 7 -  Form The Minimum.js
@@ -18,3 +18,7 @@
 // minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
 // Explanation:
 // (134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6, 7} , Without duplications
+
+function minValue(values) {
+  //your code here
+}
