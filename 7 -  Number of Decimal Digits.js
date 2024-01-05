5e9f5b326a4af2e2c641eb8c3ed01a20e09e2320
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-05 07:59:11 +0100

    added test cases

--- 7 -  Number of Decimal Digits.js
+++ 7 -  Number of Decimal Digits.js
@@ -5,6 +5,28 @@
 
 // All inputs will be valid.
 
+// Test Cases:
+
+// describe("Sample Tests", function () {
+//   const tests = [
+//     [0, 1],
+//     [9, 1],
+//     [66, 2],
+//     [12345, 5],
+//     [128685, 6],
+//     [9876543210, 10],
+//     [9007199254740991, 16],
+//   ];
+// });
+
 function digits(n) {
   // TODO
 }
+
+console.log(digits(0));
+console.log(digits(9));
+console.log(digits(66));
+console.log(digits(12345));
+console.log(digits(128685));
+console.log(digits(9876543210));
+console.log(digits(9007199254740991));
