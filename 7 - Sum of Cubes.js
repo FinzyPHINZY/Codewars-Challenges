2a69f9c8b8b428e63867f257ce0a0f6e206d5274
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-06 11:40:46 +0100

    added test cases and solution to kata

--- 7 - Sum of Cubes.js
+++ 7 - Sum of Cubes.js
@@ -9,7 +9,24 @@
 
 // 2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
 // 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
+// Test Cases:
+
+// describe("Sum of Cubes", () => {
+//     it("Fixed tests", () => {
+//       _.shuffle( [
+//         [1, 1], [2, 9], [3, 36], [4, 100],
+//         [10, 3025], [123, 58155876]
+//       ] ).forEach(([n,exp]) => assert.strictEqual(sumCubes(n), exp))
+//     });
+//   });
 
 function sumCubes(n) {
   // ...
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i ** 3;
+  }
+  return total;
 }
+
+console.log(sumCubes(2));
