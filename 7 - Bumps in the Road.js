1da219a4935eb99d72bd2c95f2afb0373c5dd8d2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-06 12:04:16 +0100

    added solution to kata

--- 7 - Bumps in the Road.js
+++ 7 - Bumps in the Road.js
@@ -6,18 +6,20 @@
 // Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
 
 // Test Cases:
-describe("Bumps in the Road", () => {
-  it("Testing for fixed tests", () => {
-    assert.strictEqual(bump("n"), "Woohoo!");
-    assert.strictEqual(bump("__nn_nnnn__n_n___n____nn__nnn"), "Woohoo!");
-    assert.strictEqual(bump("nnn_n__n_n___nnnnn___n__nnn__"), "Woohoo!");
-    assert.strictEqual(bump("_nnnnnnn_n__n______nn__nn_nnn"), "Car Dead");
-    assert.strictEqual(bump("______n___n_"), "Woohoo!");
-  });
-});
+// describe("Bumps in the Road", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(bump("n"), "Woohoo!");
+//     assert.strictEqual(bump("__nn_nnnn__n_n___n____nn__nnn"), "Woohoo!");
+//     assert.strictEqual(bump("nnn_n__n_n___nnnnn___n__nnn__"), "Woohoo!");
+//     assert.strictEqual(bump("_nnnnnnn_n__n______nn__nn_nnn"), "Car Dead");
+//     assert.strictEqual(bump("______n___n_"), "Woohoo!");
+//   });
+// });
 
 function bump(x) {
-  //
+  return x.split("").filter((e) => e === "n").length <= 15
+    ? "Woohoo!"
+    : "Car Dead";
 }
 
 console.log(bump("n"));
