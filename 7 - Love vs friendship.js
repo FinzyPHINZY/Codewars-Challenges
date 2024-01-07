fa1e200a728ae017e9a8a8a0220106dc7392d894
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-07 11:05:26 +0100

    added test cases

--- 7 - Love vs friendship.js
+++ 7 - Love vs friendship.js
@@ -12,3 +12,15 @@
 // Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
 
 // The input will always be made of only lowercase letters and will never be empty.
+
+// Test cases:
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(wordsToMarks("attitude"), 100);
+    assert.strictEqual(wordsToMarks("friends"), 75);
+    assert.strictEqual(wordsToMarks("family"), 66);
+    assert.strictEqual(wordsToMarks("selfness"), 99);
+    assert.strictEqual(wordsToMarks("knowledge"), 96);
+  });
+});
