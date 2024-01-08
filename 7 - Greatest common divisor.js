1120d74fba1537e05a8b5d34bce9b2ea4e0cccdd
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-08 10:20:32 +0100

    added test cases

--- 7 - Greatest common divisor.js
+++ 7 - Greatest common divisor.js
@@ -5,6 +5,21 @@
 
 // The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
 
+// Test Cases:
+// scribe("Example tests", () => {
+//   it("Example tests", () => {
+//     assert.strictEqual(mygcd(30, 12), 6);
+//     assert.strictEqual(mygcd(36, 12), 12);
+//     assert.strictEqual(mygcd(8, 9), 1);
+//     assert.strictEqual(mygcd(1, 1), 1);
+//   });
+// });
+
 function mygcd(x, y) {
   //your code here
 }
+
+console.log(mygcd(30, 12));
+console.log(mygcd(36, 12));
+console.log(mygcd(8, 9));
+console.log(mygcd(1, 1));
