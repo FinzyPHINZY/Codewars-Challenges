0a826a6f563f18fa790e7512d2433eae120fcc98
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-09 09:31:15 +0100

    added test cases

--- 7 -  Filter the number.js
+++ 7 -  Filter the number.js
@@ -10,6 +10,18 @@
 // Details
 // You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
 
+// describe("Sample Tests", function() {
+//     it("should pass some fixed tests", function() {
+//       assert.strictEqual(filterString("123"), 123, `For the input "123"`);
+//       assert.strictEqual(filterString("a1b2c3"), 123, `For the input "a1b2c3"`);
+//       assert.strictEqual(filterString("aa1bb2cc3dd"), 123, `For the input "aa1bb2cc3dd"`);
+//     });
+//   });
+
 var filterString = function (value) {
   //Complete this function :)
 };
+
+console.log(filterString("123"));
+console.log(filterString("a1b2c3"));
+console.log(filterString("aa1bb2cc3dd"));
