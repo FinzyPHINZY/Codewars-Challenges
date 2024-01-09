11fdb7a0e86dd55826701bbf1e78defa9ce12dea
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-09 09:29:45 +0100

    added starter code

--- 7 -  Filter the number.js
+++ 7 -  Filter the number.js
@@ -9,3 +9,7 @@
 
 // Details
 // You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+var filterString = function (value) {
+  //Complete this function :)
+};
