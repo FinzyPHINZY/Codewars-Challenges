d8847a028751768e2899683fbcbd12597c092915
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-10 09:08:40 +0100

    added test cases

--- 7 - Even numbers in an array.js
+++ 7 - Even numbers in an array.js
@@ -9,6 +9,16 @@
 // ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
 // ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
 
+// describe("Basic tests",() => {
+//     Test.assertDeepEquals(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3), [4, 6, 8]);
+//     Test.assertDeepEquals(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2), [-8, 26]);
+//     Test.assertDeepEquals(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1), [6]);
+//   });
+
 function evenNumbers(array, number) {
   // good luck
 }
+
+console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2));
+console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1));
