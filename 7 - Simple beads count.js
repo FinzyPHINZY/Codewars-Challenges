e20eff3eb24bcfe2d0a40bc3a384a9de7ea1b7ff
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-10 10:10:48 +0100

    added test cases

--- 7 - Simple beads count.js
+++ 7 - Simple beads count.js
@@ -8,6 +8,22 @@
 // Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
 // If there are less than 2 blue beads return 0.
 
+// Test Cases:
+
+// describe("Testing", function () {
+//   it("Basic tests", function () {
+//     Test.assertEquals(countRedBeads(0), 0);
+//     Test.assertEquals(countRedBeads(1), 0);
+//     Test.assertEquals(countRedBeads(3), 4);
+//     Test.assertEquals(countRedBeads(5), 8);
+//   });
+// });
+
 function countRedBeads(n) {
   //your code here
 }
+
+console.log(countRedBeads(0));
+console.log(countRedBeads(1));
+console.log(countRedBeads(3));
+console.log(countRedBeads(5));
