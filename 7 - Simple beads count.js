dab570980779c01a964566b97a0fdc2ae44c26bc
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-10 10:09:37 +0100

    added starter code

--- 7 - Simple beads count.js
+++ 7 - Simple beads count.js
@@ -7,3 +7,7 @@
 
 // Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
 // If there are less than 2 blue beads return 0.
+
+function countRedBeads(n) {
+  //your code here
+}
