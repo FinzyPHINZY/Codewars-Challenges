5f301492cf93d3782b0cb095011e546d8d3b2729
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-11 14:04:01 +0100

    added test cases

--- 6 - Consecutive strings.js
+++ 6 - Consecutive strings.js
@@ -25,6 +25,60 @@
 // Note
 // consecutive strings : follow one after another without an interruption
 
+// Test Cases:
+
+// describe("longestConsec",function() {
+//     it("Basic tests",function() {
+//         assert.strictEqual(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta")
+//         assert.strictEqual(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh")
+//         assert.strictEqual(longestConsec([], 3), "")
+//         assert.strictEqual(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
+//         assert.strictEqual(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
+//         assert.strictEqual(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "")
+//         assert.strictEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz")
+//         assert.strictEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
+//         assert.strictEqual(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "")
+//     })
+//   })
+
 function longestConsec(strarr, k) {
   // your code
 }
+
+console.log(
+  longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)
+);
+console.log(
+  longestConsec(
+    [
+      "ejjjjmmtthh",
+      "zxxuueeg",
+      "aanlljrrrxx",
+      "dqqqaaabbb",
+      "oocccffuucccjjjkkkjyyyeehh",
+    ],
+    1
+  )
+);
+console.log(longestConsec([], 3));
+console.log(
+  longestConsec(
+    [
+      "itvayloxrp",
+      "wkppqsztdkmvcuwvereiupccauycnjutlv",
+      "vweqilsfytihvrzlaodfixoyxvyuyvgpck",
+    ],
+    2
+  )
+);
+console.log(
+  longestConsec(["wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu"], 2)
+);
+console.log(
+  longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2)
+);
+console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 3));
+console.log(
+  longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 15)
+);
+console.log(longestConsec(["it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz"], 0));
