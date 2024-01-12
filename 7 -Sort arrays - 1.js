46b70aaac5fe766d9e93b8705d5e464bc41a2112
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-12 09:28:07 +0100

    added solution to kata

--- 7 -Sort arrays - 1.js
+++ 7 -Sort arrays - 1.js
@@ -3,6 +3,16 @@
 // DESCRIPTION:
 // Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
 
+// Test Cases:
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.deepEqual(sortme(["one", "two", "three"]), ["one", "three", "two"]);
+//   });
+// });
+
 // input: names - unsorted array
 // output: sorted array
 sortme = function (names) {};
+
+console.log(sortme(["one", "two", "three"]));
