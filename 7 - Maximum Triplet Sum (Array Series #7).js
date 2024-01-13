d24c26ff14ac88374ebb54846d8c20ffbe6fc3c8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-14 00:24:18 +0100

    added test cases

--- 7 - Maximum Triplet Sum (Array Series #7).js
+++ 7 - Maximum Triplet Sum (Array Series #7).js
@@ -30,6 +30,34 @@
 
 // Note : duplications are not included when summing , (i.e) the numbers added only once .
 
+// Test Cases:
+
+// describe("Basic tests", () => {
+//     it("Testing for fixed tests", () => {
+//       assert.strictEqual(maxTriSum([3,2,6,8,2,3]),17);
+//       assert.strictEqual(maxTriSum([2,9,13,10,5,2,9,5]),32);
+//       assert.strictEqual(maxTriSum([2,1,8,0,6,4,8,6,2,4]),18);
+//       assert.strictEqual(maxTriSum([-3,-27,-4,-2,-27,-2]),-9);
+//       assert.strictEqual(maxTriSum([-14,-12,-7,-42,-809,-14,-12]),-33);
+//       assert.strictEqual(maxTriSum([-13,-50,57,13,67,-13,57,108,67]),232);
+//       assert.strictEqual(maxTriSum([-7,12,-7,29,-5,0,-7,0,0,29]),41);
+//       assert.strictEqual(maxTriSum([-2,0,2]),0);
+//       assert.strictEqual(maxTriSum([-2,-4,0,-9,2]),0);
+//       assert.strictEqual(maxTriSum([-5,-1,-9,0,2]),1);
+//     });
+//   });
+
 function maxTriSum(numbers) {
   //your code here
 }
+
+console.log(maxTriSum([3, 2, 6, 8, 2, 3]));
+console.log(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5]));
+console.log(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4]));
+console.log(maxTriSum([-3, -27, -4, -2, -27, -2]));
+console.log(maxTriSum([-14, -12, -7, -42, -809, -14, -12]));
+console.log(maxTriSum([-13, -50, 57, 13, 67, -13, 57, 108, 67]));
+console.log(maxTriSum([-7, 12, -7, 29, -5, 0, -7, 0, 0, 29]));
+console.log(maxTriSum([-2, 0, 2]));
+console.log(maxTriSum([-2, -4, 0, -9, 2]));
+console.log(maxTriSum([-5, -1, -9, 0, 2]));
