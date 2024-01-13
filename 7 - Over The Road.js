6120d5fc25fed8cc76b4083eb8f1d9469f7d4bda
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-14 00:40:55 +0100

    added solution to kata

--- 7 - Over The Road.js
+++ 7 - Over The Road.js
@@ -18,6 +18,36 @@
 // 2, 3 --> 5
 // 3, 5 --> 8
 
+// Test Cases:
+
+// describe("Examples", function () {
+//   it("(1,3) --> 6", function () {
+//     assert.strictEqual(overTheRoad(1, 3), 6);
+//   });
+//   it("(3,3) --> 4", function () {
+//     assert.strictEqual(overTheRoad(3, 3), 4);
+//   });
+//   it("2,3) --> 5", function () {
+//     assert.strictEqual(overTheRoad(2, 3), 5);
+//   });
+//   it("(3,5) --> 8", function () {
+//     assert.strictEqual(overTheRoad(3, 5), 8);
+//   });
+//   it("(7,11) --> 16", function () {
+//     assert.strictEqual(overTheRoad(7, 11), 16);
+//   });
+//   it("(23633656673,310027696726) --> 596421736780", function () {
+//     assert.strictEqual(overTheRoad(23633656673, 310027696726), 596421736780);
+//   });
+// });
+
 function overTheRoad(address, n) {
   //code here
 }
+
+console.log(overTheRoad(1, 3));
+console.log(overTheRoad(3, 3));
+console.log(overTheRoad(2, 3));
+console.log(overTheRoad(3, 5));
+console.log(overTheRoad(7, 11));
+console.log(overTheRoad(23633656673, 310027696726));
