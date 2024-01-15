ef4c58878ec52f71fb09bef35491ec6b40ab5f3c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-15 10:57:25 +0100

    added test cases

--- 7 - Divide and Conquer.js
+++ 7 - Divide and Conquer.js
@@ -5,4 +5,17 @@
 
 // Return as a number.
 
+// Test Cases:
+// describe("Divide and Conquer", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(divCon([9, 3, "7", "3"]), 2);
+//     assert.strictEqual(divCon(["5", "0", 9, 3, 2, 1, "9", 6, 7]), 14);
+//     assert.strictEqual(divCon(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"]), 13);
+//   });
+// });
+
 function divCon(x) {}
+
+console.log(divCon([9, 3, "7", "3"]));
+console.log(divCon(["5", "0", 9, 3, 2, 1, "9", 6, 7]));
+console.log(divCon(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"]));
