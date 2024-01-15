f91aa701740501ca78c5550e9aa1b05a1c4e680b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-15 10:55:32 +0100

    added kata description

--- 7 - Divide and Conquer.js
+++ 7 - Divide and Conquer.js
@@ -1,1 +1,6 @@
 "use strict";
+
+// DESCRIPTION:
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+// Return as a number.
