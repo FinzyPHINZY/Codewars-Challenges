744bd3c29eb233f550ea3a6abf9b6ea088dd5fa8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-15 09:14:25 +0100

    added test cases

--- 6 - Mexican Wave.js
+++ 6 - Mexican Wave.js
@@ -14,6 +14,56 @@
 // Example
 // wave("hello") => {"Hello", "hEllo", "heLlo", "helLo", "hellO"}
 
+// Test Cases:
+// describe("Testing...", function () {
+//   it("Basic tests", () => {
+//     let result = ["Hello", "hEllo", "heLlo", "helLo", "hellO"];
+//     assert.deepEqual(wave("hello"), result, "Should return: '" + result + "'");
+
+//     result = [
+//       "Codewars",
+//       "cOdewars",
+//       "coDewars",
+//       "codEwars",
+//       "codeWars",
+//       "codewArs",
+//       "codewaRs",
+//       "codewarS",
+//     ];
+//     assert.deepEqual(
+//       wave("codewars"),
+//       result,
+//       "Should return: '" + result + "'"
+//     );
+
+//     result = [];
+//     assert.deepEqual(wave(""), result, "Should return: '" + result + "'");
+
+//     result = [
+//       "Two words",
+//       "tWo words",
+//       "twO words",
+//       "two Words",
+//       "two wOrds",
+//       "two woRds",
+//       "two worDs",
+//       "two wordS",
+//     ];
+//     assert.deepEqual(
+//       wave("two words"),
+//       result,
+//       "Should return: '" + result + "'"
+//     );
+
+//     result = [" Gap ", " gAp ", " gaP "];
+//     assert.deepEqual(wave(" gap "), result, "Should return: '" + result + "'");
+//   });
+// });
+
 function wave(str) {
   // Code here
 }
+
+console.log(wave("hello"));
+console.log(wave("codewars"));
+console.log(wave("two words"));
