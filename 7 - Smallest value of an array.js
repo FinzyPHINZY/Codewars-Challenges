a314d33e1cc4be7a0bf54ff1cc257cdafc293f1a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-15 10:54:37 +0100

    added solution to kata

--- 7 - Smallest value of an array.js
+++ 7 - Smallest value of an array.js
@@ -10,4 +10,10 @@
 
 function min(arr, toReturn) {
   // TODO
+  return toReturn === "value"
+    ? Math.min(...arr)
+    : arr.indexOf(Math.min(...arr));
 }
+
+console.log(min([1, 2, 3, 4, 5], "value"));
+console.log(min([1, 2, 3, 4, 5], "index"));
