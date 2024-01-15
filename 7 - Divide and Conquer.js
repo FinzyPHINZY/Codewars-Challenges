9fbddcee87c659e7d18fef9018d3a96a9bbe7cd5
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-15 11:03:13 +0100

    added solution to kat

--- 7 - Divide and Conquer.js
+++ 7 - Divide and Conquer.js
@@ -14,7 +14,21 @@
 //   });
 // });
 
-function divCon(x) {}
+function divCon(x) {
+  // get sum of all intergers
+  const sumInt = x
+    .filter((int) => typeof int === "number")
+    .reduce((sum, int) => sum + int, 0);
+
+  // get sum of string intergers
+  const sumStrInt = x
+    .filter((int) => typeof int === "string")
+    .reduce((sum, int) => Number(sum) + Number(int), 0);
+
+  // remove strIntergers from Intergers
+
+  return sumInt - sumStrInt;
+}
 
 console.log(divCon([9, 3, "7", "3"]));
 console.log(divCon(["5", "0", 9, 3, 2, 1, "9", 6, 7]));
