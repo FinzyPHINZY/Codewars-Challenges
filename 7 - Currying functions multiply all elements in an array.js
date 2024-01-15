cba8791f02d940d75b3dc0ef6209e50b79d8b452
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-15 10:01:23 +0100

    added solution to kata

--- 7 - Currying functions multiply all elements in an array.js
+++ 7 - Currying functions multiply all elements in an array.js
@@ -10,4 +10,10 @@
 // multiplyAll([1, 2, 3])(2) = [2, 4, 6];
 // You must not mutate the original array.
 
-function multiplyAll() {}
+function multiplyAll(arr) {
+  return function (n) {
+    return arr.map((element) => element * n);
+  };
+}
+
+console.log(multiplyAll([1, 2, 3]));
