04d8fa3fde0f4f8e8230c889ecb6d62622bbdcc8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-16 10:34:54 +0100

    added starter code

--- 7 -  16+18=214.js
+++ 7 -  16+18=214.js
@@ -10,3 +10,7 @@
 // In simple terms, our method does not like the principle of carrying over numbers and just writes down every number it calculates :-)
 
 // You may assume both integers are positive integers.
+
+function add(num1, num2) {
+  return num1 + num2;
+}
