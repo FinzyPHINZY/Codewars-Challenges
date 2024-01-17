17e94d54f766325fe9130c66c799b60f01de7d12
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-17 09:55:50 +0100

    added test cases

--- 7 - Building Strings From a Hash.js
+++ 7 - Building Strings From a Hash.js
@@ -8,7 +8,24 @@
 // Example:
 
 // solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+// Test Cases:
+
+describe("Sample tests", function () {
+  it("Should pass sample tests", function () {
+    assert.strictEqual(solution({ a: 1, b: 2 }), "a = 1,b = 2");
+    assert.strictEqual(solution({ a: "b", b: "a" }), "a = b,b = a");
+    assert.strictEqual(solution({ 0: "a", b: 2 }), "0 = a,b = 2");
+    assert.strictEqual(solution({ b: 1, c: 2, e: 3 }), "b = 1,c = 2,e = 3");
+    assert.strictEqual(solution({}), "");
+  });
+});
 
 function solution(pairs) {
   // TODO: complete
 }
+
+console.log(solution({ a: 1, b: 2 }));
+console.log(solution({ a: "b", b: "a" }));
+console.log(solution({ 0: "a", b: 2 }));
+console.log(solution({ b: 1, c: 2, e: 3 }));
+console.log(solution({}));
