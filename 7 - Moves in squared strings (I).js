0c0f6aa4ec468402676ea6b0317e4ec4e15b415b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-18 09:33:53 +0100

    added test cases

--- 7 - Moves in squared strings (I).js
+++ 7 - Moves in squared strings (I).js
@@ -38,6 +38,19 @@
 // Bash Note:
 // The input strings are separated by , instead of \n. The output strings should be separated by \r instead of \n. See "Sample Tests".
 
+// Test Cases:
+// describe("opstrings",function() {
+
+//     it("Basic tests vertMirror", function() {
+//     testing(oper(vertMirror, "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"), "QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw");
+//     testing(oper(vertMirror, "IzOTWE\nkkbeCM\nWuzZxM\nvDddJw\njiJyHF\nPVHfSx"), "EWTOzI\nMCebkk\nMxZzuW\nwJddDv\nFHyJij\nxSfHVP");
+//   })
+//   it("Basic tests horMirror", function() {
+//     testing(oper(horMirror, "lVHt\nJVhv\nCSbg\nyeCt"), "yeCt\nCSbg\nJVhv\nlVHt");
+//     testing(oper(horMirror, "njMK\ndbrZ\nLPKo\ncEYz"), "cEYz\nLPKo\ndbrZ\nnjMK");
+//   })
+// })
+
 function vertMirror(strng) {
   // Your code
 }
