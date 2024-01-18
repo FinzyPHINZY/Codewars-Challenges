223ad5ef3ddf3adb96c24b254b6155729cd0336c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-18 09:39:52 +0100

    aded test cases

--- 7 - Flatten.js
+++ 7 - Flatten.js
@@ -7,6 +7,36 @@
 // flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
 // flatten([[[1,2,3]]]) // => [[1,2,3]]
 
+//  Test Cases:
+
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertSimilar(flatten([]), []);
+//   Test.assertSimilar(flatten([1, 2, 3]), [1, 2, 3]);
+//   Test.assertSimilar(flatten([[1, 2, 3], ["a", "b", "c"], [1, 2, 3]]), [1, 2, 3, "a", "b", "c", 1, 2, 3]);
+//   Test.assertSimilar(flatten([[3, 4, 5], [[9, 9, 9]], ["a,b,c"]]), [3, 4, 5, [9, 9, 9], "a,b,c"]);
+//   Test.assertSimilar(flatten([[[3], [4], [5]], [9], [9], [8], [[1, 2, 3]]]), [[3], [4], [5], 9, 9, 8, [1, 2, 3]]);
+//   });
+//   });
+
 var flatten = function (array) {
   // TODO: Program me
 };
+
+console.log(flatten([]));
+console.log(flatten([1, 2, 3]));
+console.log(
+  flatten([
+    [1, 2, 3],
+    ["a", "b", "c"],
+    [1, 2, 3],
+  ]),
+  [1, 2, 3, "a", "b", "c", 1, 2, 3]
+);
+console.log(flatten([[3, 4, 5], [[9, 9, 9]], ["a,b,c"]]), [
+  3,
+  4,
+  5,
+  [9, 9, 9],
+  "a,b,c",
+]);
