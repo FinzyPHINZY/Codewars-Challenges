f0f1cdee059f6fafb0ee666e960f2ae316c8e9da
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-19 11:15:01 +0100

    added test cases

--- 7 - Number Of Occurrences.js
+++ 7 - Number Of Occurrences.js
@@ -12,6 +12,18 @@
 // arr.numberOfOccurrences(2) === 2;
 // arr.numberOfOccurrences(3) === 1;
 
+// Test Cases:
+
+// describe("Number of Occurrences", () => {
+//     it("Sample tests", () => {
+//       var arr = [0, 1, 2, 2, 3];
+//       assert.strictEqual(arr.slice().numberOfOccurrences(0), 1, `Incorrect answer for 0, arr = [0, 1, 2, 2, 3]`);
+//       assert.strictEqual(arr.slice().numberOfOccurrences(4), 0, `Incorrect answer for 4, arr = [0, 1, 2, 2, 3]`);
+//       assert.strictEqual(arr.slice().numberOfOccurrences(2), 2, `Incorrect answer for 2, arr = [0, 1, 2, 2, 3]`);
+//       assert.strictEqual(arr.slice().numberOfOccurrences(3), 1, `Incorrect answer for 3, arr = [0, 1, 2, 2, 3]`);
+//     });
+//   });
+
 Object.defineProperty(Array.prototype, "numberOfOccurrences", {
   value: function numberOfOccurrences(element) {},
 });
