51a79053d7f3ed16a020309ff7918fcc89654e82
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-19 11:19:56 +0100

    added test cases

--- 7 - Reverse a Number.js
+++ 7 - Reverse a Number.js
@@ -10,6 +10,27 @@
 // -456 -> -654
 // 1000 ->    1
 
+// Test Cases:
+// describe("Reverse a Number", () =>{
+//     it("Fixed Tests", () => {
+//       assert.strictEqual(reverseNumber(123), 321)
+//       assert.strictEqual(reverseNumber(-123), -321, 'Negative Numbers should be preserved')
+//       assert.strictEqual(reverseNumber(1000), 1, 'Should handle numbers ending with "0"')
+//       assert.strictEqual(reverseNumber(4321234), 4321234)
+//       assert.strictEqual(reverseNumber(5), 5)
+//       assert.strictEqual(reverseNumber(0), 0)
+//       assert.strictEqual(reverseNumber(98989898), 89898989)
+//     });
+//   });
+
 function reverseNumber(n) {
   return 0;
 }
+
+console.log(reverseNumber(123));
+console.log(reverseNumber(-123));
+console.log(reverseNumber(1000));
+console.log(reverseNumber(4321234));
+console.log(reverseNumber(5));
+console.log(reverseNumber(0));
+console.log(reverseNumber(98989898));
