91f0e8d19c3417e4026b4b443cd8dc78954113df
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-21 12:07:00 +0100

    added different solution to kata

--- 6 - Data Reverse.js
+++ 6 - Data Reverse.js
@@ -33,3 +33,11 @@ function dataReverse(data) {
 
   return reversedData;
 }
+
+const dataReverse = (data) => {
+  const bytes = [];
+  for (let i = 0; i < data.length; i += 8) {
+    bytes.unshift(...data.slice(i, i + 8));
+  }
+  return bytes;
+};
