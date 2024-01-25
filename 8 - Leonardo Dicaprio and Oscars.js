26728e5219b81955e82b563007679811edd743ac
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-25 09:38:03 +0100

    added test cases

--- 8 - Leonardo Dicaprio and Oscars.js
+++ 8 - Leonardo Dicaprio and Oscars.js
@@ -9,6 +9,22 @@
 // if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
 // if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
 // if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
-// if it was over 88 you should return "Leo got one already!"
+// if it was over 88 you should return "Leo got one already!"\
+
+// Test Cases:
+
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(leo(89), "Leo got one already!");
+    Test.assertEquals(leo(88), "Leo finally won the oscar! Leo is happy");
+    Test.assertEquals(leo(87), "When will you give Leo an Oscar?");
+    Test.assertEquals(leo(86), "Not even for Wolf of wallstreet?!");
+  });
+});
 
 function leo(oscar) {}
+
+console.log(leo(89));
+console.log(leo(88));
+console.log(leo(87));
+console.log(leo(86));
