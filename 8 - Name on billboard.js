b5c8888622443e6f145b7c8e297b5b1625439b31
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-26 09:49:02 +0100

    added solution to kata

--- 8 - Name on billboard.js
+++ 8 - Name on billboard.js
@@ -8,27 +8,31 @@
 // If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a character).
 
 // Test Cases:
-describe("Basic tests", () => {
-  Test.assertEquals(billboard("Jeong-Ho Aristotelis"), 600);
-  Test.assertEquals(billboard("Abishai Charalampos"), 570);
-  Test.assertEquals(billboard("Idwal Augustin"), 420);
-  Test.assertEquals(billboard("Hadufuns John", 20), 260);
-  Test.assertEquals(billboard("Zoroaster Donnchadh"), 570);
-  Test.assertEquals(billboard("Claude Miljenko"), 450);
-  Test.assertEquals(billboard("Werner Vígi", 15), 165);
-  Test.assertEquals(billboard("Anani Fridumar"), 420);
-  Test.assertEquals(billboard("Paolo Oli"), 270);
-  Test.assertEquals(billboard("Hjalmar Liupold", 40), 600);
-  Test.assertEquals(billboard("Simon Eadwulf"), 390);
-});
+// describe("Basic tests", () => {
+//   Test.assertEquals(billboard("Jeong-Ho Aristotelis"), 600);
+//   Test.assertEquals(billboard("Abishai Charalampos"), 570);
+//   Test.assertEquals(billboard("Idwal Augustin"), 420);
+//   Test.assertEquals(billboard("Hadufuns John", 20), 260);
+//   Test.assertEquals(billboard("Zoroaster Donnchadh"), 570);
+//   Test.assertEquals(billboard("Claude Miljenko"), 450);
+//   Test.assertEquals(billboard("Werner Vígi", 15), 165);
+//   Test.assertEquals(billboard("Anani Fridumar"), 420);
+//   Test.assertEquals(billboard("Paolo Oli"), 270);
+//   Test.assertEquals(billboard("Hjalmar Liupold", 40), 600);
+//   Test.assertEquals(billboard("Simon Eadwulf"), 390);
+// });
 
-function billboard(name, price = 30) {}
+function billboard(name, price) {
+  return name
+    .split("")
+    .map((e) => (e = price ? price : 30))
+    .reduce((a, b) => a + b, 0);
+}
 
 console.log(billboard("Jeong-Ho Aristotelis"));
-
 console.log(billboard("Abishai Charalampos"));
 console.log(billboard("Idwal Augustin"));
-console.log(billboard("Hadufuns John"));
+console.log(billboard("Hadufuns John", 20));
 console.log(billboard("Zoroaster Donnchadh"));
 console.log(billboard("Claude Miljenko"));
 console.log(billboard("Werner Vígi"));
