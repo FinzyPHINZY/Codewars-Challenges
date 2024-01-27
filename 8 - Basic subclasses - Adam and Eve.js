05f537ffa5f19cb5fd08e192f167b0d239ffda0f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-27 16:01:03 +0100

    added test cases

--- 8 - Basic subclasses - Adam and Eve.js
+++ 8 - Basic subclasses - Adam and Eve.js
@@ -5,6 +5,18 @@
 
 // You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
 
+// Test Cases
+describe("GodTest", function () {
+  it("Adam should be a Man", function () {
+    let humans = God.create();
+    Test.assertEquals(
+      humans[0] instanceof Man,
+      true,
+      "Expected Adam to be a Man"
+    );
+  });
+});
+
 class God {
   /**
    * @returns Human[]
@@ -13,4 +25,3 @@ class God {
     // code
   }
 }
-// code
