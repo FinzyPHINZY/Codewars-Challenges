0e41fcde4e44d8294f94cc1ad9a10d82c91f9d0f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-27 15:42:49 +0100

    add test cases

--- 8 -  Find Multiples of a Number.js
+++ 8 -  Find Multiples of a Number.js
@@ -4,6 +4,22 @@
 
 // For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
 
+// Test Cases:
+// const { assert } = require("chai");
+// it("Basic Tests", () => {
+//   assert.sameOrderedMembers(findMultiples(5, 25), [5, 10, 15, 20, 25]);
+//   assert.sameOrderedMembers(findMultiples(1, 2), [1, 2]);
+//   assert.sameOrderedMembers(findMultiples(5, 7), [5]);
+//   assert.sameOrderedMembers(findMultiples(4, 27), [4, 8, 12, 16, 20, 24]);
+//   assert.sameOrderedMembers(findMultiples(11, 54), [11, 22, 33, 44]);
+// });
+
 function findMultiples(integer, limit) {
   //your code here
 }
+
+console.log(findMultiples(5, 25));
+console.log(findMultiples(1, 2));
+console.log(findMultiples(5, 7));
+console.log(findMultiples(4, 27));
+console.log(findMultiples(11, 54));
