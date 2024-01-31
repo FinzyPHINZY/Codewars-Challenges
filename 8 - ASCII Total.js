7ea803149a609454e749dfe5bfa9de48523fb97b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-01-31 12:55:11 +0100

    added test cases

--- 8 - ASCII Total.js
+++ 8 - ASCII Total.js
@@ -8,6 +8,28 @@
 // uniTotal("a") == 97
 // uniTotal("aaa") == 291
 
+// Test Cases;
+
+// it("sample tests", () => {
+//   doTest("", 0);
+//   doTest("a", 97);
+//   doTest("b", 98);
+//   doTest("c", 99);
+//   doTest("d", 100);
+//   doTest("e", 101);
+//   doTest("aaa", 291);
+//   doTest("Mary Had A Little Lamb", 1873);
+// });
+
 function uniTotal(string) {
   // total up dem unicodes!
 }
+
+console.log(doTest("", 0));
+console.log(doTest("a", 97));
+console.log(doTest("b", 98));
+console.log(doTest("c", 99));
+console.log(doTest("d", 100));
+console.log(doTest("e", 101));
+console.log(doTest("aaa", 291));
+console.log(doTest("Mary Had A Little Lamb", 1873));
