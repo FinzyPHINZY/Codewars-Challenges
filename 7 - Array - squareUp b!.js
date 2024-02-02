85bfb3947b5d61f0f5112baf3b99c114bce0febf
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-02 10:57:44 +0100

    added test cases

--- 7 - Array - squareUp b!.js
+++ 7 - Array - squareUp b!.js
@@ -10,6 +10,19 @@
 // squareUp(4) => [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]
 // n<=1000.
 
+// Test Cases:
+// describe("SolutionTest", function(){
+//     it("Basic tests", function(){
+//         Test.assertDeepEquals(squareUp(4), [0, 0, 0, 1, 0, 0, 2, 1, 0, 3, 2, 1, 4, 3, 2, 1]);
+//         Test.assertDeepEquals(squareUp(9), [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 2, 1, 0, 0, 0, 0, 0, 0, 3, 2, 1, 0, 0, 0, 0, 0, 4, 3, 2, 1, 0, 0, 0, 0, 5, 4, 3, 2, 1, 0, 0, 0, 6, 5, 4, 3, 2, 1, 0, 0, 7, 6, 5, 4, 3, 2, 1, 0, 8, 7, 6, 5, 4, 3, 2, 1, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+//         Test.assertDeepEquals(squareUp(1), [1]);
+//     });
+// });
+
 function squareUp(n) {
   return []; // Do your magic!
 }
+
+console.log(squareUp(4));
+console.log(squareUp(9));
+console.log(squareUp(1));
