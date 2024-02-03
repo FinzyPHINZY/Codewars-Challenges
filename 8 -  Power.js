cc5cac27167ede66f145613f173726e846682743
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-03 20:08:54 +0100

    added starter code

--- 8 -  Power.js
+++ 8 -  Power.js
@@ -8,3 +8,8 @@
 // numberToPower(2, 3)  // -> 8 ( = 2 * 2 * 2 );
 // numberToPower(10, 6) // -> 1000000;
 // Note: Math.pow and some other Math functions like eval() and ** are disabled.
+
+function numberToPower(number, power) {
+  console.info(Math.log2(1024));
+  // Code here
+}
