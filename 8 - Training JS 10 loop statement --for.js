f40700df8187013ea031fe6e80cb15ab47cd1b59
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-05 09:01:53 +0100

    added test cases

--- 8 - Training JS 10 loop statement --for.js
+++ 8 - Training JS 10 loop statement --for.js
@@ -68,3 +68,10 @@ function pickIt(arr) {
 
   return [odd, even];
 }
+
+console.log(pickIt([1, 2]));
+console.log(pickIt([1, 2, 3]));
+console.log(pickIt([3, 2, 1]));
+console.log(pickIt([10, 20, 30]));
+console.log(pickIt([11, 21, 31]));
+console.log(pickIt([8, 1, 5, 4, 6, 1, 1]));
