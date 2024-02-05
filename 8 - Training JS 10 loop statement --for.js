a0741a34251b648920516f696c59570675f14fe8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-05 09:07:28 +0100

    added solution to kata

--- 8 - Training JS 10 loop statement --for.js
+++ 8 - Training JS 10 loop statement --for.js
@@ -62,9 +62,12 @@
 //   });
 
 function pickIt(arr) {
-  var odd = [],
-    even = [];
+  let odd = [];
+  let even = [];
   //coding here
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] % 2 === 0 ? even.push(arr[i]) : odd.push(arr[i]);
+  }
 
   return [odd, even];
 }
