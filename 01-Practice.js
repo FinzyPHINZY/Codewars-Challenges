3edb9a85e3aa737e37ae2976bfe089bc01ca2c6f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-09 10:45:08 +0100

    added series sum practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -36,3 +36,22 @@ function partlist(arr) {
 }
 
 console.log(partlist(["az", "toto", "picaro", "zone", "kiwi"]));
+
+// //////////////////////////////////////////////////////////////////////////////////
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+// If the given value is 0 then it should return 0.00
+// You will only be given Natural Numbers as arguments.
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+function seriesSum(num) {
+  for (let i = 0; i < num; i++) {
+    console.log(1 + 1 / 1 + 3);
+
+    console.log(1 + 1 + 3);
+  }
+}
