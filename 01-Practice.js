a3fbf79351ff4951478fd4bf873efe98a6d5b09b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-09 11:39:44 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -138,4 +138,15 @@ function generateShape(n) {
 
 function towerBuilder(nFloors) {
   // build here
+
+  let tower = [];
+  for (let i = 0; i < nFloors; i++) {
+    const spaces = " ".repeat(nFloors - i + 1);
+    const stars = "*".repeat(2 * i + 1);
+    tower.push(spaces + stars + spaces);
+  }
+
+  return tower;
 }
+
+console.log(towerBuilder(16));
