6698759c37ff3290034a5919a51b09ca5cd4bd0c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-09 11:30:42 +0100

    added solution to kata

--- 01-Practice.js
+++ 01-Practice.js
@@ -77,5 +77,36 @@ function solution(str) {
   return result;
 }
 
-console.log(solution("abc"));
-console.log(solution("abcdef"));
+// console.log(solution("abc"));
+// console.log(solution("abcdef"));
+
+// DESCRIPTION:
+// Task
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+// Examples
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// Test Cases:
+// describe("Sample Tests", () => {
+//   it("should pass some sample tests", () => {
+//     assert.deepEqual(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+//     assert.deepEqual(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+//     assert.deepEqual(sortArray([]), []);
+//   });
+// });
+
+function sortArray(array) {
+  const odds = array.filter((num) => num % 2 !== 0).sort();
+
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    array[i] % 2 === 0 ? result.push(array[i]) : result.push(odds.shift());
+  }
+
+  return result;
+}
+
+console.log(sortArray([5, 3, 2, 8, 1, 4]));
