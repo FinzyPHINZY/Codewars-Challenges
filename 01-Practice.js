5ab4e9ca84050f07f17a673eee78c2cad38809eb
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-09 10:24:07 +0100

    added practice file

---
+++ 01-Practice.js
@@ -0,0 +1,3 @@
+"use strict";
+
+// This file contains the katas i found tough. It will include the solutions i did without any help and improved solutions that arise to the wider scope of my knowledge as time goes on.
