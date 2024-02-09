8a7acccee8cb9a6183af1db04e782e319519f051
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-09 11:34:25 +0100

    added new kata practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -115,4 +115,27 @@ function generateShape(n) {
   return ("#".repeat(n) + "\n").repeat(n);
 }
 
-console.log(generateShape(3));
+// console.log(generateShape(3));
+
+// DESCRIPTION:
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+// For example, a tower with 3 floors looks like this:
+// [
+//   "  *  ",
+//   " *** ",
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+[
+  "     *     ",
+  "    ***    ",
+  "   *****   ",
+  "  *******  ",
+  " ********* ",
+  "***********",
+];
+
+function towerBuilder(nFloors) {
+  // build here
+}
