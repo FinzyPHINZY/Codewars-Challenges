df9daeafaba218e4b7ad19cf8dbbfef611b556a5
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-12 11:04:12 +0100

    added solution to kata

--- 6 - Array Helpers.js
+++ 6 - Array Helpers.js
@@ -12,10 +12,57 @@
 // odd() must return an array of all odd numbers
 // Note: the original array must not be changed in any case!
 
-Example;
+// Example;
+// var numbers = [1, 2, 3, 4, 5];
+
+// numbers.square(); // must return [1, 4, 9, 16, 25]
+// numbers.cube(); // must return [1, 8, 27, 64, 125]
+// numbers.average(); // must return 3
+// numbers.sum(); // must return 15
+// numbers.even(); // must return [2, 4]
+// numbers.odd(); // must return [1, 3, 5]
+
+// Test Cases:
+// escribe("Sample Tests", () => {
+//   it("test", () => {
+//     const numbers = [1, 2, 3, 4, 5];
+//     assert.deepEqual(numbers.square(), [1, 4, 9, 16, 25]);
+//     assert.deepEqual(numbers.cube(), [1, 8, 27, 64, 125]);
+//     assert.strictEqual(numbers.sum(), 15, "Wrong sum");
+//     assert.strictEqual(numbers.average(), 3, "Wrong average");
+//     assert.deepEqual(numbers.even(), [2, 4], "Wrong result for even()");
+//     assert.deepEqual(numbers.odd(), [1, 3, 5], "Wrong result for odd()");
+//   });
+// });
+
+Array.prototype.square = function () {
+  return this.map((num) => num * num);
+};
+
+Array.prototype.cube = function () {
+  return this.map((num) => num * num * num);
+};
+
+Array.prototype.average = function () {
+  if (this.length === 0) return NaN;
+  return this.reduce((acc, num) => acc + num, 0) / this.length;
+};
+
+Array.prototype.sum = function () {
+  return this.reduce((acc, num) => acc + num, 0);
+};
+
+Array.prototype.even = function () {
+  return this.filter((num) => num % 2 === 0);
+};
+
+Array.prototype.odd = function () {
+  return this.filter((num) => num % 2 !== 0);
+};
+
 var numbers = [1, 2, 3, 4, 5];
 
-numbers.square(); // must return [1, 4, 9, 16, 25]
+console.log(numbers.square()); // must return [1, 4, 9, 16, 25]
 numbers.cube(); // must return [1, 8, 27, 64, 125]
 numbers.average(); // must return 3
 numbers.sum(); // must return 15
