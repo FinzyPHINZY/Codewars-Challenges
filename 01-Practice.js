19b24c65beb4729db3c64e8c26814e1f01343f8f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-12 10:34:13 +0100

    added solution to kata

--- 01-Practice.js
+++ 01-Practice.js
@@ -297,4 +297,15 @@ function persistence(num) {
 // 67890 is the greatest sequence of 5 consecutive digits.
 // Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
 
-function solution(digits) {}
+function solution(digits) {
+  if (digits.length <= 5) return digits;
+  digits = digits.toString();
+
+  let arr = [];
+  for (let i = 0; i <= digits.length - 5; i++) {
+    arr.push(digits.slice(i, i + 5));
+  }
+  return Math.max(...arr.map(Number));
+}
+
+console.log(solution(1234567890));
