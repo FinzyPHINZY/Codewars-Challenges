2d95c7a595562a0d11c75b9b15e8a32af577df3d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-12 11:29:21 +0100

    added starter code

--- 7 -Spacify.js
+++ 7 -Spacify.js
@@ -4,3 +4,7 @@
 // Modify the spacify function so that it returns the given string with spaces inserted between each character.
 
 // spacify("hello world") // returns "h e l l o   w o r l d"
+
+function spacify(str) {
+  // return
+}
