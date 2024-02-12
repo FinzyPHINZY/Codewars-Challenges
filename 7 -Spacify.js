0a368718bb64166558257b78b0fbbddc2d61264f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-12 11:30:43 +0100

    added new kata

--- 7 -Spacify.js
+++ 7 -Spacify.js
@@ -5,6 +5,17 @@
 
 // spacify("hello world") // returns "h e l l o   w o r l d"
 
+// Test Cases:
+// describe("spacify", function () {
+//   it("basic tests", function () {
+//     assert.equal(spacify("hello world"), "h e l l o   w o r l d");
+//     assert.equal(spacify("12345"), "1 2 3 4 5");
+//   });
+// });
+
 function spacify(str) {
   // return
 }
+
+console.log(spacify("hello world"));
+console.log(spacify("12345"));
