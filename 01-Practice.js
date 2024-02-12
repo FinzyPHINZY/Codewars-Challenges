f3f01bd14f940afc7476fad95b520200ef969dca
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-12 10:23:21 +0100

    added practice description

--- 01-Practice.js
+++ 01-Practice.js
@@ -287,3 +287,14 @@ function persistence(num) {
 // console.log(persistence(39));
 // console.log(persistence(999));
 // console.log(persistence(4));
+
+// DESCRIPTION:
+// In the following 6 digit number:
+// 283910
+// 91 is the greatest sequence of 2 consecutive digits.
+// In the following 10 digit number:
+// 1234567890
+// 67890 is the greatest sequence of 5 consecutive digits.
+// Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+
+function solution(digits) {}
