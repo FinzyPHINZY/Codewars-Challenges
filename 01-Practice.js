9b7ecf9cacd1f86fd6c467e7cfb29c1c5367c03d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-12 10:09:11 +0100

    added test cases

--- 01-Practice.js
+++ 01-Practice.js
@@ -247,3 +247,8 @@ function unluckyDays(year) {
 function digitalRoot(n) {
   // ...
 }
+
+console.log(digitalRoot(16));
+console.log(digitalRoot(942));
+console.log(digitalRoot(132189));
+console.log(digitalRoot(493193));
