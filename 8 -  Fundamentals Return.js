7907be45895594a650f5ec518d4495f8b97c61d8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-14 09:47:02 +0100

    added starter code

--- 8 -  Fundamentals Return.js
+++ 8 -  Fundamentals Return.js
@@ -17,3 +17,27 @@
 // subtraction = subt
 
 // Note: All math operations will be: a (operation) b
+
+function add(a, b) {
+  return;
+}
+
+function divide(a, b) {
+  return;
+}
+
+function multiply(a, b) {
+  return;
+}
+
+function mod(a, b) {
+  return;
+}
+
+function exponent(a, b) {
+  return;
+}
+
+function subt(a, b) {
+  return;
+}
