ed42dc1213fcfe636906cce0a4e049179376b0db
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-14 09:59:23 +0100

    added solution to kata

--- 8 - Regex count lowercase letters.js
+++ 8 - Regex count lowercase letters.js
@@ -16,6 +16,32 @@
 
 // "abcdefghijklmnopqrstuvwxyz" ===> 26
 
+// Test Cases;
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(lowercaseCount("abc"), 3);
+//   Test.assertEquals(lowercaseCount("abcABC123"), 3);
+//   Test.assertEquals(lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"), 3);
+//   Test.assertEquals(lowercaseCount(""), 0)
+//   Test.assertEquals(lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"), 0)
+//   Test.assertEquals(lowercaseCount("abcdefghijklmnopqrstuvwxyz"), 26);
+//     });
+//   });
+
 function lowercaseCount(str) {
   //How many?
+  let count = 0;
+  let letters = "abcdefghijklmnopqrstuvwxyz";
+
+  for (let i = 0; i < str.length; i++) {
+    if (letters.includes(str[i])) count++;
+  }
+  return count;
 }
+
+console.log(lowercaseCount("abc"));
+console.log(lowercaseCount("abcABC123"));
+console.log(lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~"));
+console.log(lowercaseCount(""));
+console.log(lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|':;?/>.<,~"));
+console.log(lowercaseCount("abcdefghijklmnopqrstuvwxyz"));
