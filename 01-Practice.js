248230925b11cef2cb211aa6d93853c62a870aec
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-14 09:23:39 +0100

    added description and starter code for practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -375,5 +375,25 @@ function stray(arr) {
   return arr[0] === arr[1] ? arr[arr.length - 1] : arr[1];
 }
 
-console.log(stray([1, 1, 2]));
-console.log(stray([17, 17, 3, 17, 17, 17, 17]));
+// console.log(stray([1, 1, 2]));
+// console.log(stray([17, 17, 3, 17, 17, 17, 17]));
+
+// DESCRIPTION:
+// John has invited some friends. His list is:
+
+// s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+// Could you make a program that
+
+// makes this string uppercase
+// gives it sorted in alphabetical order by last name.
+// When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+// So the result of function meeting(s) will be:
+
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+// It can happen that in two distinct families with the same family name two people have the same first name too.
+
+// Notes
+// You can see another examples in the "Sample tests".
+
+function meeting(s) {}
