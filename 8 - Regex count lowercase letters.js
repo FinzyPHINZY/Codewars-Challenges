d197504ca6263d76d57c6e5b565b94607c8ba7ba
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-14 09:53:03 +0100

    added kata description

--- 8 - Regex count lowercase letters.js
+++ 8 - Regex count lowercase letters.js
@@ -1,1 +1,17 @@
 "use strict";
+
+// DESCRIPTION:
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// "abc" ===> 3
+
+// "abcABC123" ===> 3
+
+// "abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 3
+
+// "" ===> 0;
+
+// "ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" ===> 0
+
+// "abcdefghijklmnopqrstuvwxyz" ===> 26
