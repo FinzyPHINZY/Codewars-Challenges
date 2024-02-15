76838d73119e1439ab236ccc7310d7036724cdcc
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-15 12:07:26 +0100

    added test cases

--- 8 -get ascii value of character.js
+++ 8 -get ascii value of character.js
@@ -4,6 +4,19 @@
 
 // For the ASCII table you can refer to http://www.asciitable.com/
 
+// Test Cases:
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(getASCII("A"), 65);
+    Test.assertEquals(getASCII(" "), 32);
+    Test.assertEquals(getASCII("!"), 33);
+  });
+});
+
 function getASCII(c) {
   // ...
 }
+
+console.log(getASCII("A"));
+console.log(getASCII(" "));
+console.log(getASCII("!"));
