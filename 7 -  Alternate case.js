38b87206a4eb809ed0b1d1fc45056b78fd461d55
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-15 14:01:51 +0100

    added solution to kata

--- 7 -  Alternate case.js
+++ 7 -  Alternate case.js
@@ -13,7 +13,16 @@
 // });
 
 function alternateCase(s) {
-  return "";
+  let output = [];
+
+  s = s.split("").forEach((char) => {
+    if (char === char.toUpperCase()) output.push(char.toLowerCase());
+    else {
+      output.push(char.toUpperCase());
+    }
+  });
+
+  return output.join("");
 }
 
 console.log(alternateCase("abc"));
