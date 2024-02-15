827826e89df7dc9294d1865866c8a40af324a557
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-15 13:31:02 +0100

    added test cases

--- 7 -  Alternate case.js
+++ 7 -  Alternate case.js
@@ -3,6 +3,19 @@
 // DESCRIPTION:
 // Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
 
+// Test Cases:
+// describe("Alternate case", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(alternateCase("abc"), "ABC");
+//     assert.strictEqual(alternateCase("ABC"), "abc");
+//     assert.strictEqual(alternateCase("Hello World"), "hELLO wORLD");
+//   });
+// });
+
 function alternateCase(s) {
   return "";
 }
+
+console.log(alternateCase("abc"));
+console.log(alternateCase("ABC"));
+console.log(alternateCase("Hello World"));
