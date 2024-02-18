c8c8284ffd202280bb7800fb282b8baf33ae38f5
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-18 14:49:03 +0100

    added solution to kata

--- 8 - UEFA EURO 2016.js
+++ 8 - UEFA EURO 2016.js
@@ -8,6 +8,13 @@
 
 function uefaEuro2016(teams, scores) {
   // your code...
+  // check if game was a draw or win
+  if (scores[0] === scores[1])
+    return `At match ${teams[0]} - ${teams[1]}, teams played draw.`;
+  // manipulate output statement
+  return scores[0] > scores[1]
+    ? `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
+    : `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`;
 }
 
 console.log(uefaEuro2016(["Germany", "Ukraine"], [2, 0]));
