cc2e10065c72822c04d5f827bb1b7f03c4dc179c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-18 14:39:12 +0100

    added starter code

--- 8 - UEFA EURO 2016.js
+++ 8 - UEFA EURO 2016.js
@@ -5,3 +5,7 @@
 // uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
 // uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
 // uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+function uefaEuro2016(teams, scores) {
+  // your code...
+}
