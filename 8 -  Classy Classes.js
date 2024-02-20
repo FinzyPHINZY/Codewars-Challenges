94054b49b8294fcc0e52f3d91c7d04b2db2ff813
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-20 09:40:47 +0100

    added solution to kata

--- 8 -  Classy Classes.js
+++ 8 -  Classy Classes.js
@@ -8,16 +8,22 @@
 // Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
 
 // Test Cases:
-describe("Tests", () => {
-  it("test", () => {
-    var john = new Person("john", 34);
-    Test.assertEquals(john.info, "johns age is 34");
-  });
-});
+// describe("Tests", () => {
+//   it("test", () => {
+//     var john = new Person("john", 34);
+//     Test.assertEquals(john.info, "johns age is 34");
+//   });
+// });
 
 class Person {
-  constructor() {}
+  constructor(name, age) {
+    (this._name = name), (this._age = age);
+  }
+
+  get info() {
+    return `${this._name}s age is ${this._age}`;
+  }
 }
 
 const john = new Person("john", 34);
-console.log(john);
+console.log(john._name, john._age);
