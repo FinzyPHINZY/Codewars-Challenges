29e61d704ee9973386eb04f485a574456d6f9c7c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-20 09:23:18 +0100

    added test cases

--- 8 -  Classy Classes.js
+++ 8 -  Classy Classes.js
@@ -7,6 +7,17 @@
 // Task
 // Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
 
+// Test Cases:
+describe("Tests", () => {
+  it("test", () => {
+    var john = new Person("john", 34);
+    Test.assertEquals(john.info, "johns age is 34");
+  });
+});
+
 class Person {
   constructor() {}
 }
+
+const john = new Person("john", 34);
+console.log(john);
