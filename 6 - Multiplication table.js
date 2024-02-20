94e70ab41b275429426b0ea195baa4ddb202e511
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-20 09:19:49 +0100

    added solution to kata

--- 6 - Multiplication table.js
+++ 6 - Multiplication table.js
@@ -23,8 +23,19 @@
 //   });
 // });
 
-multiplicationTable = function (size) {
+const multiplicationTable = function (size) {
   // insert code here
+  let result = [];
+
+  for (let i = 1; i <= size; i++) {
+    let row = [];
+    for (let j = 1; j <= size; j++) {
+      row.push(i * j);
+    }
+    result.push(row);
+  }
+
+  return result;
 };
 
 console.log(multiplicationTable(3));
