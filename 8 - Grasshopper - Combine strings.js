0caa53b2baf0235d8cc8c09d992adee31852f010
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-21 08:59:06 +0100

    added kata description

--- 8 - Grasshopper - Combine strings.js
+++ 8 - Grasshopper - Combine strings.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// DESCRIPTION:
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
