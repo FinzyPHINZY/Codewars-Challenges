043c08a9027b759e5f2c48bc93b9052af92e4968
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-21 09:49:17 +0100

    added test cases

--- 8 - Parse float.js
+++ 8 - Parse float.js
@@ -3,6 +3,15 @@
 // DESCRIPTION:
 // Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
 
+// Test Cases
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertEquals(parseF("1"), 1.0);
+//   });
+// });
+
 function parseF(s) {
   return null;
 }
+
+console.log(parseF("1"));
