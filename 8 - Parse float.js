b887f8fb552e8e7023d7b6905a66f61cbe52fa76
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-21 09:45:02 +0100

    added kata description

--- 8 - Parse float.js
+++ 8 - Parse float.js
@@ -1,1 +1,4 @@
 "use strict";
+
+// DESCRIPTION:
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
