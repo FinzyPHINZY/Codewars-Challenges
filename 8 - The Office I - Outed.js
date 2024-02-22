af9712ada6028fa1b5546a4a9a371ea521b7cf9e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-22 09:00:01 +0100

    added test cases
    ;

--- 8 - The Office I - Outed.js
+++ 8 - The Office I - Outed.js
@@ -10,4 +10,69 @@
 
 // Note that your boss is in the room (boss), their score is worth double it's face value (but they are still just one person!).
 
+// Test Cases:
+
+// describe("Example tests",() =>{
+//     it("Should pass Example tests",() =>{
+//       assert.strictEqual(outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura'), 'Get Out Now!');
+//       assert.strictEqual(outed({'tim':1, 'jim':3, 'randy':9, 'sandy':6, 'andy':7, 'katie':6, 'laura':9, 'saajid':9, 'alex':9, 'john':9, 'mr':8}, 'katie'), 'Nice Work Champ!');
+//       assert.strictEqual(outed({'tim':2, 'jim':4, 'randy':0, 'sandy':5, 'andy':8, 'katie':6, 'laura':2, 'saajid':2, 'alex':3, 'john':2, 'mr':8}, 'john'), 'Get Out Now!');
+//     })
+//   })
+
 function outed(meet, boss) {}
+
+console.log(
+  outed(
+    {
+      tim: 0,
+      jim: 2,
+      randy: 0,
+      sandy: 7,
+      andy: 0,
+      katie: 5,
+      laura: 1,
+      saajid: 2,
+      alex: 3,
+      john: 2,
+      mr: 0,
+    },
+    "laura"
+  )
+);
+console.log(
+  outed(
+    {
+      tim: 1,
+      jim: 3,
+      randy: 9,
+      sandy: 6,
+      andy: 7,
+      katie: 6,
+      laura: 9,
+      saajid: 9,
+      alex: 9,
+      john: 9,
+      mr: 8,
+    },
+    "katie"
+  )
+);
+console.log(
+  outed(
+    {
+      tim: 2,
+      jim: 4,
+      randy: 0,
+      sandy: 5,
+      andy: 8,
+      katie: 6,
+      laura: 2,
+      saajid: 2,
+      alex: 3,
+      john: 2,
+      mr: 8,
+    },
+    "john"
+  )
+);
