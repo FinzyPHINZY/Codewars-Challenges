5fd40736274a626967af5135620a30bf4e4f56d9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-24 12:25:58 +0100

    added test cases

--- 7 -  Sum of Odd Cubed Numbers.js
+++ 7 -  Sum of Odd Cubed Numbers.js
@@ -2,8 +2,22 @@
 
 // Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
 
+// Test Cases:
+
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(cubeOdd([1, 2, 3, 4]), 28);
+    Test.assertEquals(cubeOdd([-3, -2, 2, 3]), 0);
+    Test.assertEquals(cubeOdd(["a", 12, 9, "z", 42]), undefined);
+  });
+});
+
 function cubeOdd(arr) {
   // insert code here >.<
 
   return 0;
 }
+
+console.log(cubeOdd([1, 2, 3, 4]));
+console.log(cubeOdd([-3, -2, 2, 3]));
+console.log(cubeOdd(["a", 12, 9, "z", 42]));
