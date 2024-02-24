13f5ff263fc043a23244ece3e1fa9c61a772cca2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-24 12:37:54 +0100

    added solution to kata

--- 7 -  Sum of Odd Cubed Numbers.js
+++ 7 -  Sum of Odd Cubed Numbers.js
@@ -3,19 +3,23 @@
 // Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
 
 // Test Cases:
-
-describe("Tests", () => {
-  it("test", () => {
-    Test.assertEquals(cubeOdd([1, 2, 3, 4]), 28);
-    Test.assertEquals(cubeOdd([-3, -2, 2, 3]), 0);
-    Test.assertEquals(cubeOdd(["a", 12, 9, "z", 42]), undefined);
-  });
-});
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertEquals(cubeOdd([1, 2, 3, 4]), 28);
+//     Test.assertEquals(cubeOdd([-3, -2, 2, 3]), 0);
+//     Test.assertEquals(cubeOdd(["a", 12, 9, "z", 42]), undefined);
+//   });
+// });
 
 function cubeOdd(arr) {
   // insert code here >.<
+  let checkValAintNum = arr.every((value) => typeof value === "number");
 
-  return 0;
+  checkValAintNum
+    ? arr
+        .filter((value) => value % 2 !== 0)
+        .reduce((sum, num) => sum + num * num * num, 0)
+    : undefined;
 }
 
 console.log(cubeOdd([1, 2, 3, 4]));
