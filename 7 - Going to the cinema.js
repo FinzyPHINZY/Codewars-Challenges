45a80c87754b7a37827a8518a8afadfacf77abe2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-25 14:10:11 +0100

    added solution to kata

--- 7 - Going to the cinema.js
+++ 7 - Going to the cinema.js
@@ -24,4 +24,14 @@
 
 function movie(card, ticket, perc) {
   // your code
+  var costA = (n = 0),
+    costB = card;
+  while (Math.ceil(costB) >= costA) {
+    costA += ticket;
+    costB += ticket * Math.pow(perc, ++n);
+  }
+  return n;
 }
+
+console.log(movie(500, 15, 0.9));
+console.log(movie(100, 10, 0.95));
