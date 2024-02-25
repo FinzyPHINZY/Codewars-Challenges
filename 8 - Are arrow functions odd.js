dd90d5392e0214b3b8d01dcd6c32e3fd764a3491
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-25 13:58:49 +0100

    added test cases

--- 8 - Are arrow functions odd.js
+++ 8 - Are arrow functions odd.js
@@ -5,7 +5,22 @@
 // [1, 2, 3, 4, 5]  -->  [1, 3, 5]
 // [2, 4, 6]        -->  []
 
+// Test Cases
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertSimilar(odds([]), [], "Should handle empty array");
+//   Test.assertSimilar(odds([2, 4, 6]), [], "Should handle array with even numbers only");
+//   Test.assertSimilar(odds([1, 3, 5]), [1, 3, 5], "Should handle array with odd numbers only");
+//   Test.assertSimilar(odds([1, 2, 3, 4, 5, 6]), [1, 3, 5], "Should handle mixed array");
+//     });
+//   });
+
 function odds(values) {
   // arrow it
   // return values.filter( => );
 }
+
+console.log(odds([]));
+console.log(odds([2, 4, 6]));
+console.log(odds([1, 3, 5]));
+console.log(odds([1, 2, 3, 4, 5, 6]));
