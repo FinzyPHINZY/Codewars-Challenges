850446c684dbf1b67271a3dd1555ce68d2a475d8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-25 14:17:34 +0100

    added solution to kata

--- 8 - Type of sum.js
+++ 8 - Type of sum.js
@@ -4,23 +4,24 @@
 // Return the type of the sum of the two arguments
 
 // Test Cases:
-describe("Basic tests", () => {
-  Test.assertEquals(typeOfSum(12, 1), "number");
-  Test.assertEquals(typeOfSum("d", 1), "string");
-  Test.assertEquals(typeOfSum(1, "a"), "string");
-  Test.assertEquals(typeOfSum("dd", ""), "string");
-  Test.assertEquals(typeOfSum(true, 1), "number");
-  Test.assertEquals(typeOfSum("s", false), "string");
-  Test.assertEquals(typeOfSum(null, 1), "number");
-  Test.assertEquals(typeOfSum("s", null), "string");
-  Test.assertEquals(typeOfSum(null, undefined), "number");
-  Test.assertEquals(typeOfSum(undefined, "re"), "string");
-  Test.assertEquals(typeOfSum(undefined, true), "number");
-  Test.assertEquals(typeOfSum(null, false), "number");
-});
+// describe("Basic tests", () => {
+//   Test.assertEquals(typeOfSum(12, 1), "number");
+//   Test.assertEquals(typeOfSum("d", 1), "string");
+//   Test.assertEquals(typeOfSum(1, "a"), "string");
+//   Test.assertEquals(typeOfSum("dd", ""), "string");
+//   Test.assertEquals(typeOfSum(true, 1), "number");
+//   Test.assertEquals(typeOfSum("s", false), "string");
+//   Test.assertEquals(typeOfSum(null, 1), "number");
+//   Test.assertEquals(typeOfSum("s", null), "string");
+//   Test.assertEquals(typeOfSum(null, undefined), "number");
+//   Test.assertEquals(typeOfSum(undefined, "re"), "string");
+//   Test.assertEquals(typeOfSum(undefined, true), "number");
+//   Test.assertEquals(typeOfSum(null, false), "number");
+// });
 
 function typeOfSum(a, b) {
   // good luck
+  return typeof (a + b);
 }
 
 console.log(typeOfSum(12, 1));
