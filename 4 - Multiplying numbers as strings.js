21c76b7ede5a6570d4be7eab776e63f1fa865371
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-26 09:08:57 +0100

    added solution to kata

--- 4 - Multiplying numbers as strings.js
+++ 4 - Multiplying numbers as strings.js
@@ -35,8 +35,30 @@
 //     });
 //   });
 
-function multiply(a, b) {}
+function multiply(a, b) {
+  var aa = a.split("").reverse();
+  var bb = b.split("").reverse();
 
+  var stack = [];
+
+  for (var i = 0; i < aa.length; i++) {
+    for (var j = 0; j < bb.length; j++) {
+      var m = aa[i] * bb[j];
+      stack[i + j] = stack[i + j] ? stack[i + j] + m : m;
+    }
+  }
+
+  for (var i = 0; i < stack.length; i++) {
+    var num = stack[i] % 10;
+    var move = Math.floor(stack[i] / 10);
+    stack[i] = num;
+
+    if (stack[i + 1]) stack[i + 1] += move;
+    else if (move != 0) {
+      stack[i + 1] = move;
+    }
+  }
+}
 console.log(multiply("2", "3"));
 console.log(multiply("30", "69"));
 console.log(multiply("11", "85"));
