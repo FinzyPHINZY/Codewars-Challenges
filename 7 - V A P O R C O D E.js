d89605dee091cc3e640c84f94c19436ab8474cfe
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-27 05:30:54 +0100

    added starter code

--- 7 - V A P O R C O D E.js
+++ 7 - V A P O R C O D E.js
@@ -11,3 +11,5 @@
 // "Lets go to the movies"       -->  "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"
 // "Why isn't my code working?"  -->  "W  H  Y  I  S  N  '  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?"
 // FUNDAMENTALS
+
+function vaporcode(string) {}
