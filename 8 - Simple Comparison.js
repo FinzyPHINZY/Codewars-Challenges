d898143e3cf43b5e9835a90101ef9675d026e76b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-28 10:29:00 +0100

    added solution to kata and test cases

--- 8 - Simple Comparison.js
+++ 8 - Simple Comparison.js
@@ -5,6 +5,26 @@
 
 // To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
 
+// Test Cases:
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertEquals(add("1", 1), true);
+//     Test.assertEquals(add(1, "1"), true);
+//     Test.assertEquals(add(1, "0"), false);
+//     Test.assertEquals(add("11", 11), true);
+//     Test.assertEquals(add(12, 12), true);
+//     Test.assertEquals(add(120, "021"), false);
+//   });
+// });
+
 function add(a, b) {
   // code here
+  typeof a === typeof b ? true : false;
 }
+
+console.log(add("1", 1));
+console.log(add(1, "1"));
+console.log(add(1, "0"));
+console.log(add("11", 11));
+console.log(add(12, 12));
+console.log(add(120, "021"));
