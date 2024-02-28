67451e166f4fec5378ae2cf0fd1eb81279983461
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-28 09:11:20 +0100

    added starter code

--- 8 - Grasshopper - Bug Squashing.js
+++ 8 - Grasshopper - Bug Squashing.js
@@ -12,3 +12,16 @@
 // - printStatus
 // - rollDice
 // - move
+
+var health = 100;
+var position = 0;
+var coins = 0;
+
+function main() {
+  getCoins();
+  move();
+  printStatus();
+  combat();
+  rolDice();
+  attack();
+}
