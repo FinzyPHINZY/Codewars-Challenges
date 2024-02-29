d408ac28b5d135ee704968843fb2375273740620
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-29 10:16:05 +0100

    added test cases

--- 8 - Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
+++ 8 - Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
@@ -11,6 +11,23 @@
 // "Hi! Hi!" ---> "Hi! Hi"
 // "Hi"      ---> "Hi"
 
+// Test Cases:
+// it("It should works for basic tests", function () {
+//   doTest("Hi!", "Hi");
+//   doTest("Hi!!!", "Hi");
+//   doTest("!Hi", "!Hi");
+//   doTest("!Hi!", "!Hi");
+//   doTest("Hi! Hi!", "Hi! Hi");
+//   doTest("Hi", "Hi");
+// });
+
 function remove(string) {
   return "";
 }
+
+console.log(remove("Hi!"));
+console.log(remove("Hi!!!"));
+console.log(remove("!Hi"));
+console.log(remove("!Hi!"));
+console.log(remove("Hi! Hi!"));
+console.log(remove("Hi"));
