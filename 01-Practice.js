4135e9fc0fac7445371c03879e22cdb2570d33b6
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-01 10:03:51 +0100

    added solution to practicek

--- 01-Practice.js
+++ 01-Practice.js
@@ -666,3 +666,17 @@ function duplicateCount(text) {
 //     ]);
 //   });
 // });
+
+function multiplicationTable(size) {
+  let table = [];
+  for (let i = 1; i <= size; i++) {
+    let row = [];
+    for (let j = 1; j <= size; j++) {
+      row.push(i * j);
+    }
+    table.push(row);
+  }
+  return table;
+}
+
+console.log(multiplicationTable(3));
