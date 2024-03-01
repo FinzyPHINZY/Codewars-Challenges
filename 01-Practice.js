9011a541cea725e21aa684e37a7ae5e19cb5dd01
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-01 10:00:18 +0100

    added kata for practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -636,10 +636,33 @@ function duplicateCount(text) {
   return count;
 }
 
-console.log(duplicateCount("abcde"));
-console.log(duplicateCount("aabbcde"));
-console.log(duplicateCount("aabBcde"));
-console.log(duplicateCount("indivisibility"));
-console.log(duplicateCount("indivisibilities"));
-console.log(duplicateCount("aA11"));
-console.log(duplicateCount("ABBA"));
+// console.log(duplicateCount("abcde"));
+// console.log(duplicateCount("aabbcde"));
+// console.log(duplicateCount("aabBcde"));
+// console.log(duplicateCount("indivisibility"));
+// console.log(duplicateCount("indivisibilities"));
+// console.log(duplicateCount("aA11"));
+// console.log(duplicateCount("ABBA"));
+
+// DESCRIPTION:
+// Your task, is to create N×N multiplication table, of size provided in parameter.
+
+// For example, when given size is 3:
+
+// 1 2 3
+// 2 4 6
+// 3 6 9
+// For the given example, the return value should be:
+
+// [[1,2,3],[2,4,6],[3,6,9]]
+
+// Test Cases:
+// describe("Tests", () => {
+//   it("test", () => {
+//     Test.assertSimilar(multiplicationTable(3), [
+//       [1, 2, 3],
+//       [2, 4, 6],
+//       [3, 6, 9],
+//     ]);
+//   });
+// });
