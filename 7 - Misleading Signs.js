8e70ba3f12855be0fbfbeffa44d99b01ee4392fa
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-01 17:42:42 +0100

    added test cases

--- 7 - Misleading Signs.js
+++ 7 - Misleading Signs.js
@@ -40,6 +40,22 @@
 // So he cannot say that those letters are not r or k from smi(rk) because he wouldn't know
 // So your friend cannot figure out if you are fooling him.
 
+// Test CaseS:
+
+// it("example tests", function() {
+//     assert.strictEqual( gaslighting("snack","snake","c"), true, `gaslighting("snack","snake","c")` );
+//     assert.strictEqual( gaslighting("snack","snack","snack"), false, `gaslighting("snack","snack","snack")` );
+//     assert.strictEqual( gaslighting("snake","snack","c"), true, `gaslighting("snake","snack","c")` );
+//     assert.strictEqual( gaslighting("ping","pong","png"), false, `gaslighting("ping","pong","png")` );
+//     assert.strictEqual( gaslighting("ping","pong","i"), true, `gaslighting("ping","pong","i")` );
+//   });
+
 function gaslighting(shirtWord, yourWord, friendsLetters) {
   return true || false;
 }
+
+console.log(gaslighting("snack", "snake", "c"));
+console.log(gaslighting("snack", "snack", "snack"));
+console.log(gaslighting("snake", "snack", "c"));
+console.log(gaslighting("ping", "pong", "png"));
+console.log(gaslighting("ping", "pong", "i"));
