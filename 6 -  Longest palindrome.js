9c3b3e603a0de39793b3e247cb18b386b7b4db91
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-02 07:17:11 +0100

    added solution to kata

--- 6 -  Longest palindrome.js
+++ 6 -  Longest palindrome.js
@@ -8,5 +8,28 @@
 // For example if passed "Hannah" it should return 6 and if passed "aabbcc_yYx_" it should return 9 because one possible palindrome would be "abcyxycba".
 
 function longestPalindrome(str) {
-  //Good Luck
+  //Good Luck // Convert the string to lowercase to make it case insensitive
+  str = str.toLowerCase();
+
+  // Create a map to store the frequency of characters
+  const charCount = new Map();
+  for (const char of str) {
+    if (charCount.has(char)) {
+      charCount.set(char, charCount.get(char) + 1);
+    } else {
+      charCount.set(char, 1);
+    }
+  }
+
+  // Count the number of characters with odd frequency
+  let oddCount = 0;
+  for (const count of charCount.values()) {
+    if (count % 2 !== 0) {
+      oddCount++;
+    }
+  }
+
+  // The length of the longest palindrome is the length of the input string
+  // minus the number of characters with odd frequency
+  return Math.max(1, str.length - oddCount);
 }
