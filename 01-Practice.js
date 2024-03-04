fb67bf0d0b2e50d844d519415b9af59fecb0ddc2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-04 10:04:11 +0100

    added test casees

--- 01-Practice.js
+++ 01-Practice.js
@@ -712,3 +712,5 @@ function splitAndMerge(string, seperator) {
 // Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
 
 function solution(digits) {}
+
+console.log(solution(1234567890));
