94233e3ed7ff7dd873f44b594f2d2cf4746f69f4
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-04 11:46:03 +0100

    added solution to kata

--- Double Char.js
+++ Double Char.js
@@ -1,7 +1,14 @@
 function doubleChar(str) {
-  let doubleChar = "";
-  for (let i = 0; i < str.length; i++) {
-    doubleChar += str[i] + str[i];
-  }
-  return doubleChar;
+  // let doubleChar = "";
+  // for (let i = 0; i < str.length; i++) {
+  //   doubleChar += str[i].repeat(2);
+  // }
+  // return doubleChar;
+
+  str
+    .split("")
+    .map((e) => e.repeat(2))
+    .join(" ");
 }
+
+console.log(doubleChar("Hello World"));
