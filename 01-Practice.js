10598f23ab58f4f8973dfcfa75f0cde737980f3e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-05 05:24:48 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -752,6 +752,39 @@ function nthTerm(num) {
   return s.toFixed(2);
 }
 
-console.log(nthTerm(1));
-console.log(nthTerm(2));
-console.log(nthTerm(5));
+// console.log(nthTerm(1));
+// console.log(nthTerm(2));
+// console.log(nthTerm(5));
+
+// Description:
+// The museum of incredible dull things
+
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+// Task
+
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+// Examples
+
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1
+
+function museum(arr) {
+  const output = [];
+  let min = arr.indexOf(Math.min(...arr));
+  arr.forEach((num, i) => {
+    if (i !== min) {
+      output.push(num);
+    }
+  });
+
+  return output;
+}
+
+// console.log(museum([1, 2, 3, 4, 5]));
+// console.log(museum([5, 3, 2, 1, 4]));
+// console.log(museum([2, 2, 1, 2, 1]));
