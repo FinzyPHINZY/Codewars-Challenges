2d715172aadd50645a2f2e9fa4c42eab8571de8b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-05 05:30:44 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -816,10 +816,10 @@ function nthTerm(n) {
 // Create a function that returns the CSV representation of a two-dimensional numeric array.
 // Example:
 // input:
-//    [[ 0, 1, 2, 3, 4 ],
-//     [ 10,11,12,13,14 ],
-//     [ 20,21,22,23,24 ],
-//     [ 30,31,32,33,34 ]]
+//  [[ 0, 1, 2, 3, 4 ],
+//   [ 10,11,12,13,14 ],
+//   [ 20,21,22,23,24 ],
+//   [ 30,31,32,33,34 ]]
 
 // output:
 //      '0,1,2,3,4\n'
@@ -830,4 +830,14 @@ function nthTerm(n) {
 
 function toCsvText(array) {
   // good luck
+  return array.join("\n");
 }
+
+// console.log(
+//   toCsvText([
+//     [0, 1, 2, 3, 4],
+//     [10, 11, 12, 13, 14],
+//     [20, 21, 22, 23, 24],
+//     [30, 31, 32, 33, 34],
+//   ])
+// );
