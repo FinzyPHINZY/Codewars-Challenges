ec2d21c4449ed285bab3e47b1b275422bcc6912d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-05 10:55:55 +0100

    added test cases

--- WeIrD StRiNg CaSe.js
+++ WeIrD StRiNg CaSe.js
@@ -9,6 +9,21 @@
 // "String" => "StRiNg"
 // "Weird string case" => "WeIrD StRiNg CaSe"
 
+// Test Cases:
+// describe("toWeirdCase", function () {
+//   it("should return the correct value for a single word", function () {
+//     Test.assertEquals(toWeirdCase("This"), "ThIs");
+//     Test.assertEquals(toWeirdCase("is"), "Is");
+//   });
+//   it("should return the correct value for multiple words", function () {
+//     Test.assertEquals(toWeirdCase("This is a test"), "ThIs Is A TeSt");
+//   });
+// });
+
 function toWeirdCase(string) {
   //TODO
 }
+
+console.log(toWeirdCase("This"));
+console.log(toWeirdCase("is"));
+console.log(toWeirdCase("This is a test"));
