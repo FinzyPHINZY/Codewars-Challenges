fd7fca71a7607a19eb24fcdf5d1bc79ebc6324ba
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-05 09:30:56 +0100

    added test cases

--- Rot13.js
+++ Rot13.js
@@ -4,6 +4,19 @@
 
 // Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
 
+// Test Cases:
+
+// describe("Tests", function() {
+//     it("Sample tests", function() {
+//       for (const [input, expected] of [["test", "grfg"], ["Test", "Grfg"]]) {
+//         assert.strictEqual(rot13(input), expected, `Test failed with messsage = '${input}'`);
+//       }
+//     });
+//   });
+
 function rot13(message) {
   //your code here
 }
+
+console.log(rot13("test"));
+console.log(rot13("Test"));
