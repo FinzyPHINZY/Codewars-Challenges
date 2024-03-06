c5b9879ed23ebf773287433736f353ab22b7e6e4
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-06 09:23:18 +0100

    updated practice solution

--- 01-Practice.js
+++ 01-Practice.js
@@ -908,8 +908,11 @@ function order(words) {
 
 function order(words) {
   // ...
-  return words.split(" ").sort((a, b) => a.match(/\d+/) - b.match(/\d+/));
+  return words
+    .split(" ")
+    .sort((a, b) => a.match(/\d+/) - b.match(/\d+/))
+    .join(" ");
 }
 
-console.log(order("is2 Thi1s T4est 3a"));
-console.log(order("4of Fo1r pe6ople g3ood th5e the2"));
+// console.log(order("is2 Thi1s T4est 3a"));
+// console.log(order("4of Fo1r pe6ople g3ood th5e the2"));
