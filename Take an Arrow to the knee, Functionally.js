4a93411926c5cf7e90eab03e2508107548c7a6d9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-07 10:06:59 +0100

    added solution to kata

--- Take an Arrow to the knee, Functionally.js
+++ Take an Arrow to the knee, Functionally.js
@@ -19,9 +19,9 @@
 //     });
 //   });
 
-var ArrowFunc = function(arr) {
-    return arr.map( => ).join(''); //Complete this function
-  }
+var ArrowFunc = function (arr) {
+  return arr.map((num) => String.fromCharCode(num)).join(""); //Complete this function
+};
 
 console.log(ArrowFunc([84, 101, 115, 116]));
 console.log(ArrowFunc([70, 85, 83, 32, 82, 79, 72, 32, 68, 65, 72]));
