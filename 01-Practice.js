54b53fe64c93019b9f0cfdae0d211555bd8f83e9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-07 09:03:38 +0100

    added practice starters

--- 01-Practice.js
+++ 01-Practice.js
@@ -916,3 +916,13 @@ function order(words) {
 
 // console.log(order("is2 Thi1s T4est 3a"));
 // console.log(order("4of Fo1r pe6ople g3ood th5e the2"));
+
+// Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+// The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+// Examples:
+// "String" => "StRiNg"
+// "Weird string case" => "WeIrD StRiNg CaSe"
+
+function toWeirdCase(string) {
+  // Code here!
+}
