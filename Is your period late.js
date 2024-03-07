396f29fa970c4fd8d98487e256e4d5b600ecd36e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-07 09:32:30 +0100

    added test cases

--- Is your period late.js
+++ Is your period late.js
@@ -25,3 +25,6 @@
 function periodIsLate(last, today, cycleLength) {
   return false;
 }
+
+console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 35));
+console.log(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 28));
