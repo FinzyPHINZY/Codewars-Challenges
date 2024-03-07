49d7dc9f4a979c716411fe45982d955926349f3b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-07 09:31:33 +0100

    added test cases

--- Is your period late.js
+++ Is your period late.js
@@ -12,6 +12,16 @@
 
 // Return true if the number of days passed from last to today is greater than cycleLength. Otherwise, return false.
 
+// Test Cases:
+// describe("Sample Test", () => {
+//     it("It is not late", () => {
+//       assert.isFalse(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 35));
+//     });
+//     it("It is late", () => {
+//       assert.isTrue(periodIsLate(new Date(2016, 6, 13), new Date(2016, 7, 16), 28));
+//     });
+//   });
+
 function periodIsLate(last, today, cycleLength) {
   return false;
 }
