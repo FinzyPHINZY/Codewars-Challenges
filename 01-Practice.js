0a989267359bd4621eb17cbd16c0a81c1c670bf9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-07 09:21:49 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -953,3 +953,15 @@ function toWeirdCase(string) {
 // 1 --> 1 --> "1.00"
 // 2 --> 1 + 1/4 --> "1.25"
 // 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+function nthTeam(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum += 1 / (1 + i * 3);
+  }
+  return sum.toFixed(2);
+}
+
+console.log(nthTeam(1));
+console.log(nthTeam(2));
+console.log(nthTeam(5));
