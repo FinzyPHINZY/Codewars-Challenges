a5484cb7144a5557970c9fcb2e65c5dfd2a0fe92
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-08 09:14:50 +0100

    added test cases

--- Tidy Number (Special Numbers Series #9).js
+++ Tidy Number (Special Numbers Series #9).js
@@ -7,6 +7,21 @@
 // Task
 // Given a number, Find if it is Tidy or not .
 
+// Test Cases:
+// describe("Basic tests", () => {
+//   Test.assertEquals(tidyNumber(12), true);
+//   Test.assertEquals(tidyNumber(102), false);
+//   Test.assertEquals(tidyNumber(9672), false);
+//   Test.assertEquals(tidyNumber(2789), true);
+//   Test.assertEquals(tidyNumber(2335), true);
+// });
+
 function tidyNumber(n) {
   //your code here
 }
+
+console.log(tidyNumber(12));
+console.log(tidyNumber(102));
+console.log(tidyNumber(9672));
+console.log(tidyNumber(2789));
+console.log(tidyNumber(2335));
