3a77ef11cedef6d33b20336af67bc0b33b151bbc
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-08 10:27:35 +0100

    added test cases

--- Sort by Last Char.js
+++ Sort by Last Char.js
@@ -7,4 +7,17 @@
 
 // All inputs will be valid.
 
+// Test Cases:
+// describe("Sort by Last Char", () => {
+//     it("Testing for fixed tests", () => {
+//       assert.deepEqual(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+//       assert.deepEqual(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']);
+//       assert.deepEqual(last('take me to semynak'), ['take', 'me', 'semynak', 'to']);
+//     })
+//   });
+
 function last(x) {}
+
+console.log(last("man i need a taxi up to ubud"));
+console.log(last("what time are we climbing up the volcano"));
+console.log(last("take me to semynak"));
