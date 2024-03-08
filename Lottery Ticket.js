ed747b1a8552595907ce39420d78127578669a2e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-08 09:56:57 +0100

    added solution to kata

--- Lottery Ticket.js
+++ Lottery Ticket.js
@@ -14,4 +14,56 @@
 
 // All inputs will be in the correct format. Strings on tickets are not always the same length.
 
-function bingo(ticket, win) {}
+// count wins
+// check if wins is more or less than win
+// loop through string and convert them to charcode nums. str.split('').map((e, i) =>{str.charCodeAt(i)})
+
+// Test Cases:
+// describe("Tests",()=>{
+//     it('Fixed tests', ()=>{
+//     (bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2), 'Loser!');
+//       (bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1), 'Winner!');
+//       (bingo([['HGTYRE', 74], ['BE', 66], ['JKTY', 74]], 3), 'Loser!');
+//     });
+//   });
+
+function bingo(ticket, win) {
+  let output = [];
+  let count = 0;
+  ticket.forEach((arr) => {
+    arr[0] = arr[0].split("").map((e, i) => arr[0].charCodeAt(i));
+    if (arr[0].includes(arr[1])) count++;
+  });
+  return count >= win ? "Winner!" : "Loser!";
+}
+
+console.log(
+  bingo(
+    [
+      ["ABC", 65],
+      ["HGR", 74],
+      ["BYHT", 74],
+    ],
+    2
+  )
+);
+console.log(
+  bingo(
+    [
+      ["ABC", 65],
+      ["HGR", 74],
+      ["BYHT", 74],
+    ],
+    1
+  )
+);
+console.log(
+  bingo(
+    [
+      ["HGTYRE", 74],
+      ["BE", 66],
+      ["JKTY", 74],
+    ],
+    3
+  )
+);
