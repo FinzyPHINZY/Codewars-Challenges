32bb734b7e878bd8a5b5df0fb006036fcd473a4f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-12 09:32:03 +0100

    added kata description

--- Name Array Capping.js
+++ Name Array Capping.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// DESCRIPTION:
+// Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+
+// example
+
+// capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+// capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kels
