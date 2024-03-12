529548a6ba3dfe96f6a5bded84823e08260ca4ed
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-12 09:42:05 +0100

    added solution to kata

--- Name Array Capping.js
+++ Name Array Capping.js
@@ -8,7 +8,22 @@
 // capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
 // capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kels
 
-function capMe(names) {}
+function capMe(names) {
+  //   return names.map((name) => {
+  //     return name
+  //       .split("")
+  //       .map((char, i) => {
+  //         if (i === 0) {
+  //           return char.toUpperCase();
+  //         } else {
+  //           return char.toLowerCase();
+  //         }
+  //       })
+  //       .join("");
+  //   });
 
-console.log(capMe());
+  return names.map((name) => name[0].toUpperCase() + name.slice(1));
+}
+
+console.log(capMe(["jo", "nelson", "jurie"]));
 console.log(capMe(["KARLY", "DANIEL", "KELSEY"]));
