cb6a030956a13c37c6d612aaa28a9a69b870ca39
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-12 10:10:40 +0100

    added solution to kata

--- String array duplicates.js
+++ String array duplicates.js
@@ -14,19 +14,28 @@
 // Good luck!
 
 // Test Cases:
-// describe("Basic tests", function(){
-//     Test.assertDeepEquals(dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo']);
-//     Test.assertDeepEquals(dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese']);
-//     Test.assertDeepEquals(dup(["kelless","keenness"]), ['keles','kenes']);
-//     Test.assertDeepEquals(dup(["Woolloomooloo","flooddoorroommoonlighters","chuchchi"]), ['Wolomolo','flodoromonlighters','chuchchi']);
-//     Test.assertDeepEquals(dup(["adanac","soonness","toolless","ppellee"]), ['adanac','sones','toles','pele']);
-//     Test.assertDeepEquals(dup(["callalloo","feelless","heelless"]), ['calalo','feles','heles']);
-//     Test.assertDeepEquals(dup(["putteellinen","keenness"]), ['putelinen','kenes']);
-//     Test.assertDeepEquals(dup(["kelless","voorraaddoosspullen","achcha"]), ['keles','voradospulen','achcha']);
-//     });
+{
+  // describe("Basic tests", function(){
+  //     Test.assertDeepEquals(dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo']);
+  //     Test.assertDeepEquals(dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese']);
+  //     Test.assertDeepEquals(dup(["kelless","keenness"]), ['keles','kenes']);
+  //     Test.assertDeepEquals(dup(["Woolloomooloo","flooddoorroommoonlighters","chuchchi"]), ['Wolomolo','flodoromonlighters','chuchchi']);
+  //     Test.assertDeepEquals(dup(["adanac","soonness","toolless","ppellee"]), ['adanac','sones','toles','pele']);
+  //     Test.assertDeepEquals(dup(["callalloo","feelless","heelless"]), ['calalo','feles','heles']);
+  //     Test.assertDeepEquals(dup(["putteellinen","keenness"]), ['putelinen','kenes']);
+  //     Test.assertDeepEquals(dup(["kelless","voorraaddoosspullen","achcha"]), ['keles','voradospulen','achcha']);
+  //     });
+}
 
 function dup(s) {
   //..
+  return s.map((word) => {
+    let result = "";
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] !== word[i + 1]) result += word[i];
+    }
+    return result;
+  });
 }
 
 console.log(
