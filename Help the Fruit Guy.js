cad6ecd4ba94b4ac6adcc40defa400fad59da237
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-12 10:42:12 +0100

    added starter code

--- Help the Fruit Guy.js
+++ Help the Fruit Guy.js
@@ -7,3 +7,7 @@
 // If the array is null/nil/None or empty you should return empty array ([]).
 // The rotten fruit name will be in this camelcase (rottenFruit).
 // The returned array should be in lowercase.
+
+function removeRotten(bagOfFruits) {
+  // ...
+}
