82d13cdc62d8a47449d1d86235880a7c3c2af091
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-12 09:59:35 +0100

    added test cases

--- String array duplicates.js
+++ String array duplicates.js
@@ -13,6 +13,29 @@
 
 // Good luck!
 
+// Test Cases:
+// describe("Basic tests", function(){
+//     Test.assertDeepEquals(dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo']);
+//     Test.assertDeepEquals(dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese']);
+//     Test.assertDeepEquals(dup(["kelless","keenness"]), ['keles','kenes']);
+//     Test.assertDeepEquals(dup(["Woolloomooloo","flooddoorroommoonlighters","chuchchi"]), ['Wolomolo','flodoromonlighters','chuchchi']);
+//     Test.assertDeepEquals(dup(["adanac","soonness","toolless","ppellee"]), ['adanac','sones','toles','pele']);
+//     Test.assertDeepEquals(dup(["callalloo","feelless","heelless"]), ['calalo','feles','heles']);
+//     Test.assertDeepEquals(dup(["putteellinen","keenness"]), ['putelinen','kenes']);
+//     Test.assertDeepEquals(dup(["kelless","voorraaddoosspullen","achcha"]), ['keles','voradospulen','achcha']);
+//     });
+
 function dup(s) {
   //..
 }
+
+console.log(
+  dup(["ccooddddddewwwaaaaarrrrsssss", "piccaninny", "hubbubbubboo"])
+);
+console.log(dup(["abracadabra", "allottee", "assessee"]));
+console.log(dup(["kelless", "keenness"]));
+console.log(dup(["Woolloomooloo", "flooddoorroommoonlighters", "chuchchi"]));
+console.log(dup(["adanac", "soonness", "toolless", "ppellee"]));
+console.log(dup(["callalloo", "feelless", "heelless"]));
+console.log(dup(["putteellinen", "keenness"]));
+console.log(dup(["kelless", "voorraaddoosspullen", "achcha"]));
