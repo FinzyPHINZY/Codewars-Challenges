6fc6f2e4ee1ad714b285c37dc9a7f3b6acdd2919
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-13 09:33:40 +0100

    added starter code

--- 01-Practice.js
+++ 01-Practice.js
@@ -1145,3 +1145,5 @@ function dup(s) {
 // If the array is null/nil/None or empty you should return empty array ([]).
 // The rotten fruit name will be in this camelcase (rottenFruit).
 // The returned array should be in lowercase.
+
+function isRotten(arr) {}
