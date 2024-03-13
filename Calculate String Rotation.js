86859767cc19aad0e759fa0ad8c4bb4b0b7e5fa8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-13 16:02:13 +0100

    added solution to kata

--- Calculate String Rotation.js
+++ Calculate String Rotation.js
@@ -16,6 +16,9 @@
 
 function shiftedDiff(first, second) {
   // ...
+  if (first.length != second.length) return -1;
+
+  return (second + second).indexOf(first);
 }
 
 console.log(shiftedDiff("coffee", "eecoff"));
