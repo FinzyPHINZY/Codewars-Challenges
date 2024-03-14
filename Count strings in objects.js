8c83cbf656eb8ecb3de4f1462d7289e78da32539
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-14 09:27:32 +0100

    added solution to kata

--- Count strings in objects.js
+++ Count strings in objects.js
@@ -13,5 +13,21 @@
 //   //returns 3
 
 function strCount(obj) {
+  let count = 0;
   // Your code here
+  for (const key in obj) {
+    if (typeof obj[key] === "string") count++;
+    if (typeof obj[key] === "object") count += strCount(obj[key]);
+  }
+  return count;
 }
+
+console.log(
+  strCount({
+    first: "1",
+    second: "2",
+    third: false,
+    fourth: ["anytime", 2, 3, 4],
+    fifth: null,
+  })
+);
