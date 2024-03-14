9e2482c5e43a52015477874c15c2ef901c741c1f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-14 10:38:57 +0100

    added solution to kata

--- Substring-fun.js
+++ Substring-fun.js
@@ -12,16 +12,26 @@
 //   n=0     n=1     n=2
 // Note: Test cases contain valid input only - i.e. a string array or an empty array; and each word will have enough letters.
 
-describe("Basic tests", () => {
-  it("Testing for fixed tests", () => {
-    assert.strictEqual(nthChar([]), "");
-    assert.strictEqual(nthChar(["yoda", "best", "has"]), "yes");
-  });
-});
+// Test Cases:
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(nthChar([]), "");
+//     assert.strictEqual(nthChar(["yoda", "best", "has"]), "yes");
+//   });
+// });
 
 function nthChar(words) {
-  // return ...
+  return words
+    .map((word, i) => {
+      return word[i];
+    })
+    .join("");
 }
 
 console.log(nthChar([]));
-console.log(nthChar([["yoda", "best", "has"]]));
+console.log(nthChar(["yoda", "best", "has"]));
+
+// P: Array of strings
+// R: A string containing letters from each string in the array bases on its positon in the array
+// E: ['yoda', 'best', 'has'] = 'yes'
+// P: Create a modifiable named storage(variable) for an empty string; Loop through the array arguement and loop through each word; return a letter form each word based on the word position in the array. If word position is 5, return word[5]
