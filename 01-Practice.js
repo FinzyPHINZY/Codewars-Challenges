5444691d48ac8baa2e986d4d8c9c81ae5e7828ca
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-14 08:20:09 +0100

    added new kata practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -1153,4 +1153,27 @@ function isRotten(arr) {
   });
 }
 
-console.log(isRotten(["apple", "rottenBanana", "apple"]));
+// console.log(isRotten(["apple", "rottenBanana", "apple"]));
+
+// DESCRIPTION:
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+// For example:
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+// Good luck!
+
+function dup(s) {
+  //..
+}
+
+// describe("Basic tests", function(){
+// Test.assertDeepEquals(dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo']);
+// Test.assertDeepEquals(dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese']);
+// Test.assertDeepEquals(dup(["kelless","keenness"]), ['keles','kenes']);
+// Test.assertDeepEquals(dup(["Woolloomooloo","flooddoorroommoonlighters","chuchchi"]), ['Wolomolo','flodoromonlighters','chuchchi']);
+// Test.assertDeepEquals(dup(["adanac","soonness","toolless","ppellee"]), ['adanac','sones','toles','pele']);
+// Test.assertDeepEquals(dup(["callalloo","feelless","heelless"]), ['calalo','feles','heles']);
+// Test.assertDeepEquals(dup(["putteellinen","keenness"]), ['putelinen','kenes']);
+// Test.assertDeepEquals(dup(["kelless","voorraaddoosspullen","achcha"]), ['keles','voradospulen','achcha']);
+// });
