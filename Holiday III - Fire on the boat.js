c9ec530f9f0afda2f3c87099fa7e30558d037aa9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-16 04:19:47 +0100

    added test cases

--- Holiday III - Fire on the boat.js
+++ Holiday III - Fire on the boat.js
@@ -9,4 +9,25 @@
 
 // Go to work!
 
+// Test Cases:
+// describe("Holiday III - Fire on the boat", () => {
+//     it("Fixed tests", () => {
+//       assert.strictEqual(fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"), "Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast");
+//       assert.strictEqual(fireFight("Mast Deck Engine Water Fire"), "Mast Deck Engine Water ~~");
+//       assert.strictEqual(fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"), "~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain");
+//     });
+//   });
+
 function fireFight(s) {}
+
+console.log(
+  fireFight(
+    "Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"
+  )
+);
+console.log(fireFight("Mast Deck Engine Water Fire"));
+console.log(
+  fireFight(
+    "Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"
+  )
+);
