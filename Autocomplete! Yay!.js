c92031df86f944444520391224c0cb57f6ea5f8a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-16 04:44:08 +0100

    added test cases

--- Autocomplete! Yay!.js
+++ Autocomplete! Yay!.js
@@ -12,7 +12,11 @@
 
 // For example, "Apple" and "airport" would both return for an input of 'a'. However, they should return as "Apple" and "airport" in their original cases.
 
-function autocomplete(input, dictionary) {}
+function autocomplete(input, dictionary) {
+  return dictionary.filter((word) => word.startsWith(input));
+}
+
+console.log(autocomplete("ai", ["airplane", "airport", "apple", "ball"]));
 
 // PREP
 // P: Takes in two arguments, an input(string) and dictionary(array of strings)
