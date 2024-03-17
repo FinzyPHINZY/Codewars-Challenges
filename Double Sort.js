d83a3b641f4d1e43ddf874b3119879c1b3657d15
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-17 14:27:03 +0100

    added solution to kata

--- Double Sort.js
+++ Double Sort.js
@@ -21,13 +21,31 @@
 
 function dbSort(a) {
   // Code here
+  const checkNum = a.every((char) => typeof char === "number");
+  const checkStr = a.every((char) => typeof char === "string");
+  if (checkNum) return a.sort((a, b) => a - b);
+  else if (checkStr) return a.sort();
+  else {
+    const nums = a
+      .filter((char) => typeof char === "number")
+      .sort((a, b) => a - b);
+    const strings = a.filter((char) => typeof char === "string").sort();
+    return nums.concat(strings);
+  }
 }
 
 console.log(dbSort([6, 2, 3, 4, 5]));
 console.log(dbSort([14, 32, 3, 5, 5]));
 console.log(dbSort([1, 2, 3, 4, 5]));
 console.log(dbSort(["Banana", "Orange", "Apple", "Mango", 0, 2, 2]));
+console.log(dbSort(["Banana", "Orange", "Apple", "Mango"]));
 console.log(dbSort(["C", "W", "W", "W", 1, 2, 0]));
 console.log(
   dbSort(["Apple", 46, "287", 574, "Peach", "3", "69", 78, "Grape", "423"])
 );
+
+// PREP
+// P: Takes in an array containing letters or numbers or both
+// R: returns a sorter array. if (array includes only letters) returns them in alphabetical order else if(array includes only numbers) returns them in ascending order else if(array includes both numbers and letters) returns both in ascending and alphabetical order
+// E: ["C", "W", "W", "W", 1, 2, 0]) to return [0,1,2,"C","W","W","W"]
+// P: check if array contains only letters or numbers or both.
