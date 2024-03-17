d6ba78afc93605bc012bdce60d8d2ba0db1ee8d1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-17 12:54:51 +0100

    added new kata

--- Coding Meetup #14 - Higher-Order Functions Series - Order the food.js
+++ Coding Meetup #14 - Higher-Order Functions Series - Order the food.js
@@ -27,6 +27,49 @@
 // The input array will always be valid and formatted as in the example above.
 // there are 5 possible meal options and the strings representing the selected meal option will always be formatted in the same way, as follows: 'standard', 'vegetarian', 'vegan', 'diabetic', 'gluten-intolerant'.
 
+// Test Cases:
+
+var list1 = [
+  {
+    firstName: "Noah",
+    lastName: "M.",
+    country: "Switzerland",
+    continent: "Europe",
+    age: 19,
+    language: "C",
+    meal: "vegetarian",
+  },
+  {
+    firstName: "Anna",
+    lastName: "R.",
+    country: "Liechtenstein",
+    continent: "Europe",
+    age: 52,
+    language: "JavaScript",
+    meal: "standard",
+  },
+  {
+    firstName: "Ramona",
+    lastName: "R.",
+    country: "Paraguay",
+    continent: "Americas",
+    age: 29,
+    language: "Ruby",
+    meal: "vegan",
+  },
+  {
+    firstName: "George",
+    lastName: "B.",
+    country: "England",
+    continent: "Europe",
+    age: 81,
+    language: "C",
+    meal: "vegetarian",
+  },
+];
+
 function orderFood(list) {
   // thank you for checking out the Coding Meetup kata :)
 }
+
+console.log(orderFood(list1));
