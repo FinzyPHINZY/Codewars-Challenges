94075282775ffe339e49feda3eb3e26a21daa08b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-17 13:14:41 +0100

    added starter code

--- Double Sort.js
+++ Double Sort.js
@@ -6,3 +6,7 @@
 // Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type.
 
 // Note that numbers written as strings are strings and must be sorted with the other strings.
+
+function dbSort(a) {
+  // Code here
+}
