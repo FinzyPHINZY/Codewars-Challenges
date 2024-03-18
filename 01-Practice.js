fb4fae85a39e9822c8ffbfff2001a8d2a26e396c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-18 13:07:45 +0100

    added solution to practice" "s
    "

--- 01-Practice.js
+++ 01-Practice.js
@@ -1299,3 +1299,32 @@ function dup(s) {
 // Test.assertDeepEquals(dup(["putteellinen","keenness"]), ['putelinen','kenes']);
 // Test.assertDeepEquals(dup(["kelless","voorraaddoosspullen","achcha"]), ['keles','voradospulen','achcha']);
 // });
+
+// Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+// The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+// Examples:
+// "String" => "StRiNg"
+// "Weird string case" => "WeIrD StRiNg CaSe"
+
+function toWeirdCase(string) {
+  // Code here!
+  return string.split(" ").map((word) => {
+    return word
+      .split("")
+      .map((char, i) => {
+        if (i % 2 !== 0) {
+          return char.toLowerCase();
+        } else {
+          return char.toUpperCase();
+        }
+      })
+      .join("");
+  });
+}
+
+// P: takes in a string argument
+// R: returns a new string with words uppercased and lowercased according to their index
+// E:  "String" => "StRiNg"
+// P:
+console.log(toWeirdCase("String"));
+console.log(toWeirdCase("Weird string case"));
