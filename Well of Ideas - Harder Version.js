a4d60f4148aabeff58771801ef4221257d744592
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-19 09:55:44 +0100

    added starter code

--- Well of Ideas - Harder Version.js
+++ Well of Ideas - Harder Version.js
@@ -8,3 +8,5 @@
 // The sub arrays may not be the same length.
 
 // The solution should be case insensitive (ie good, GOOD and gOOd all count as a good idea). All inputs may not be strings.
+
+function well(x) {}
