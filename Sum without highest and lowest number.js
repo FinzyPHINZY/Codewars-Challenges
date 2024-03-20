557f6a02264dcd42b5475fccfa65cfff8fabf000
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-20 10:54:51 +0100

    added solution to kata

--- Sum without highest and lowest number.js
+++ Sum without highest and lowest number.js
@@ -14,16 +14,24 @@
 // Input validation
 // If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
 
+// Test Cases:
+
+// console.log(sumArray(null));
+// console.log(sumArray([]));
+// console.log(sumArray([3]));
+// console.log(sumArray([3, 5]));
+// console.log(sumArray([6, 2, 1, 8, 10]));
+console.log(sumArray([0, 1, 6, 10, 10]));
+// console.log(sumArray([-6, -20, -1, -10, -12]));
+// console.log(sumArray([-6, 20, -1, 10, -12]));
+
 function sumArray(array) {
   if (!array || array.length <= 2) {
     return 0;
   }
 
-  const maxVal = Math.max(...array);
-  const minVal = Math.min(...array);
-
-  return array.reduce(
-    (sum, num) => sum + (num !== maxVal && num !== minVal ? num : 0),
-    0
-  );
+  return array
+    .sort((a, b) => a - b)
+    .filter((num, i) => i !== 0 && i !== array.length - 1)
+    .reduce((sum, num) => sum + num, 0);
 }
