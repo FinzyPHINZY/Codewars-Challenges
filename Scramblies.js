877fd085e4e91ca527ff077669e20268c16fba79
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-20 10:59:47 +0100

    added test cases

--- Scramblies.js
+++ Scramblies.js
@@ -12,6 +12,32 @@
 // scramble('cedewaraaossoqqyt', 'codewars') ==> True
 // scramble('katas', 'steak') ==> False
 
+// Test Cases:
+
+//   assert.strictEqual(scramble('rkqodlw',           'world'      ), true );
+//   assert.strictEqual(scramble('cedewaraaossoqqyt', 'codewars'   ), true );
+//   assert.strictEqual(scramble('katas',             'steak'      ), false);
+//   assert.strictEqual(scramble('scriptjavx',        'javascript' ), false);
+//   assert.strictEqual(scramble('scriptingjava',     'javascript' ), true );
+//   assert.strictEqual(scramble('scriptsjava',       'javascripts'), true );
+//   assert.strictEqual(scramble('javscripts',        'javascript' ), false);
+//   assert.strictEqual(scramble('jscripts',          'javascript' ), false);
+//   assert.strictEqual(scramble('aabbcamaomsccdd',   'commas'     ), true );
+//   assert.strictEqual(scramble('commas',            'commas'     ), true );
+//   assert.strictEqual(scramble('sammoc',            'commas'     ), true )
+
 function scramble(str1, str2) {
   return false;
 }
+
+console.log(scramble("rkqodlw", "world"));
+console.log(scramble("cedewaraaossoqqyt", "codewars"));
+console.log(scramble("katas", "steak"));
+console.log(scramble("scriptjavx", "javascript"));
+console.log(scramble("scriptingjava", "javascript"));
+console.log(scramble("scriptsjava", "javascripts"));
+console.log(scramble("javscripts", "javascript"));
+console.log(scramble("jscripts", "javascript"));
+console.log(scramble("aabbcamaomsccdd", "commas"));
+console.log(scramble("commas", "commas"));
+console.log(scramble("sammoc", "commas"));
