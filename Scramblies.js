b0782cacef77955d5ef5c7a4018a293a7dfcf4d0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-20 10:57:50 +0100

    added starter code

--- Scramblies.js
+++ Scramblies.js
@@ -11,3 +11,7 @@
 // scramble('rkqodlw', 'world') ==> True
 // scramble('cedewaraaossoqqyt', 'codewars') ==> True
 // scramble('katas', 'steak') ==> False
+
+function scramble(str1, str2) {
+  return false;
+}
