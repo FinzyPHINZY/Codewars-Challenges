236f838f2f7034da1e871677afacb1465320f693
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-21 19:39:48 +0100

    added solution to kata

--- Sum Strings as Numbers.js
+++ Sum Strings as Numbers.js
@@ -15,6 +15,12 @@
 // Test Cases:
 // Test.assertEquals(sumStrings("123", "456"), "579");
 
-function sumStrings(a, b) {}
+function sumStrings(a, b) {
+  return (BigInt(a) + BigInt(b)).toString();
+}
 
 console.log(sumStrings("123", "456"));
+console.log(sumStrings("", "5"));
+console.log(
+  sumStrings("712569312664357328695151392", "8100824045303269669937")
+);
