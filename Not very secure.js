115037c01f57887d998db9d02ddac56fe2801bb1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-21 19:21:18 +0100

    added starter code

--- Not very secure.js
+++ Not very secure.js
@@ -8,3 +8,7 @@
 // At least one character ("" is not valid)
 // Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
 // No whitespaces / underscore
+
+function alphanumeric(string) {
+  //your code here
+}
