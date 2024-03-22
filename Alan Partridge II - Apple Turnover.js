6e0e607eb69c6b6644d0c3b6c4756f543e31f2cf
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-22 12:07:24 +0100

    added solution to kata

--- Alan Partridge II - Apple Turnover.js
+++ Alan Partridge II - Apple Turnover.js
@@ -23,7 +23,12 @@
 //   "Help yourself to a honeycomb Yorkie for the glovebox."
 // );
 
-function apple(x) {}
+function apple(x) {
+  x = parseInt(x);
+  return x * x > 1000
+    ? "It's hotter than the sun!!"
+    : "Help yourself to a honeycomb Yorkie for the glovebox.";
+}
 
 console.log(apple("50"));
 console.log(apple(4));
