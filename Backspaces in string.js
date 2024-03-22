344430e594988179bfba6bb4c2b3a64919df126b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-22 12:16:09 +0100

    added test cases

--- Backspaces in string.js
+++ Backspaces in string.js
@@ -11,6 +11,21 @@
 // "#######"       ==>  ""
 // ""              ==>  ""
 
+// Test Cases:
+// describe("cleanString", () => {
+//   it("should work correctly", () => {
+//     assert.strictEqual(cleanString("abc#d##c"), "ac");
+//     assert.strictEqual(cleanString("abc####d##c#"), "");
+//   });
+// });
+
 function cleanString(s) {
   // ... your code ...
 }
+
+console.log(cleanString("abc#d##c"));
+console.log(cleanString("abc##d######"));
+console.log(cleanString("#######"));
+console.log(cleanString(""));
+console.log(cleanString("abc#d##c"));
+console.log(cleanString("abc####d##c#"));
