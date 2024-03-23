e61b8eb6bc895331c3769f2291bc1fd155a6bc2c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-23 11:34:35 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -1350,6 +1350,39 @@ function myLanguages(results) {
   // }
 }
 
-console.log(myLanguages({ Java: 10, Ruby: 80, Python: 65 }));
-console.log(myLanguages({ Hindi: 60, Greek: 71, Dutch: 93 }));
-console.log(myLanguages({ "C++": 50, ASM: 10, Haskell: 20 }));
+// console.log(myLanguages({ Java: 10, Ruby: 80, Python: 65 }));
+// console.log(myLanguages({ Hindi: 60, Greek: 71, Dutch: 93 }));
+// console.log(myLanguages({ "C++": 50, ASM: 10, Haskell: 20 }));
+
+// DESCRIPTION:
+// This is a spin off of my first kata.
+// You are given a string containing a sequence of character sequences separated by commas.
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+// Examples
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+
+function array(string) {
+  // TODO
+  string = string.split(",");
+
+  if (string.length < 3) return null;
+
+  return string
+    .filter((char, i) => i !== 0 && i !== string.length - 1)
+    .join(" ");
+}
+
+// console.log(array(""));
+// console.log(array("1"));
+// console.log(array("A1,B2"));
+// console.log(array("1,2,3"));
+// console.log(array("1,2,3,4"));
+// console.log(array("A1,B2,C3,D4,E5"));
+// console.log(array("A,1,23,456,78,9,Z"));
