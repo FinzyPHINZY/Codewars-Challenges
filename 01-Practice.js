e57a43236284fa8310da40098fea4cf6c17b03a0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-23 11:58:16 +0100

    added new kata practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -1425,3 +1425,48 @@ console.log(partlist(["az", "toto", "picaro", "zone", "kiwi"]));
 // R: Return an array of arrays containing the words
 // E: just above
 // P: create a return arr.loop through the array and add a sliced array of the current word index to the loop index
+
+// DESCRIPTION:
+// Time to win the lottery!
+// Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+// Example ticket:
+// [ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+// To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, you get a mini win. Note you can only have one mini win per sub array.
+// Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+// All inputs will be in the correct format. Strings on tickets are not always the same length.
+
+function bingo(ticket, win) {}
+
+assert.strictEqual(
+  bingo(
+    [
+      ["ABC", 65],
+      ["HGR", 74],
+      ["BYHT", 74],
+    ],
+    2
+  ),
+  "Loser!"
+);
+assert.strictEqual(
+  bingo(
+    [
+      ["ABC", 65],
+      ["HGR", 74],
+      ["BYHT", 74],
+    ],
+    1
+  ),
+  "Winner!"
+);
+assert.strictEqual(
+  bingo(
+    [
+      ["HGTYRE", 74],
+      ["BE", 66],
+      ["JKTY", 74],
+    ],
+    3
+  ),
+  "Loser!"
+);
