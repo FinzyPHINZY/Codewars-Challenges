f2f7c8ce4590de91f0ada5175f64c4caf3495a6a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-24 11:40:23 +0100

    added prep

--- Moving Zeros To The End.js
+++ Moving Zeros To The End.js
@@ -14,3 +14,9 @@ function moveZeros(arr) {
 
 console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
 console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+
+// PREP
+// P: contains an array of various elements
+// R: returns an array of elements with the zeros moved to the end of the array as well as the other elements maintaining thier orginal position
+// E: moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]), [1, 2, 1, 1, 3, 1, 0, 0, 0, 0];
+// P: We can sort this in multiple ways. we can populate a new array with the elements that arent equal to zero and add an array containing zeros up to the zeros count in the function argument
