a230f439cad23e9a8fb5cad07667221eed1975ee
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-25 10:18:42 +0100

    added test cases

--- 01-Practice.js
+++ 01-Practice.js
@@ -1504,17 +1504,20 @@ function modifyMultiply(str, loc, num) {
 // P: takes in three arguments - str, loc , num. 'str is a string, loc and nums are numbers
 // R: returns a string str[loc] times num
 
+// Task
+// Implement a function which accepts 2 arguments: string and separator.
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+// For example:
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
 
-Task
-Implement a function which accepts 2 arguments: string and separator.
-The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
-For example:
-splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
-splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
-splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
-splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
-
-
-function splitAndMerge(str, sep){
-  return
+function splitAndMerge(str, sep) {
+  return;
 }
+
+console.log(splitAndMerge("My name is John", " "));
+console.log(splitAndMerge("My name is John", "-"));
+console.log(splitAndMerge("Hello World!", "."));
+console.log(splitAndMerge("Hello World!", ","));
