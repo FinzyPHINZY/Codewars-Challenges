f89a190477a07cb92b9ab6f2f9dd5b4b2130e6f4
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-27 09:37:48 +0100

    added test cases

--- Find the nth Digit of a Number.js
+++ Find the nth Digit of a Number.js
@@ -7,6 +7,8 @@
 // If num is negative, ignore its sign and treat it as a positive value
 // If nth is not positive, return -1
 // Keep in mind that 42 = 00042. This means that findDigit(42, 5) would return 0
+
+// Test Cases:
 // Examples(num, nth --> output)
 // 5673, 4 --> 5
 // 129, 2 --> 2
@@ -17,3 +19,11 @@
 // 24, -8 --> -1
 
 var findDigit = function (num, nth) {};
+
+console.log(findDigit(5673, 4));
+console.log(findDigit(129, 2));
+console.log(findDigit(-2825, 3));
+console.log(findDigit(-456, 4));
+console.log(findDigit(0, 20));
+console.log(findDigit(65, 0));
+console.log(findDigit(24, -8));
