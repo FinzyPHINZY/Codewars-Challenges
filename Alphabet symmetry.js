d1f6b3224c2dd87b73c0300f56ab281e051c49a4
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-27 09:10:03 +0100

    added test cases

--- Alphabet symmetry.js
+++ Alphabet symmetry.js
@@ -9,6 +9,17 @@
 
 // Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
 
+// Test Cases:
+// assert.deepEqual(solve(["abode", "ABc", "xyzD"]), [4, 3, 1]);
+// assert.deepEqual(solve(["abide", "ABc", "xyz"]), [4, 3, 0]);
+// assert.deepEqual(solve(["IAMDEFANDJKL","thedefgh","xyzDEFghijabc"]),[6, 5, 7]);
+// assert.deepEqual(solve(["encode", "abc", "xyzD", "ABmD"]), [1, 3, 1, 3]);
+
 function solve(arr) {
   //code
 }
+
+console.log(solve(["abode", "ABc", "xyzD"]));
+console.log(solve(["abide", "ABc", "xyz"]));
+console.log(solve(["IAMDEFANDJKL", "thedefgh", "xyzDEFghijabc"]));
+console.log(solve(["encode", "abc", "xyzD", "ABmD"]));
