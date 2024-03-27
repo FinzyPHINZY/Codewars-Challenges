f0e93e5778f3b87b95946bc23f80de6a2be0172b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-27 09:18:57 +0100

    added test cases

--- esreveR.js
+++ esreveR.js
@@ -5,6 +5,17 @@
 
 // (the dedicated builtin(s) functionalities are deactivated)
 
+// Test Cases:
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.deepEqual(reverse([1, 2, 3]), [3, 2, 1]);
+//     assert.deepEqual(reverse([1, null, 14, "two"]), ["two", 14, null, 1]);
+//   });
+// });
+
 reverse = function (array) {
   // TODO: program me!
 };
+
+console.log(reverse([1, 2, 3]));
+console.log(reverse([1, null, 14, "two"]));
