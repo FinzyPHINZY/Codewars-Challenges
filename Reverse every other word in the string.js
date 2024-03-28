91f9ce037b3a2c7ae8004bc0ba839c33d8995d3b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-28 11:24:37 +0100

    added test cases

--- Reverse every other word in the string.js
+++ Reverse every other word in the string.js
@@ -1,3 +1,11 @@
 "use strict";
 
 // Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+
+// Test Cases:
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(reverse("Reverse this string, please!"), "Reverse siht string, !esaelp")
+//   Test.assertEquals(reverse("I really don't like reversing strings!"),"I yllaer don't ekil reversing !sgnirts")
+//     });
+//   });
