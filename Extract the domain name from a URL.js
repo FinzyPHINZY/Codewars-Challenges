413b7706f5b0c5732b5aec85a8828b831a71e4ae
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-03-29 04:21:42 +0100

    added starter code

--- Extract the domain name from a URL.js
+++ Extract the domain name from a URL.js
@@ -5,3 +5,7 @@
 // * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
 // * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
 // * url = "https://www.cnet.com"                -> domain name = cnet"
+
+function domainName(url) {
+  //your code here
+}
