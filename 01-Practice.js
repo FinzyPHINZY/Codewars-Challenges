0744ab82e7cb726bf232ba4f6920bd85d2c76e48
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-01 11:06:51 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -1674,3 +1674,16 @@ function cleanString(s) {
 
 // console.log(cleanString("abc#d##c"));
 // console.log(cleanString("abc####d##c#"));
+
+// Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+// The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+// Examples:
+// "String" => "StRiNg"
+// "Weird string case" => "WeIrD StRiNg CaSe"
+
+function toWeirdCase(string) {
+  // Code here!
+}
+
+console.log(toWeirdCase("String"));
+console.log(toWeirdCase("Weird string case"));
