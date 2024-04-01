09aab742e238c5da79601d1dbfafd9ff38a2be5f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-01 11:19:35 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -1724,6 +1724,14 @@ function toWeirdCase(string) {
 
 function towerBuilder(nFloors) {
   // build here
+  let tower = [];
+  for (let i = 0; i < nFloors; i++) {
+    const spaces = " ".repeat(nFloors - i - 1);
+    const stars = "*".repeat(2 * i + 1);
+    tower.push(spaces + stars + spaces);
+  }
+
+  return tower;
 }
 
 console.log(towerBuilder(6));
