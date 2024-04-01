b7a4850913b37488ea81f2a3f2587631f4fff626
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-01 11:11:44 +0100

    added practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -1700,5 +1700,30 @@ function toWeirdCase(string) {
     .join(" ");
 }
 
-console.log(toWeirdCase("String"));
-console.log(toWeirdCase("Weird string case"));
+// console.log(toWeirdCase("String"));
+// console.log(toWeirdCase("Weird string case"));
+
+// DESCRIPTION:
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+// For example, a tower with 3 floors looks like this:
+// [
+//   "  *  ",
+//   " *** ",
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+// [
+//   "     *     ",
+//   "    ***    ",
+//   "   *****   ",
+//   "  *******  ",
+//   " ********* ",
+//   "***********"
+// ]
+
+function towerBuilder(nFloors) {
+  // build here
+}
+
+console.log(towerBuilder(6));
