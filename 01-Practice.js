55dea282d1fa14be7187ba7d3bc4ce0da3ac6fc6
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-02 09:52:20 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -1769,17 +1769,21 @@ function domainName(url) {
 
 function dup(s) {
   //..
-  s = s.split(" ");
-  return s;
+  return s.map((word) =>
+    word
+      .split("")
+      .filter((char, i) => word[i] !== word[i + 1])
+      .join("")
+  );
 }
 
-console.log(
-  dup(["ccooddddddewwwaaaaarrrrsssss", "piccaninny", "hubbubbubboo"])
-); //,['codewars','picaniny','hubububo']);
-console.log(dup(["abracadabra", "allottee", "assessee"])); //,['abracadabra','alote','asese']);
-console.log(dup(["kelless", "keenness"])); //, ['keles','kenes']);
-console.log(dup(["Woolloomooloo", "flooddoorroommoonlighters", "chuchchi"])); //, ['Wolomolo','flodoromonlighters','chuchchi']);
-console.log(dup(["adanac", "soonness", "toolless", "ppellee"])); //, ['adanac','sones','toles','pele']);
-console.log(dup(["callalloo", "feelless", "heelless"])); //, ['calalo','feles','heles']);
-console.log(dup(["putteellinen", "keenness"])); //, ['putelinen','kenes']);
-console.log(dup(["kelless", "voorraaddoosspullen", "achcha"])); //, ['keles','voradospulen','achcha']);
+// console.log(
+//   dup(["ccooddddddewwwaaaaarrrrsssss", "piccaninny", "hubbubbubboo"])
+// ); //,['codewars','picaniny','hubububo']);
+// console.log(dup(["abracadabra", "allottee", "assessee"])); //,['abracadabra','alote','asese']);
+// console.log(dup(["kelless", "keenness"])); //, ['keles','kenes']);
+// console.log(dup(["Woolloomooloo", "flooddoorroommoonlighters", "chuchchi"])); //, ['Wolomolo','flodoromonlighters','chuchchi']);
+// console.log(dup(["adanac", "soonness", "toolless", "ppellee"])); //, ['adanac','sones','toles','pele']);
+// console.log(dup(["callalloo", "feelless", "heelless"])); //, ['calalo','feles','heles']);
+// console.log(dup(["putteellinen", "keenness"])); //, ['putelinen','kenes']);
+// console.log(dup(["kelless", "voorraaddoosspullen", "achcha"])); //, ['keles','voradospulen','achcha']);
