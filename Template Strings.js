a9d21f2d6875cc75e16a2b3b79cc53b58c7f1d29
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-05 08:44:22 +0100

    addeed new kata

--- Template Strings.js
+++ Template Strings.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
+
+var templateStrings = function (noun, adjective) {
+  return; //complete this
+};
