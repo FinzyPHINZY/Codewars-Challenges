3d9b003fefe042a9dba578ba6e15169f496aece9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-08 10:29:53 +0100

    added added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -1946,7 +1946,14 @@ function oddNum(arr) {
 // Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
 // All inputs will be in the correct format. Strings on tickets are not always the same length.
 
-function bingo(ticket, win) {}
+function bingo(ticket, win) {
+  let count = 0;
+  ticket.forEach((arr) => {
+    arr[0] = arr[0].split("").map((char, i) => arr[0].charCodeAt(i));
+    if (arr[0].includes(arr[1])) count++;
+  });
+  return count >= win ? "Winner!" : "Loser!";
+}
 
 console.log(
   bingo(
