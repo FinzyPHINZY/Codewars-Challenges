34a2b248c9b05689d2eb112b7b13eb91de5b3eb0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-08 09:25:44 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -1935,5 +1935,46 @@ function oddNum(arr) {
   }
 }
 
-console.log(oddNum([1, 1, 2])); // 2
-console.log(oddNum([17, 17, 3, 17, 17, 17, 17])); // 3
+// console.log(oddNum([1, 1, 2])); // 2
+// console.log(oddNum([17, 17, 3, 17, 17, 17, 17])); // 3
+
+// Time to win the lottery!
+// Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+// Example ticket:
+// [ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+// To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, you get a mini win. Note you can only have one mini win per sub array.
+// Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+// All inputs will be in the correct format. Strings on tickets are not always the same length.
+
+function bingo(ticket, win) {}
+
+console.log(
+  bingo(
+    [
+      ["ABC", 65],
+      ["HGR", 74],
+      ["BYHT", 74],
+    ],
+    2
+  )
+); //, 'Loser!');
+console.log(
+  bingo(
+    [
+      ["ABC", 65],
+      ["HGR", 74],
+      ["BYHT", 74],
+    ],
+    1
+  )
+); // 'Winner!');
+console.log(
+  bingo(
+    [
+      ["HGTYRE", 74],
+      ["BE", 66],
+      ["JKTY", 74],
+    ],
+    3
+  )
+); //, 'Loser!');
