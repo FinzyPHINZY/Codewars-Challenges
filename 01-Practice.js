39a180f077d00f32af8ab49c80ff6bce2f46357f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-12 05:51:33 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2085,16 +2085,38 @@ function orderFood(list) {
 // Note: only positive integers will be tested.
 
 function sumOfIntegersInString(s) {
-  return; // what the function name says
+  const numbers = "1234567890";
+  let res = "";
+  let sum = 0;
+
+  for (let i = 0; i <= s.length; i += 1) {
+    if (numbers.includes(s[i])) {
+      res += s[i];
+    } else {
+      if (res !== "") {
+        sum = sum + parseInt(res);
+        res = "";
+      }
+    }
+  }
+  return sum;
 }
 
-console.log("12.4"); //, 16
-console.log("h3ll0w0rld"); //, 3
-console.log("2 + 3 = "); //, 5
-console.log(
-  "Our company made approximately 1 million in gross revenue last quarter."
-); //, 1
-console.log("The Great Depression lasted from 1929 to 1939."); //, 3868
-console.log("Dogs are our best friends."); //, 0
-console.log("C4t5 are 4m4z1ng."); //, 18
-console.log("The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog"); //, 3635
+// console.log(sumOfIntegersInString("12.4")); //, 16
+// console.log(sumOfIntegersInString("h3ll0w0rld")); //, 3
+// console.log(sumOfIntegersInString("2 + 3 = ")); //, 5
+// console.log(
+//   sumOfIntegersInString(
+//     "Our company made approximately 1 million in gross revenue last quarter."
+//   )
+// ); //, 1
+// console.log(
+//   sumOfIntegersInString("The Great Depression lasted from 1929 to 1939.")
+// ); //, 3868
+// console.log(sumOfIntegersInString("Dogs are our best friends.")); //, 0
+// console.log(sumOfIntegersInString("C4t5 are 4m4z1ng.")); //, 18
+// console.log(
+//   sumOfIntegersInString(
+//     "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog"
+//   )
+// ); //, 3635
