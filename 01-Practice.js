7c52f7f18548c5dd5873752183291413cd4a3e01
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-12 06:18:19 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2016,10 +2016,17 @@ function persistence(num) {
 // Task
 // You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
 
-console.log(sortArray([7, 1])); // [1, 7]
-console.log(sortArray([5, 8, 6, 3, 4])); //  [3, 8, 6, 5, 4]
-console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])); // [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+function sortArray(arr) {
+  const oddNumbers = arr.filter((num) => num % 2 !== 0).sort((a, b) => a - b);
+  const evenNumbers = arr.filter((num) => num % 2 === 0);
 
-console.log(sortArray([5, 3, 2, 8, 1, 4])); //, [1, 3, 2, 8, 5, 4]);
-console.log(sortArray([5, 3, 1, 8, 0])); //, [1, 3, 5, 8, 0]);
-console.log(sortArray([])); // , []);
+  return arr.map((num) => (num % 2 == 0 ? num : oddNumbers.shift()));
+}
+
+// console.log(sortArray([7, 1])); // [1, 7]
+// console.log(sortArray([5, 8, 6, 3, 4])); //  [3, 8, 6, 5, 4]
+// console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])); // [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+// console.log(sortArray([5, 3, 2, 8, 1, 4])); //, [1, 3, 2, 8, 5, 4]);
+// console.log(sortArray([5, 3, 1, 8, 0])); //, [1, 3, 5, 8, 0]);
+// console.log(sortArray([])); // , []);
