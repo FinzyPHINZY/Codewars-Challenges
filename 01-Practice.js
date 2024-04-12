91deae4bed2c9c1021ae42ff0de5548d7d387c9c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-12 06:18:50 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2030,3 +2030,8 @@ function sortArray(arr) {
 // console.log(sortArray([5, 3, 2, 8, 1, 4])); //, [1, 3, 2, 8, 5, 4]);
 // console.log(sortArray([5, 3, 1, 8, 0])); //, [1, 3, 5, 8, 0]);
 // console.log(sortArray([])); // , []);
+
+// DESCRIPTION:
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// The input string will only consist of lower case letters and/or spaces.
