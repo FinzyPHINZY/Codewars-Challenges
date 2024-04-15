16756f860d14b95aa731615ade53b325493e3980
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-15 09:04:36 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2491,3 +2491,31 @@ function removeSmallest(arr) {
 // console.log(removeSmallest([1, 2, 3, 4, 5]));
 // console.log(removeSmallest([5, 3, 2, 1, 4]));
 // console.log(removeSmallest([2, 2, 1, 2, 1]));
+
+// In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+// The result should also be ordered from highest to lowest.
+
+// Examples:
+
+// [4, 10, 10, 9]  =>  [10, 9]
+// [1, 1, 1]  =>  [1]
+// []  =>  []
+
+function twoHighest(arr) {
+  //code here
+  arr = arr.sort((a, b) => b - a);
+  arr = new Set(arr);
+
+  return [...arr].slice(0, 2);
+}
+
+// console.log(twoHighest([])); //, [])
+// console.log(twoHighest([15])); //, [15])
+// console.log(twoHighest([15, 20, 20, 17])); //, [20, 17])
+// console.log(
+//   twoHighest([
+//     67512, 3074, 12811, 12239, 25921, 21664, 19780, 95875, 71359, 85466, 36894,
+//     11177, 13197, 95875, 75104, 71915, 7095, 25945, 8608, 88147, 60532, 29569,
+//   ])
+// ); //: expected [ 95875, 95875 ] to deeply equal [ 95875, 88147 ]
