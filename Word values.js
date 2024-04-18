b55963e6043c761fda7cde3fa2a09cbb1c09d197
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-18 09:43:26 +0100

    added test cases

--- Word values.js
+++ Word values.js
@@ -15,3 +15,6 @@
 function wordValue(a) {
   return;
 }
+
+console.log(wordValue(["codewars", "abc", "xyz"])); //, [88, 12, 225]);
+console.log(wordValue(["abc abc", "abc abc", "abc", "abc"])); //, [12, 24, 18, 24]);
