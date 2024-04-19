f802c1dc4b0e24cdb3f8fec435b80185c7517e34
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-19 09:39:51 +0100

    added solution to kata

--- pick a set of first elements.js
+++ pick a set of first elements.js
@@ -4,13 +4,18 @@
 
 // If n == 0 return an empty sequence []
 
-Examples;
-var arr = ["a", "b", "c", "d", "e"];
-first(arr); //=> ['a'];
-first(arr, 2); //=> ['a', 'b']
-first(arr, 3); //=> ['a', 'b', 'c'];
-first(arr, 0); //=> [];
+// Examples;
+// var arr = ["a", "b", "c", "d", "e"];
+// first(arr); //=> ['a'];
+// first(arr, 2); //=> ['a', 'b']
+// first(arr, 3); //=> ['a', 'b', 'c'];
+// first(arr, 0); //=> [];
 
-function first(arr, n) {
-  // return arr;
+function first(arr, n = 1) {
+  console.log(arr.slice(0, n));
+  return arr.slice(0, n);
 }
+
+console.log(first(["a", "b", "c", "d", "e"])); //, ["a"]);
+console.log(first(["a", "b", "c", "d", "e"], 2)); //, ["a", "b"]);
+// console.log(first([958,121,@K?,WYStM,984,pzld?U,577,737,370,447]));
