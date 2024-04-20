c2f34de81fe0e0da9bbef47b929e97c212842a44
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-20 15:44:46 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2641,9 +2641,27 @@ function array(string) {
   return string.length < 3 ? null : string.slice(1, -1);
 }
 
-console.log(array("1,2,3"));
-console.log(array("1,2,3,4"));
-console.log(array("1,2,3,4,5"));
-console.log(array(""));
-console.log(array("1"));
-console.log(array("1,2"));
+// console.log(array("1,2,3"));
+// console.log(array("1,2,3,4"));
+// console.log(array("1,2,3,4,5"));
+// console.log(array(""));
+// console.log(array("1"));
+// console.log(array("1,2"));
+
+// DESCRIPTION:
+// Digital root is the recursive sum of all the digits in a number.
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digitalRoot(n) {
+  // ...
+}
+
+console.log(digitalRoot(16));
+console.log(digitalRoot(942));
+console.log(digitalRoot(132189));
+console.log(digitalRoot(493193));
