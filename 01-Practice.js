ac944254dfec0295ba6bfe31e06dbae3ffaa7af3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-20 15:53:20 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2671,7 +2671,23 @@ function digitalRoot(n) {
   return digitalRoot(sum);
 }
 
-console.log(digitalRoot(16));
-console.log(digitalRoot(942));
-console.log(digitalRoot(132189));
-console.log(digitalRoot(493193));
+// console.log(digitalRoot(16));
+// console.log(digitalRoot(942));
+// console.log(digitalRoot(132189));
+// console.log(digitalRoot(493193));
+
+// DESCRIPTION:
+// Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+// Find the number of Friday 13th in the given year.
+// Input: Year in Gregorian calendar as integer.
+// Output: Number of Black Fridays in the year as an integer.
+// Examples:
+// unluckyDays(2015) == 3
+// unluckyDays(1986) == 1
+
+function unluckyDays(year) {
+  //your code here
+}
+
+console.log(unluckyDays(2015));
+console.log(unluckyDays(1986));
