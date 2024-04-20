4efcf2477eb48af2f7d7e6d70de19968c6146835
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-20 16:42:07 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2687,6 +2687,13 @@ function digitalRoot(n) {
 
 function unluckyDays(year) {
   //your code here
+
+  let count = 0;
+  for (let month = 0; month < 12; month++) {
+    const date = new Date(year, month, 13);
+    if (date.getDay() === 5) count++;
+  }
+  return count;
 }
 
 console.log(unluckyDays(2015));
