625d07ef5474d7917cbc92e879079e6bcc1dd992
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-21 21:08:48 +0100

    added solution to kata

--- 01-Practice.js
+++ 01-Practice.js
@@ -2815,9 +2815,18 @@ function orderFood(list) {
 
 function digitalRoot(n) {
   // ...
+  if (n < 10) return n;
+
+  let sum = n
+    .toString()
+    .split("")
+    .map(Number)
+    .reduce((sum, num) => sum + num, 0);
+
+  return digitalRoot(sum);
 }
 
-console.log(digitalRoot(16));
-console.log(digitalRoot(942));
-console.log(digitalRoot(132189));
-console.log(digitalRoot(493193));
+// console.log(digitalRoot(16));
+// console.log(digitalRoot(942));
+// console.log(digitalRoot(132189));
+// console.log(digitalRoot(493193));
