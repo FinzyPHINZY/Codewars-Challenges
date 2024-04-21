7385744bfb50961bad37ed8366505cb5b15d1e23
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-21 21:04:59 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2793,13 +2793,26 @@ function orderFood(list) {
   for (const dev of list) {
     const meal = dev.meal;
 
-    if(devMeal[meal]) {
+    if (devMeal[meal]) {
       devMeal[meal] += 1;
-    }
-    else{
+    } else {
       devMeal[meal] = 1;
     }
-  }return devMeal
+  }
+  return devMeal;
 }
 
-console.log(orderFood(list1)); // var answer = { vegetarian: 2, standard: 1, vegan: 1 };
+// console.log(orderFood(list1)); // var answer = { vegetarian: 2, standard: 1, vegan: 1 };
+
+// DESCRIPTION:
+// Digital root is the recursive sum of all the digits in a number.
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digitalRoot(n) {
+  // ...
+}
