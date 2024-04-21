2eb2a3e012312c09fb7c6e09ea09de695e3b8e65
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-21 19:58:34 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2710,6 +2710,10 @@ function unluckyDays(year) {
 
 function order(words) {
   // ...
+  return words
+    .split(" ")
+    .sort((a, b) => a.match(/\d+/) - b.match(/\d+/))
+    .join(" ");
 }
 
 console.log(order("is2 Thi1s T4est 3a")); //, "Thi1s is2 3a T4est")
