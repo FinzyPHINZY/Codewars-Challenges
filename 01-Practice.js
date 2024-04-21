960abb210ac65b5094a269ea053136ac20bc83e7
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-21 20:49:55 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2716,6 +2716,79 @@ function order(words) {
     .join(" ");
 }
 
-console.log(order("is2 Thi1s T4est 3a")); //, "Thi1s is2 3a T4est")
-console.log(order("4of Fo1r pe6ople g3ood th5e the2")); //, "Fo1r the2 g3ood 4of th5e pe6ople")
-console.log(order("")); //, "", "empty input should return empty string" )
+// console.log(order("is2 Thi1s T4est 3a")); //, "Thi1s is2 3a T4est")
+// console.log(order("4of Fo1r pe6ople g3ood th5e the2")); //, "Fo1r the2 g3ood 4of th5e pe6ople")
+// console.log(order("")); //, "", "empty input should return empty string" )
+
+// DESCRIPTION:
+// You will be given an array of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+// Your task is to return an object which includes the count of food options selected by the developers on the meetup sign-up form..
+// For example, given the following input array:
+// var list1 = [
+//   { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C',
+//     meal: 'vegetarian' },
+//   { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript',
+//     meal: 'standard' },
+//   { firstName: 'Ramona', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby',
+//     meal: 'vegan' },
+//   { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C',
+//     meal: 'vegetarian' },
+// ];
+// your function should return the following object (the order of properties does not matter):
+// { vegetarian: 2, standard: 1, vegan: 1 }
+// Notes:
+// The order of the meals count in the object does not matter.
+// The count value should be a valid number.
+// The input array will always be valid and formatted as in the example above.
+// there are 5 possible meal options and the strings representing the selected meal option will always be formatted in the same way, as follows: 'standard', 'vegetarian', 'vegan', 'diabetic', 'gluten-intolerant'.
+
+// describe("Tests", () => {
+//   it("test", () => {
+var list1 = [
+  {
+    firstName: "Noah",
+    lastName: "M.",
+    country: "Switzerland",
+    continent: "Europe",
+    age: 19,
+    language: "C",
+    meal: "vegetarian",
+  },
+  {
+    firstName: "Anna",
+    lastName: "R.",
+    country: "Liechtenstein",
+    continent: "Europe",
+    age: 52,
+    language: "JavaScript",
+    meal: "standard",
+  },
+  {
+    firstName: "Ramona",
+    lastName: "R.",
+    country: "Paraguay",
+    continent: "Americas",
+    age: 29,
+    language: "Ruby",
+    meal: "vegan",
+  },
+  {
+    firstName: "George",
+    lastName: "B.",
+    country: "England",
+    continent: "Europe",
+    age: 81,
+    language: "C",
+    meal: "vegetarian",
+  },
+];
+
+// Test.assertDeepEquals(orderFood(list1), answer);
+//   });
+// });
+
+function orderFood(list) {
+  // thank you for checking out the Coding Meetup kata :)
+}
+
+console.log(orderFood(list1)); // var answer = { vegetarian: 2, standard: 1, vegan: 1 };
