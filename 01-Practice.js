4ccd17f163f594b86e2602d5969dd532e17c6c0a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-26 04:36:44 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2830,3 +2830,18 @@ function digitalRoot(n) {
 // console.log(digitalRoot(942));
 // console.log(digitalRoot(132189));
 // console.log(digitalRoot(493193));
+
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+function domainName(url) {
+  //your code here
+}
+
+console.log(domainName("http://google.com")); //, "google");
+console.log(domainName("http://google.co.jp")); //, "google");
+console.log(domainName("www.xakep.ru")); //, "xakep");
+console.log(domainName("https://youtube.com")); //, "youtube");
