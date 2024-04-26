3bbc96a3c40115d9185b2ddbc50bfdeac701db50
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-26 04:39:08 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -2839,9 +2839,14 @@ function digitalRoot(n) {
 
 function domainName(url) {
   //your code here
+  url = url.replace("https://", "");
+  url = url.replace("http://", "");
+  url = url.replace("www.", "").split(".")[0];
+
+  return url;
 }
 
-console.log(domainName("http://google.com")); //, "google");
-console.log(domainName("http://google.co.jp")); //, "google");
-console.log(domainName("www.xakep.ru")); //, "xakep");
-console.log(domainName("https://youtube.com")); //, "youtube");
+// console.log(domainName("http://google.com")); //, "google");
+// console.log(domainName("http://google.co.jp")); //, "google");
+// console.log(domainName("www.xakep.ru")); //, "xakep");
+// console.log(domainName("https://youtube.com")); //, "youtube");
