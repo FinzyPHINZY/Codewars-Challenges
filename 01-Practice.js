721ed883961dd208112eb3ea438f326d07577420
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-04-26 05:09:03 +0100

    solution to kata

--- 01-Practice.js
+++ 01-Practice.js
@@ -2940,7 +2940,30 @@ function digitalRoot(n) {
   return digitalRoot(sum);
 }
 
-console.log(digitalRoot(16));
-console.log(digitalRoot(942));
-console.log(digitalRoot(132189));
-console.log(digitalRoot(493193));
+// console.log(digitalRoot(16));
+// console.log(digitalRoot(942));
+// console.log(digitalRoot(132189));
+// console.log(digitalRoot(493193));
+
+// DESCRIPTION:
+// Find the missing letter
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+// Example:
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+// (Use the English alphabet with 26 letters!)
+// Have fun coding it and please don't forget to vote and rank this kata! :-)
+// I have also created other katas. Take a look if you enjoyed this kata!
+
+function findMissingLetter(array) {
+  const letter =
+    array[0] == array[0].toUpperCase()
+      ? "abcdefghijklmnopqrstuvwxyz"
+      : "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return letter;
+}
+
+console.log(findMissingLetter(["a", "b", "c", "d", "f"]));
+console.log(findMissingLetter(["O", "Q", "R", "S"]));
