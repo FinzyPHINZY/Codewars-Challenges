1ac30d27616aa88a7793416e34f3b87fd3539381
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-01 05:23:10 +0100

    added test cases

--- Dashatize it.js
+++ Dashatize it.js
@@ -10,3 +10,11 @@
 function dashatize(num) {
   return "";
 }
+
+console.log(dashatize(274)); // "2-7-4");
+console.log(dashatize(5311)); // "5-3-1-1");
+console.log(dashatize(86320)); // "86-3-20");
+console.log(dashatize(974302)); // "9-7-4-3-02");
+console.log(dashatize(0)); // "0");
+console.log(dashatize(-1)); // "1");
+console.log(dashatize(-28369)); // "28-3-6-9");
