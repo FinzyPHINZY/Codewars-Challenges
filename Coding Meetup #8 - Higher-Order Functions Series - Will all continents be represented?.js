4847898a7003ea6ee9b5daaefac8be820084e887
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-01 04:59:08 +0100

    added test cases

--- Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?.js
+++ Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?.js
@@ -24,3 +24,60 @@
 function allContinents(list) {
   // thank you for checking out the Coding Meetup kata :)
 }
+
+var list1 = [
+  {
+    firstName: "Fatima",
+    lastName: "A.",
+    country: "Algeria",
+    continent: "Africa",
+    age: 25,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Agustín",
+    lastName: "M.",
+    country: "Chile",
+    continent: "Americas",
+    age: 37,
+    language: "C",
+  },
+  {
+    firstName: "Jing",
+    lastName: "X.",
+    country: "China",
+    continent: "Asia",
+    age: 39,
+    language: "Ruby",
+  },
+  {
+    firstName: "Laia",
+    lastName: "P.",
+    country: "Andorra",
+    continent: "Europe",
+    age: 55,
+    language: "Ruby",
+  },
+  {
+    firstName: "Oliver",
+    lastName: "Q.",
+    country: "Australia",
+    continent: "Oceania",
+    age: 65,
+    language: "PHP",
+  },
+];
+
+var list2 = [
+  {
+    firstName: "Fatima",
+    lastName: "A.",
+    country: "Algeria",
+    continent: "Africa",
+    age: 25,
+    language: "JavaScript",
+  },
+];
+
+console.log(allContinents(list1)); //, true);
+console.log(allContinents(list2)); // false);
