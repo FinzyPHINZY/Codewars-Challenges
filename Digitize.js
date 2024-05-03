8ecb1022b4aba1eb6e99313c929560fb581ff9a9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-03 09:19:43 +0100

    added test cases

--- Digitize.js
+++ Digitize.js
@@ -11,3 +11,9 @@
 // 8675309 => [8,6,7,5,3,0,9]
 
 function digitize(n) {}
+
+console.log(digitize(123)); //, [1,2,3])
+console.log(digitize(1)); //, [1])
+console.log(digitize(0)); //, [0])
+console.log(digitize(1230)); //, [1,2,3, 0])
+console.log(digitize(8675309)); //, [8,6,7,5,3,0,9])
