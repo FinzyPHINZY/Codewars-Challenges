d5d3a0fafc899a091c7510ac05313a0d854fd493
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-04 04:24:04 +0100

    added test cases

--- Sort Arrays (Ignoring Case).js
+++ Sort Arrays (Ignoring Case).js
@@ -8,3 +8,8 @@
 // input: names - unsorted strings
 // output: case-agnostic sort
 sortme = function (names) {};
+
+console.log(sortme(["Hello", "there", "I'm", "fine"])); //, ["fine", "Hello", "I'm", "there"])
+console.log(sortme(["C", "d", "a", "B"])); //, ["a", "B", "C", "d"])
+console.log(sortme(["CodeWars"])); //, ["CodeWars"])
+console.log(sortme([])); //, [])
