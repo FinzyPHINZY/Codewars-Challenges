64521df1bf646464fc76d804dc3c4c99127741c9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-04 04:42:40 +0100

    added solution to kata

--- Sort Arrays (Ignoring Case).js
+++ Sort Arrays (Ignoring Case).js
@@ -7,7 +7,11 @@
 
 // input: names - unsorted strings
 // output: case-agnostic sort
-sortme = function (names) {};
+const sortme = function (names) {
+  return names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+  return names.sort((a, b) => a.localeCompare(b));
+};
 
 console.log(sortme(["Hello", "there", "I'm", "fine"])); //, ["fine", "Hello", "I'm", "there"])
 console.log(sortme(["C", "d", "a", "B"])); //, ["a", "B", "C", "d"])
