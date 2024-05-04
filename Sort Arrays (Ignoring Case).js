b8824fd9b8024236b5b4234cfc834ccee49e076f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-04 04:22:22 +0100

    added starter code

--- Sort Arrays (Ignoring Case).js
+++ Sort Arrays (Ignoring Case).js
@@ -4,3 +4,7 @@
 
 // ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
 // ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+// input: names - unsorted strings
+// output: case-agnostic sort
+sortme = function (names) {};
