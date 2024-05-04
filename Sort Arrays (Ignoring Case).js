5ff49445fa60f2df3d87d63f51c4a33010796f9c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-04 04:21:53 +0100

    added kata description

--- Sort Arrays (Ignoring Case).js
+++ Sort Arrays (Ignoring Case).js
@@ -1,1 +1,6 @@
 "use strict";
+
+// Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+// ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+// ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
