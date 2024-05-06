68e13fc6d81e96c5945e8d154c54f753e46747a2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-06 11:42:43 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -3394,7 +3394,9 @@ function sumOfIntegersInString(s) {
 // ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
 // ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
 
-const sortme = function (names) {};
+const sortme = function (names) {
+  return names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+};
 
 console.log(sortme(["Hello", "there", "I'm", "fine"])); //, "fine","Hello", "I'm","there",]);
 console.log(sortme(["C", "d", "a", "B"])); //, ["a", "B", "C", "d"]);
