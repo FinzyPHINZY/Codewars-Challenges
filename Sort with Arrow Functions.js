c1ff6b78346b7ffc7b99f874afc5415965e66c9f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-06 13:15:54 +0100

    added kata description

--- Sort with Arrow Functions.js
+++ Sort with Arrow Functions.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// Order People by age Using Arrow Function
+// Sort and Order people by their age using Arrow Functions
+// Task
+// Your task is to order a list containg people objects by age using the new Javascript Arrow Functions
+// Input
+// Input will be a valid array with People objects containing an Age and Name
+// Output
+// Output will be a valid sorted array with People objects sorted by Age in ascending order
