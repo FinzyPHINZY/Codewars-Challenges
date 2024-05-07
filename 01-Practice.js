87f674d141378be639b4c92a6ef94e00f51c99ec
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-07 04:22:17 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -3394,11 +3394,23 @@ function sumOfIntegersInString(s) {
 // ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
 // ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
 
-const sortme = function (names) {
-  return names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-};
+// const sortme = function (names) {
+//   return names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+// };
 
-console.log(sortme(["Hello", "there", "I'm", "fine"])); //, "fine","Hello", "I'm","there",]);
+// console.log(sortme(["Hello", "there", "I'm", "fine"])); //, "fine","Hello", "I'm","there",]);
+// console.log(sortme(["C", "d", "a", "B"])); //, ["a", "B", "C", "d"]);
+// console.log(sortme(["CodeWars"])); //, ["CodeWars"]);
+// console.log(sortme([])); //, []);
+
+// Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+// ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+// ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+const sortme = function (names) {};
+
+console.log(sortme(["Hello", "there", "I'm", "fine"])); //, ["fine","Hello","I'm","there",]);
 console.log(sortme(["C", "d", "a", "B"])); //, ["a", "B", "C", "d"]);
 console.log(sortme(["CodeWars"])); //, ["CodeWars"]);
 console.log(sortme([])); //, []);
