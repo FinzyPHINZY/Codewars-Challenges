7b1ee2ef7a78d8a9902dc56b7b66a374e3149bcd
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-08 09:43:47 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -3425,6 +3425,11 @@ const sortme = function (names) {
 
 function domainName(url) {
   //your code here
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .split(".")[0];
 }
 
 console.log(domainName("http://google.com")); //, "google");
