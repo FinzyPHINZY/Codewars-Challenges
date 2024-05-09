d964eb037b6525b91824249f59d29522c409a6cf
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-09 11:20:57 +0100

    added solution to kata

--- Prefill an Array.js
+++ Prefill an Array.js
@@ -24,7 +24,31 @@
 //     prefill("xyz", 1)
 //       --> throws TypeError with message "xyz is invalid"
 
-function prefill(n, v) {}
+function prefill(n, v) {
+  if (typeof n === "boolean") {
+    throw new TypeError(`${n} is invalid`);
+  }
+
+  if (typeof n === "string") {
+    const integerRegex = /^\d+$/;
+    const floatRegex = /^\d*\.\d*$/;
+
+    if (n.match(integerRegex) || n.match(floatRegex)) {
+      n = parseFloat(n);
+    }
+  }
+
+  if (isNaN(n) || n < 0 || n % 1 !== 0) {
+    throw new TypeError(`${n} is invalid`);
+  }
+  const arr = [];
+
+  for (let i = 0; i < parseInt(n); i++) {
+    arr.push(v);
+  }
+
+  return arr;
+}
 
 console.log(prefill(3, 1)); //, [1,1,1]);
 console.log(prefill(2, "abc")); //, ['abc','abc']);
