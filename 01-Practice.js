1dad1e42abbe01d1279137c857012c064d0689df
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-09 09:34:22 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -3432,7 +3432,33 @@ function domainName(url) {
     .split(".")[0];
 }
 
-console.log(domainName("http://google.com")); //, "google");
-console.log(domainName("http://google.co.jp")); // , "google");
-console.log(domainName("www.xakep.ru")); //, "xakep");
-console.log(domainName("https://youtube.com")); //, "youtube");
+// console.log(domainName("http://google.com")); //, "google");
+// console.log(domainName("http://google.co.jp")); // , "google");
+// console.log(domainName("www.xakep.ru")); //, "xakep");
+// console.log(domainName("https://youtube.com")); //, "youtube");
+
+// DESCRIPTION:
+// This is a spin off of my first kata.
+// You are given a string containing a sequence of character sequences separated by commas.
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+// Examples
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+
+function array(string) {
+  // TODO
+}
+
+console.log(array("")); //, null);
+console.log(array("1")); //, null);
+console.log(array("A1,B2")); //, null);
+console.log(array("1,2,3")); //, "2");
+console.log(array("1,2,3,4")); //, "2 3");
+console.log(array("A1,B2,C3,D4,E5")); //, "B2 C3 D4");
+console.log(array("A,1,23,456,78,9,Z")); //, "1 23 456 78 9");
