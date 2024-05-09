e003e5c91d7e5b29bfe887b470a6f2ee8f21321a
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-09 10:14:09 +0100

    added test cases

--- Length of missing array.js
+++ Length of missing array.js
@@ -15,5 +15,23 @@
 // There will always be a missing element and its length will be always between the given arrays.
 
 function getLengthOfMissingArray(arrayOfArrays) {
-  return 0;
+  return arrayOfArrays;
 }
+
+console.log(
+  getLengthOfMissingArray([[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]])
+); //, 3);
+console.log(
+  getLengthOfMissingArray([[5, 2, 9], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]])
+); //, 2);
+console.log(getLengthOfMissingArray([[null], [null, null, null]])); //, 2);
+console.log(
+  getLengthOfMissingArray([
+    ["a", "a", "a"],
+    ["a", "a"],
+    ["a", "a", "a", "a"],
+    ["a"],
+    ["a", "a", "a", "a", "a", "a"],
+  ])
+); //, 5);
+console.log(getLengthOfMissingArray([])); //, 0);
