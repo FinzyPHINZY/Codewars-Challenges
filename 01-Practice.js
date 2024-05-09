dcfc0c71569656a8c70ab8dd2464b1a11cff1cf5
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-09 09:50:38 +0100

    added solution to kata

--- 01-Practice.js
+++ 01-Practice.js
@@ -3543,14 +3543,17 @@ let reindeer = [
 // ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
 // ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
 
-const sortme = function (names) {};
+const sortme = function (names) {
+  return names.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+  return names.sort((a, b) => a.toLowerCase() - b.toLowerCase());
+};
 
-console.log(sortme(["Hello", "there", "I'm", "fine"]), [
-  "fine",
-  "Hello",
-  "I'm",
-  "there",
-]);
-console.log(sortme(["C", "d", "a", "B"]), ["a", "B", "C", "d"]);
-console.log(sortme(["CodeWars"]), ["CodeWars"]);
-console.log(sortme([]), []);
+// console.log(sortme(["Hello", "there", "I'm", "fine"]), [
+//   "fine",
+//   "Hello",
+//   "I'm",
+//   "there",
+// ]);
+// console.log(sortme(["C", "d", "a", "B"]), ["a", "B", "C", "d"]);
+// console.log(sortme(["CodeWars"]), ["CodeWars"]);
+// console.log(sortme([]), []);
