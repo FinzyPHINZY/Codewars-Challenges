e5e4d111f08a90b8f3d0948e05d7c71e043fc0e1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-10 09:29:50 +0100

    added solution to kata

--- Is Integer Array?.js
+++ Is Integer Array?.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // Write a function with the signature shown below:
-
 // returns true  / True if every element in an array is an integer or a float with no decimals.
 // returns true  / True if array is empty.
 // returns false / False for every other input.
