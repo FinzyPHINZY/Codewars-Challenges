68757978369f02442990c0fb2619b3fdfb55f76f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-12 12:02:00 +0100

    added kata description

--- Did she say hallo?.js
+++ Did she say hallo?.js
@@ -1,1 +1,5 @@
 "use strict";
+
+// you can assume the input is a string.
+// to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
+// function should be case insensitive to pass the tests
