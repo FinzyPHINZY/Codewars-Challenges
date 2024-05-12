37d4ea5bb04147d5862f6914ee049157c846baed
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-12 12:02:36 +0100

    added starter code

--- Did she say hallo?.js
+++ Did she say hallo?.js
@@ -3,3 +3,8 @@
 // you can assume the input is a string.
 // to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
 // function should be case insensitive to pass the tests
+
+function validateHello(greetings) {
+  // res =  //regex here/.test(greetings)
+  // return res
+}
