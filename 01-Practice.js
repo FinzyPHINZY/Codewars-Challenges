7f362e272ca8ce81fd4c998153131156dabd1c88
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-13 11:59:44 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -3601,16 +3601,16 @@ function test(num) {
 //   });
 // }
 
-console.log(
-  bingo(
-    [
-      ["ABC", 65],
-      ["HGR", 74],
-      ["BYHT", 74],
-    ],
-    2
-  )
-); //'Loser!');
+// console.log(
+//   bingo(
+//     [
+//       ["ABC", 65],
+//       ["HGR", 74],
+//       ["BYHT", 74],
+//     ],
+//     2
+//   )
+// ); //'Loser!');
 // console.log(
 //   bingo(
 //     [
@@ -3639,7 +3639,32 @@ function splitAndMerge(greet, sep) {
   return greet.split(" ").join(sep);
 }
 // For example:
-console.log(splitAndMerge("My name is John", " ")); // ==  "M y n a m e i s J o h n"
-console.log(splitAndMerge("My name is John", "-")); // ==  "M-y n-a-m-e i-s J-o-h-n"
-console.log(splitAndMerge("Hello World!", ".")); // ==  "H.e.l.l.o W.o.r.l.d.!"
-console.log(splitAndMerge("Hello World!", ",")); // ==  "H,e,l,l,o W,o,r,l,d,!"
+// console.log(splitAndMerge("My name is John", " ")); // ==  "M y n a m e i s J o h n"
+// console.log(splitAndMerge("My name is John", "-")); // ==  "M-y n-a-m-e i-s J-o-h-n"
+// console.log(splitAndMerge("Hello World!", ".")); // ==  "H.e.l.l.o W.o.r.l.d.!"
+// console.log(splitAndMerge("Hello World!", ",")); // ==  "H,e,l,l,o W,o,r,l,d,!"
+
+// DESCRIPTION:
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+// For example (Input --> Output):
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+function persistence(num) {
+  //code me
+  let count = 0;
+  while (num > 10) {
+    count++;
+    num = num
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce((sum, num) => sum * num, 1);
+  }
+
+  return count;
+}
+
+// console.log(persistence(39));
+// console.log(persistence(999));
