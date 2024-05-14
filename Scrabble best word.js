6c21d8add5159cbd2b08832eabe502f84eab23f9
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-14 09:11:55 +0100

    added test cases

--- Scrabble best word.js
+++ Scrabble best word.js
@@ -15,3 +15,26 @@
 function getBestWord(points, words) {
   //Return the shortest word realizing the best score
 }
+
+var points = [
+  1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 10, 1, 2, 1, 1, 3, 8, 1, 1, 1, 1, 4, 10, 10, 10,
+  10,
+];
+var simpleWords = ["WHO", "IS", "THE", "BEST", "OF", "US"];
+var rndmWords = [
+  "NOQ",
+  "TXAY",
+  "S",
+  "OM",
+  "ESFT",
+  "CJUKQ",
+  "QL",
+  "QO",
+  "ASTK",
+  "Y",
+];
+var firstBestWord = ["JGPCWVWFW", "JXHNKBJJG"];
+
+console.log(getBestWord(points, simpleWords)); //,0);
+console.log(getBestWord(points, rndmWords)); //,5);
+console.log(getBestWord(points, firstBestWord)); //,0);
