04e6ca0e258dd4762cfec3b7c634e2c668a8cdd0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-16 10:08:08 +0100

    added solution to kata

--- Nesting Structure Comparison.js
+++ Nesting Structure Comparison.js
@@ -19,21 +19,51 @@
 // [ [ [ ], [ ] ] ].sameStructureAs( [ [ 1, 1 ] ] );
 // For your convenience, there is already a function 'isArray(o)' declared and defined that returns true if its argument is an array, false otherwise.
 
+function isArray(o) {
+  return Array.isArray(o);
+}
+
 Array.prototype.sameStructureAs = function (other) {
   // Return 'true' if and only if 'other' has the same
   // nesting structure as 'this'.
   // Note: You are given a function isArray(o) that returns
   // whether its argument is an array.
+
+  if (!isArray(other)) {
+    return false;
+  }
+
+  if (this.length !== other.length) {
+    return false;
+  }
+
+  for (let i = 0; i < this.length; i++) {
+    const thisElement = this[i];
+    const otherElement = other[i];
+
+    if (isArray(thisElement) && isArray(otherElement)) {
+      // Both are arrays, check their structure recursively
+      if (!thisElement.sameStructureAs(otherElement)) {
+        return false;
+      }
+    } else if (isArray(thisElement) !== isArray(otherElement)) {
+      // One is an array and the other is not
+      return false;
+    }
+    // If both are not arrays, continue checking next elements
+  }
+
+  return true;
 };
 
-assert.isTrue([1, 1, 1].sameStructureAs([2, 2, 2])); //, "[1,1,1] same as [2,2,2]");
-assert.isTrue([1, [1, 1]].sameStructureAs([2, [2, 2]])); //, "[1,[1,1]] same as [2,[2,2]]");
-assert.isNotTrue([1, [1, 1]].sameStructureAs([[2, 2], 2])); //, "[1,[1,1]] not same as [[2,2],2]");
-assert.isNotTrue([1, [1, 1]].sameStructureAs([2, [2]])); //, "[1,[1,1]] not same as [2,[2]]");
-assert.isTrue([[[], []]].sameStructureAs([[[], []]])); //, "[[[],[]]] same as [[[],[]]]");
-assert.isNotTrue([[[], []]].sameStructureAs([[1, 1]])); //, "[[[],[]]] not same as [[1,1]]");
-assert.isTrue([1, [[[1]]]].sameStructureAs([2, [[[2]]]])); //, "[1,[[[1]]]] same as [2,[[[2]]]]");
-assert.isNotTrue([].sameStructureAs(1)); //, "[] not same as 1");
-assert.isNotTrue([].sameStructureAs({})); //, "[] not same as {}");
-assert.isTrue([1, "[", "]"].sameStructureAs(["[", "]", 1])); //, "[1,'[',']'] same as ['[',']',1]");
-assert.isNotTrue([1, 2].sameStructureAs([[3], 3])); //, "[1,2] not same as [[3],3]" );
+console.log([1, 1, 1].sameStructureAs([2, 2, 2])); //, "[1,1,1] same as [2,2,2]");
+console.log([1, [1, 1]].sameStructureAs([2, [2, 2]])); //, "[1,[1,1]] same as [2,[2,2]]");
+console.log([1, [1, 1]].sameStructureAs([[2, 2], 2])); //, "[1,[1,1]] not same as [[2,2],2]");
+console.log([1, [1, 1]].sameStructureAs([2, [2]])); //, "[1,[1,1]] not same as [2,[2]]");
+console.log([[[], []]].sameStructureAs([[[], []]])); //, "[[[],[]]] same as [[[],[]]]");
+console.log([[[], []]].sameStructureAs([[1, 1]])); //, "[[[],[]]] not same as [[1,1]]");
+console.log([1, [[[1]]]].sameStructureAs([2, [[[2]]]])); //, "[1,[[[1]]]] same as [2,[[[2]]]]");
+console.log([].sameStructureAs(1)); //, "[] not same as 1");
+console.log([].sameStructureAs({})); //, "[] not same as {}");
+console.log([1, "[", "]"].sameStructureAs(["[", "]", 1])); //, "[1,'[',']'] same as ['[',']',1]");
+console.log([1, 2].sameStructureAs([[3], 3])); //, "[1,2] not same as [[3],3]" );
