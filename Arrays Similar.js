bb601a7e6276bdb2cd6bf811d2fa2fc3632bc597
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-16 10:22:44 +0100

    added solution to kata

--- Arrays Similar.js
+++ Arrays Similar.js
@@ -13,6 +13,11 @@
 
 function arraysSimilar(arr1, arr2) {
   // TODO: Implement your arraysSimilar
+  if (arr1.length !== arr2.length) return false;
+
+  arr1 = arr1.sort((a, b) => a - b);
+  arr2 = arr2.sort((a, b) => a - b);
+  return arr1.every((char, i) => arr1[i] === arr2[i]);
 }
 
 const arr1 = [1, 2, 2, 3, 4],
