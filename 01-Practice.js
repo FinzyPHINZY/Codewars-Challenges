c8cf4746da2d4d882645177bf5aaa79c892a226d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-17 12:05:04 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -3805,3 +3805,34 @@ function cleanString(s) {
 
 // console.log(cleanString("abc#d##c")); //'ac');
 // console.log(cleanString("abc####d##c#")); //, '');
+
+("use strict");
+
+// Happy Holidays fellow Code Warriors!
+// Now, Dasher! Now, Dancer! Now, Prancer, and Vixen! On, Comet! On, Cupid! On, Donder and Blitzen! That's the order Santa wanted his reindeer...right? What do you mean he wants them in order by their last names!? Looks like we need your help Code Warrior!
+
+// Sort Santa's Reindeer
+// Write a function that accepts a sequence of Reindeer names, and returns a sequence with the Reindeer names sorted by their last names.
+
+// Notes:
+// It's guaranteed that each string is composed of two words
+// In case of two identical last names, keep the original order
+// Examples
+// For this input:
+
+function sortReindeer(reindeerNames) {}
+
+let inputArr = [
+  "Dasher Tonoyan",
+  "Dancer Moore",
+  "Prancer Chua",
+  "Vixen Hall",
+  "Comet Karavani",
+  "Cupid Foroutan",
+  "Donder Jonker",
+  "Blitzen Claus",
+];
+// let output = [
+//   "Prancer Chua","Blitzen Claus","Cupid Foroutan","Vixen Hall","Donder Jonker","Comet Karavani","Dancer Moore","Dasher Tonoyan",
+// ];
+console.log(sortReindeer(inputArr)); //, output);
