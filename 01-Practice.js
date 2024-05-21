ca8180d5b4d84b03909ea6010c71f2dff0cabe08
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-21 10:20:54 +0100

    added solutio to kata

--- 01-Practice.js
+++ 01-Practice.js
@@ -4012,10 +4012,29 @@ function duplicateCount(text) {
   return multiples;
 }
 
-console.log(duplicateCount("abcde"));
-console.log(duplicateCount("aabbcde"));
-console.log(duplicateCount("aabBcde"));
-console.log(duplicateCount("indivisibility"));
-console.log(duplicateCount("Indivisibilities"));
-console.log(duplicateCount("aA11"));
-console.log(duplicateCount("ABBA"));
+// console.log(duplicateCount("abcde"));
+// console.log(duplicateCount("aabbcde"));
+// console.log(duplicateCount("aabBcde"));
+// console.log(duplicateCount("indivisibility"));
+// console.log(duplicateCount("Indivisibilities"));
+// console.log(duplicateCount("aA11"));
+// console.log(duplicateCount("ABBA"));
+
+// DESCRIPTION:
+// Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
+
+// In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+// 'X' --> busy
+// 'O' --> empty
+// If all rooms are busy, return "None available!"
+
+function meeting(x) {
+  if (!x.includes("O")) return "None available!";
+
+  return x.indexOf("O");
+}
+
+console.log(meeting(["X", "O", "X"])); //, 1);
+console.log(meeting(["O", "X", "X", "X", "X"])); //, 0);
+console.log(meeting(["X", "X", "X", "X", "X"])); //, 'None available!');
