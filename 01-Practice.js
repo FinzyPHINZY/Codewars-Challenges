0de41c9b52f8d259bb594193df2e4f7a7c457ce1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-24 09:36:02 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -4135,7 +4135,26 @@ function domainName(url) {
   return url.split(".")[0];
 }
 
-console.log(domainName("http://google.com")); //  "google");
-console.log(domainName("http://google.co.jp")); //  "google");
-console.log(domainName("www.xakep.ru")); //, "xakep");
-console.log(domainName("https://youtube.com")); // "youtube");
+// console.log(domainName("http://google.com")); //  "google");
+// console.log(domainName("http://google.co.jp")); //  "google");
+// console.log(domainName("www.xakep.ru")); //, "xakep");
+// console.log(domainName("https://youtube.com")); // "youtube");
+
+// You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+
+// Examples
+// []                   -->  0
+// [1, 2, 3]            -->  3
+// ["x", "y", ["z"]]    -->  4
+// [1, 2, [3, 4, [5]]]  -->  7
+// The input will always be an array.
+
+function deepCount(a) {
+  //...
+}
+
+console.log(deepCount([])); //, 0, "Expected 0");
+console.log(deepCount([1, 2, 3])); //, 3, "Expected 3");
+console.log(deepCount(["x", "y", ["z"]])); //, 4, "Expected 4");
+console.log(deepCount([1, 2, [3, 4, [5]]])); //, 7, "Expected 7");
+console.log(deepCount([[[[[[[[[]]]]]]]]])); //, 8, "Expected 8");
