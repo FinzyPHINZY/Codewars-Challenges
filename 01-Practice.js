c4379b14c0f88dfa10ba941abb5c81d16b9c6b04
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-24 09:34:49 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -4128,9 +4128,14 @@ function findMissingLetter(array) {
 
 function domainName(url) {
   //your code here
+  url = url.replace("https://", "");
+  url = url.replace("http://", "");
+  url = url.replace("www.", "");
+
+  return url.split(".")[0];
 }
 
-console.log(domainName("http://google.com")); // "google");
-console.log(domainName("http://google.co.jp")); // "google");
+console.log(domainName("http://google.com")); //  "google");
+console.log(domainName("http://google.co.jp")); //  "google");
 console.log(domainName("www.xakep.ru")); //, "xakep");
-console.log(domainName("https://youtube.com")); //"youtube");
+console.log(domainName("https://youtube.com")); // "youtube");
