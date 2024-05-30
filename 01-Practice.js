53b923bdebb8bf9a5e5f08b93d7941788dccfabb
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-05-30 10:44:47 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -4266,7 +4266,26 @@ function sortByValueAndIndex(array) {
     .map((value) => value.value);
 }
 
-console.log(sortByValueAndIndex([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ];
-console.log(sortByValueAndIndex([23, 2, 3, 4, 5])); // [ 2, 3, 4, 23, 5 ];
-console.log(sortByValueAndIndex([26, 2, 3, 4, 5])); // [ 2, 3, 4, 5, 26 ];
-console.log(sortByValueAndIndex([9, 5, 1, 4, 3])); // [ 1, 9, 5, 3, 4 ];
+// console.log(sortByValueAndIndex([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ];
+// console.log(sortByValueAndIndex([23, 2, 3, 4, 5])); // [ 2, 3, 4, 23, 5 ];
+// console.log(sortByValueAndIndex([26, 2, 3, 4, 5])); // [ 2, 3, 4, 5, 26 ];
+// console.log(sortByValueAndIndex([9, 5, 1, 4, 3])); // [ 1, 9, 5, 3, 4 ];
+
+// You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+
+// Examples
+// []                   -->  0
+// [1, 2, 3]            -->  3
+// ["x", "y", ["z"]]    -->  4
+// [1, 2, [3, 4, [5]]]  -->  7
+// The input will always be an array.
+
+function deepCount(a) {
+  //...
+}
+
+console.log(deepCount([])); //, 0, "Expected 0");
+console.log(deepCount([1, 2, 3])); //, 3, "Expected 3");
+console.log(deepCount(["x", "y", ["z"]])); //, 4, "Expected 4");
+console.log(deepCount([1, 2, [3, 4, [5]]])); //, 7, "Expected 7");
+console.log(deepCount([[[[[[[[[]]]]]]]]])); //, 8, "Expected 8");
