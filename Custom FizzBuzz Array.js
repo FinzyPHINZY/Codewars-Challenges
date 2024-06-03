a8d585fd3c0aa4158d0e023c5b79de787e15be48
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-03 10:42:10 +0100

    added solution to kata

--- Custom FizzBuzz Array.js
+++ Custom FizzBuzz Array.js
@@ -15,8 +15,25 @@
 // ('Foo', 'Bar', 2, 3)  -->  [ 1, "Foo", "Bar", "Foo", 5, "FooBar", 7, ... ]
 // Examples
 
-const fizzBuzzCustom = function (stringOne, stringTwo, numOne, numTwo) {
-  console.log(stringOne, stringTwo, numOne, numTwo);
+const fizzBuzzCustom = function (
+  stringOne = "Fizz",
+  stringTwo = "Buzz",
+  numOne = 3,
+  numTwo = 5
+) {
+  let result = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % numOne === 0 && i % numTwo === 0) {
+      result.push(stringOne + stringTwo);
+    } else if (i % numOne === 0) {
+      result.push(stringOne);
+    } else if (i % numTwo === 0) {
+      result.push(stringTwo);
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
 };
 
 console.log(fizzBuzzCustom()); //[15]); //; // returns 16
