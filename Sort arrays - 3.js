eda4ab497a0c311f882d8627576cda90975509fc
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-05 09:45:25 +0100

    added test cases

--- Sort arrays - 3.js
+++ Sort arrays - 3.js
@@ -8,3 +8,17 @@
 // input: courses - array of course-names "name-yymm"
 // output: sorted by "yymm", then "name"
 sortme = function (courses) {};
+
+console.log(sortme(["web-1305", "site-1305", "web-1304", "site-1304"])); //, ["site-1304", "web-1304", "site-1305", "web-1305"])
+console.log(
+  sortme([
+    "aeb-1305",
+    "site-1305",
+    "play-1215",
+    "web-1304",
+    "site-1304",
+    "beb-1305",
+  ])
+); // //, ["play-1215", "site-1304", "web-1304", "aeb-1305", "beb-1305", "site-1305"])
+console.log(sortme([]), []);
+console.log(sortme(["aeb-1305", "aeb-1305"]), ["aeb-1305", "aeb-1305"]);
