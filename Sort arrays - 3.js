74ce3f1f73fe64d4e4fb394147040258025eadd8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-05 09:57:39 +0100

    added solution to kata

--- Sort arrays - 3.js
+++ Sort arrays - 3.js
@@ -7,18 +7,28 @@
 
 // input: courses - array of course-names "name-yymm"
 // output: sorted by "yymm", then "name"
-sortme = function (courses) {};
+const sortme = function (courses) {
+  return courses.sort((a, b) => {
+    const [nameA, yymmA] = a.split("-");
+    const [nameB, yymmB] = b.split("-");
+
+    if (yymmA === yymmB) {
+      return nameA.localeCompare(nameB); // Sort by name if yymm is the same
+    }
+    return yymmA.localeCompare(yymmB); // Sort by yymm
+  });
+};
 
 console.log(sortme(["web-1305", "site-1305", "web-1304", "site-1304"])); //, ["site-1304", "web-1304", "site-1305", "web-1305"])
-console.log(
-  sortme([
-    "aeb-1305",
-    "site-1305",
-    "play-1215",
-    "web-1304",
-    "site-1304",
-    "beb-1305",
-  ])
-); // //, ["play-1215", "site-1304", "web-1304", "aeb-1305", "beb-1305", "site-1305"])
+// console.log(
+//   sortme([
+//     "aeb-1305",
+//     "site-1305",
+//     "play-1215",
+//     "web-1304",
+//     "site-1304",
+//     "beb-1305",
+//   ])
+// ); // //, ["play-1215", "site-1304", "web-1304", "aeb-1305", "beb-1305", "site-1305"])
 console.log(sortme([]), []);
-console.log(sortme(["aeb-1305", "aeb-1305"]), ["aeb-1305", "aeb-1305"]);
+// console.log(sortme(["aeb-1305", "aeb-1305"]), ["aeb-1305", "aeb-1305"]);
