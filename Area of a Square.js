1ede07eefbef6e0424ddedf8919f9723164b8968
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-06 11:16:54 +0100

    added test case

--- Area of a Square.js
+++ Area of a Square.js
@@ -7,3 +7,7 @@
 // Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
 
 function squareArea(A) {}
+
+console.log(squareArea(2)); //, 1.62, 1e-2);
+console.log(squareArea(0)); //, 0, 1e-2);
+console.log(squareArea(14.05)); //, 80, 1e-2);
