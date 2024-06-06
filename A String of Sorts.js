ee4dc842f7b14338e5bf005efb8a10b5b5f99848
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-06 10:57:53 +0100

    added test cases

--- A String of Sorts.js
+++ A String of Sorts.js
@@ -14,3 +14,7 @@
 function sortString(string, ordering) {
   return string;
 }
+
+console.log(sortString("foos", "of")); //, "oofs");
+console.log(sortString("string", "gnirts")); //, "gnirts");
+console.log(sortString("banana", "abn")); //, "aaabnn");
