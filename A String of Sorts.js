9081043111717b7045d2593fffd87affa63664a3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-06 11:13:13 +0100

    added solution to kata

--- A String of Sorts.js
+++ A String of Sorts.js
@@ -11,10 +11,20 @@
 
 // STRINGSSORTINGALGORITHMS
 
-function sortString(string, ordering) {
-  return string;
-}
+const sortString = (source, order) => {
+  return order
+    .split("")
+    .filter((x, i, a) => a.indexOf(x) == i)
+    .map((c) => source.split("").filter((i) => i === c))
+    .reduce((acc, a) => acc.concat(a), [])
+    .concat(source.split("").filter((c) => !order.includes(c)))
+    .join("");
+};
 
 console.log(sortString("foos", "of")); //, "oofs");
 console.log(sortString("string", "gnirts")); //, "gnirts");
 console.log(sortString("banana", "abn")); //, "aaabnn");
+
+// sortString :- takes in two arguments(string - string to modify, ordering - blueprint for modification).
+// create return variable - empty string.
+//
