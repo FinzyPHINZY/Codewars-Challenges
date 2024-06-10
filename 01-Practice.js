ba7977be3d9b573d8dbb382a560f919acc9e17dc
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-10 12:03:41 +0100

    added solution to practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -4428,10 +4428,10 @@ function sortByValueAndIndex(array) {
 // Create a function that returns the CSV representation of a two-dimensional numeric array.
 // Example:
 // input:
-//    [[ 0, 1, 2, 3, 4 ],
-//     [ 10,11,12,13,14 ],
-//     [ 20,21,22,23,24 ],
-//     [ 30,31,32,33,34 ]]
+//  [[ 0, 1, 2, 3, 4 ],
+//   [ 10,11,12,13,14 ],
+//   [ 20,21,22,23,24 ],
+//   [ 30,31,32,33,34 ]]
 
 // output:
 //      '0,1,2,3,4\n'
@@ -4442,4 +4442,14 @@ function sortByValueAndIndex(array) {
 
 function toCsvText(array) {
   // good luck
+  return array.join("\n");
 }
+
+console.log(
+  toCsvText([
+    [0, 1, 2, 3, 4],
+    [10, 11, 12, 13, 14],
+    [20, 21, 22, 23, 24],
+    [30, 31, 32, 33, 34],
+  ])
+);
