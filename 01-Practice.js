d38dfc26ba9bdd69334c1327e3d8f2b0bea9c6bb
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-10 10:27:06 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -4377,7 +4377,33 @@ const sortmeArr = function (names) {
   return names.sort((a, b) => a - b);
 };
 
-console.log(sortme(["Hello", "there", "I'm", "fine"])); //, ["fine", "Hello", "I'm", "there"])
-console.log(sortme(["C", "d", "a", "B"])); //, ["a", "B", "C", "d"])
-console.log(sortme(["CodeWars"])); //, ["CodeWars"])
-console.log(sortme([])); //, [])
+// console.log(sortme(["Hello", "there", "I'm", "fine"])); //, ["fine", "Hello", "I'm", "there"])
+// console.log(sortme(["C", "d", "a", "B"])); //, ["a", "B", "C", "d"])
+// console.log(sortme(["CodeWars"])); //, ["CodeWars"])
+// console.log(sortme([])); //, [])
+
+// Sort an array by value and index
+// Your task is to sort an array of integer numbers by the product of the value and the index of the positions.
+
+// For sorting the index starts at 1, NOT at 0!
+// The sorting has to be ascending.
+// The array will never be null and will always contain numbers.
+
+// Example:
+
+// Input: 23, 2, 3, 4, 5
+// Product of value and index:
+// 23 => 23 * 1 = 23  -> Output-Pos 4
+//  2 =>  2 * 2 = 4   -> Output-Pos 1
+//  3 =>  3 * 3 = 9   -> Output-Pos 2
+//  4 =>  4 * 4 = 16  -> Output-Pos 3
+//  5 =>  5 * 5 = 25  -> Output-Pos 5
+
+// Output: 2, 3, 4, 23, 5
+
+function sortByValueAndIndex(array) {}
+
+console.log(sortByValueAndIndex([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ];
+console.log(sortByValueAndIndex([23, 2, 3, 4, 5])); // [ 2, 3, 4, 23, 5 ];
+console.log(sortByValueAndIndex([26, 2, 3, 4, 5])); // [ 2, 3, 4, 5, 26 ];
+console.log(sortByValueAndIndex([9, 5, 1, 4, 3])); // [ 1, 9, 5, 3, 4 ];
