07768e1e25104d7772ca8023beab3981f641fba6
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-11 08:42:13 +0100

    added new practice

--- 01-Practice.js
+++ 01-Practice.js
@@ -4472,33 +4472,88 @@ function bingo(ticket, win) {
   return count >= win ? "Winner!" : "Loser!";
 }
 
-console.log(
-  bingo(
-    [
-      ["ABC", 65],
-      ["HGR", 74],
-      ["BYHT", 74],
-    ],
-    2
-  )
-); //, 'Loser!');
-console.log(
-  bingo(
-    [
-      ["ABC", 65],
-      ["HGR", 74],
-      ["BYHT", 74],
-    ],
-    1
-  )
-); // 'Winner!');
-console.log(
-  bingo(
-    [
-      ["HGTYRE", 74],
-      ["BE", 66],
-      ["JKTY", 74],
-    ],
-    3
-  )
-); //, 'Loser!');
+// console.log(
+//   bingo(
+//     [
+//       ["ABC", 65],
+//       ["HGR", 74],
+//       ["BYHT", 74],
+//     ],
+//     2
+//   )
+// ); //, 'Loser!');
+// console.log(
+//   bingo(
+//     [
+//       ["ABC", 65],
+//       ["HGR", 74],
+//       ["BYHT", 74],
+//     ],
+//     1
+//   )
+// ); // 'Winner!');
+// console.log(
+//   bingo(
+//     [
+//       ["HGTYRE", 74],
+//       ["BE", 66],
+//       ["JKTY", 74],
+//     ],
+//     3
+//   )
+// ); //, 'Loser!');
+
+// DESCRIPTION:
+// A balanced number is a number where the sum of digits to the left of the middle digit(s) and the sum of digits to the right of the middle digit(s) are equal.
+
+// If the number has an odd number of digits, then there is only one middle digit. (For example, 92645 has one middle digit, 6.) Otherwise, there are two middle digits. (For example, the middle digits of 1301 are 3 and 0.)
+
+// The middle digit(s) should not be considered when determining whether a number is balanced or not, e.g. 413023 is a balanced number because the left sum and right sum are both 5.
+
+// The task
+// Given a number, find if it is balanced, and return the string "Balanced" or "Not Balanced" accordingly. The passed number will always be positive.
+
+// Examples
+// 7 ==> return "Balanced"
+// Explanation:
+// middle digit(s): 7
+// sum of all digits to the left of the middle digit(s) -> 0
+// sum of all digits to the right of the middle digit(s) -> 0
+// 0 and 0 are equal, so it's balanced.
+// 295591 ==> return "Not Balanced"
+// Explanation:
+// middle digit(s): 55
+// sum of all digits to the left of the middle digit(s) -> 11
+// sum of all digits to the right of the middle digit(s) -> 10
+// 11 and 10 are not equal, so it's not balanced.
+// 959 ==> return "Balanced"
+// Explanation:
+// middle digit(s): 5
+// sum of all digits to the left of the middle digit(s) -> 9
+// sum of all digits to the right of the middle digit(s) -> 9
+// 9 and 9 are equal, so it's balanced.
+// 27102983 ==> return "Not Balanced"
+// Explanation:
+// middle digit(s): 02
+// sum of all digits to the left of the middle digit(s) -> 10
+// sum of all digits to the right of the middle digit(s) -> 20
+// 10 and 20 are not equal, so it's not balanced.
+
+// Test Cases:
+
+// describe("Check less than thousand", function () {
+//   it("Check balanced number", function () {
+//     Test.assertEquals(balancedNum(7), "Balanced");
+//     Test.assertEquals(balancedNum(959), "Balanced");
+//     Test.assertEquals(balancedNum(13), "Balanced");
+//     Test.assertEquals(balancedNum(432), "Not Balanced");
+//     Test.assertEquals(balancedNum(424), "Balanced");
+//   });
+//   it("Check Larger number", function () {
+//     Test.assertEquals(balancedNum(1024), "Not Balanced");
+//     Test.assertEquals(balancedNum(66545), "Not Balanced");
+//     Test.assertEquals(balancedNum(295591), "Not Balanced");
+//     Test.assertEquals(balancedNum(1230987), "Not Balanced");
+//     Test.assertEquals(balancedNum(56239814), "Balanced");
+//   });
+// });
