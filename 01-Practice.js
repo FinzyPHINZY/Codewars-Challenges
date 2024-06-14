3731cdf41545245d261e3fcc55e00ecec0287dcb
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-06-14 19:59:56 +0100

    Update 01-Practice.js

--- 01-Practice.js
+++ 01-Practice.js
@@ -3635,6 +3635,8 @@ function test(num) {
 // Task
 // Implement a function which accepts 2 arguments: string and separator.
 // The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+
 function splitAndMerge(greet, sep) {
   return greet.split(" ").join(sep);
 }
