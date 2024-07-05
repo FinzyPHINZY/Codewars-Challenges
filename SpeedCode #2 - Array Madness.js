09c05c16fca40f3bbf02173d847dfc03ea404717
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-05 10:09:23 +0100

    added starter code

--- SpeedCode #2 - Array Madness.js
+++ SpeedCode #2 - Array Madness.js
@@ -7,3 +7,7 @@
 
 // arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
 // Get your timer out. Are you ready? Ready, get set, GO!!!
+
+function arrayMadness(a, b) {
+  // Ready, get set, GO!!!
+}
