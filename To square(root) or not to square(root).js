f9cb094381bfb52d3c147e8a572b0504b3db2c51
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-05 09:32:00 +0100

    added test cases

--- To square(root) or not to square(root).js
+++ To square(root) or not to square(root).js
@@ -14,3 +14,12 @@
 function squareOrSquareRoot(array) {
   return array;
 }
+
+var expected = [2, 9, 3, 49, 4, 1];
+console.log(squareOrSquareRoot([4, 3, 9, 7, 2, 1])); //, expected);
+
+expected = [10, 10201, 25, 25, 1, 1];
+console.log(squareOrSquareRoot([100, 101, 5, 5, 1, 1])); //, expected);
+
+expected = [1, 4, 9, 2, 25, 36];
+console.log(squareOrSquareRoot([1, 2, 3, 4, 5, 6])); //, expected);
