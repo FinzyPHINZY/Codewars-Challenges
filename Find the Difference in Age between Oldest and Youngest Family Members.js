c21a0bedf80ac71fb26d0710d3b6f08af97c70ea
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-22 09:14:19 +0100

    addded test cases

--- Find the Difference in Age between Oldest and Youngest Family Members.js
+++ Find the Difference in Age between Oldest and Youngest Family Members.js
@@ -7,3 +7,6 @@
 function differenceInAges(ages) {
   return [0, 0, 0];
 }
+
+console.log(differenceInAges([82, 15, 6, 38, 35])); //), [6, 82, 76]);
+console.log(differenceInAges([57, 99, 14, 32])); //, [14, 99, 85]);
