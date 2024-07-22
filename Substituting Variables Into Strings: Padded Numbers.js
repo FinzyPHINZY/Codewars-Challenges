77082123e6e5b9c61e0b1ef80816d25d0509543d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-22 09:12:30 +0100

    added new kata

--- Substituting Variables Into Strings: Padded Numbers.js
+++ Substituting Variables Into Strings: Padded Numbers.js
@@ -8,9 +8,20 @@
 
 function solution(value) {
   //...
+
+  const padNums = 5 - String(value).length;
+  const newValue = Array(padNums).fill(0).map(Number).join("") + value;
+
+  return `Value is ${newValue}`;
 }
 
 console.log(solution(5)); //, "Value is 00005")
 console.log(solution(1204)); //, "Value is 01204")
 console.log(solution(109)); //, "Value is 00109")
 console.log(solution(0)); //, "Value is 00000")
+
+// PREP
+// P: Function takes in a number argument
+// R: Returns a string of 'Value is ' concatenated with the number as a 5 digit padded number
+// E: Available Above
+// P: In javascript, theres a padstart and padend function that we can use. so thats all i'll do.
