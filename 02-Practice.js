f2c46ec483cde8442306f89cf6072c38a609a192
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-23 09:52:08 +0100

    added new practice

--- 02-Practice.js
+++ 02-Practice.js
@@ -53,6 +53,67 @@ function dullThings(arr) {
   return arr.filter((_, i) => i !== min);
 }
 
-console.log(dullThings([1, 2, 3, 4, 5]));
-console.log(dullThings([5, 3, 2, 1, 4]));
-console.log(dullThings([2, 2, 1, 2, 1]));
+// console.log(dullThings([1, 2, 3, 4, 5]));
+// console.log(dullThings([5, 3, 2, 1, 4]));
+// console.log(dullThings([2, 2, 1, 2, 1]));
+
+("use strict");
+
+// Happy Holidays fellow Code Warriors!
+// Now, Dasher! Now, Dancer! Now, Prancer, and Vixen! On, Comet! On, Cupid! On, Donder and Blitzen! That's the order Santa wanted his reindeer...right? What do you mean he wants them in order by their last names!? Looks like we need your help Code Warrior!
+
+// Sort Santa's Reindeer
+// Write a function that accepts a sequence of Reindeer names, and returns a sequence with the Reindeer names sorted by their last names.
+
+// Notes:
+// It's guaranteed that each string is composed of two words
+// In case of two identical last names, keep the original order
+// Examples
+// For this input:
+
+// [
+//   "Dasher Tonoyan",
+//   "Dancer Moore",
+//   "Prancer Chua",
+//   "Vixen Hall",
+//   "Comet Karavani",
+//   "Cupid Foroutan",
+//   "Donder Jonker",
+//   "Blitzen Claus"
+// ]
+// You should return this output:
+
+// [
+//   "Prancer Chua",
+//   "Blitzen Claus",
+//   "Cupid Foroutan",
+//   "Vixen Hall",
+//   "Donder Jonker",
+//   "Comet Karavani",
+//   "Dancer Moore",
+//   "Dasher Tonoyan",
+// ]
+
+function sortReindeer(reindeerNames) {}
+
+let input = [
+  "Dasher Tonoyan",
+  "Dancer Moore",
+  "Prancer Chua",
+  "Vixen Hall",
+  "Comet Karavani",
+  "Cupid Foroutan",
+  "Donder Jonker",
+  "Blitzen Claus",
+];
+let output = [
+  "Prancer Chua",
+  "Blitzen Claus",
+  "Cupid Foroutan",
+  "Vixen Hall",
+  "Donder Jonker",
+  "Comet Karavani",
+  "Dancer Moore",
+  "Dasher Tonoyan",
+];
+console.log(sortReindeer(input)); //, output);
