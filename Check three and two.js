e8e3df448b9e9983facc6fc1546fd7fb360bf78c
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-23 11:29:31 +0100

    added solution to kata

--- Check three and two.js
+++ Check three and two.js
@@ -9,8 +9,36 @@
 
 function checkThreeAndTwo(array) {
   //your code here
+  const set = new Set(array);
+  if (set.size !== 2) return false;
+
+  let wordObj = {};
+
+  array.forEach((char) => {
+    if (wordObj[char]) {
+      wordObj[char]++;
+    } else {
+      wordObj[char] = 1;
+    }
+  });
+
+  let bools = [];
+
+  for (const key in wordObj) {
+    if (wordObj[key] === 3) {
+      bools.push(true);
+    } else if (wordObj[key] === 2) {
+      //   return true;
+      bools.push(true);
+    }
+  }
+
+  if (bools.length < 2) return false;
+
+  return bools.every((value) => value === true && value !== null);
 }
 
-console.log(["a", "a", "a", "b", "b"]); //, true);
-console.log(["a", "c", "a", "c", "b"]); // false);
-console.log(["a", "a", "a", "a", "a"]); //false );
+console.log(checkThreeAndTwo(["a", "a", "a", "b", "b"])); //, true);
+console.log(checkThreeAndTwo(["a", "c", "a", "c", "b"])); // false);
+console.log(checkThreeAndTwo(["a", "a", "a", "a", "a"])); //false );
+console.log(checkThreeAndTwo(["c", "b", "b", "b", "b"]));
