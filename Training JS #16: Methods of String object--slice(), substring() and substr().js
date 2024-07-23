c949d4b3fd7d5a18fde2d0b8a58ea57ebc80a293
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-23 11:06:07 +0100

    added test cases

--- Training JS #16: Methods of String object--slice(), substring() and substr().js
+++ Training JS #16: Methods of String object--slice(), substring() and substr().js
@@ -101,3 +101,7 @@
 function cutIt(arr) {
   //coding here...
 }
+
+console.log(cutIt(["ab", "cde", "fgh"])); //, ["ab","cd","fg"]);
+console.log(cutIt(["abc", "defgh", "ijklmn"])); // , ["abc","def","ijk"]);
+console.log(cutIt(["codewars", "javascript", "java"])); // , ["code","java","java"]);
