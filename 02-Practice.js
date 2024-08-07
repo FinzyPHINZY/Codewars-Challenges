2ba5e9a946462aa69f1e5550fce1b5872490b69d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-08-07 09:53:52 +0100

    added new practice and solution

--- 02-Practice.js
+++ 02-Practice.js
@@ -323,7 +323,34 @@ function splitAndMerge(string, separator) {
     .join(" ");
 }
 
-console.log(splitAndMerge("My name is John", " ")); // == "M y n a m e i s J o h n";
-console.log(splitAndMerge("My name is John", "-")); // == "M-y n-a-m-e i-s J-o-h-n";
-console.log(splitAndMerge("Hello World!", ".")); // == "H.e.l.l.o W.o.r.l.d.!";
-console.log(splitAndMerge("Hello World!", ",")); // == "H,e,l,l,o W,o,r,l,d,!";
+// console.log(splitAndMerge("My name is John", " ")); // == "M y n a m e i s J o h n";
+// console.log(splitAndMerge("My name is John", "-")); // == "M-y n-a-m-e i-s J-o-h-n";
+// console.log(splitAndMerge("Hello World!", ".")); // == "H.e.l.l.o W.o.r.l.d.!";
+// console.log(splitAndMerge("Hello World!", ",")); // == "H,e,l,l,o W,o,r,l,d,!";
+
+// DESCRIPTION:
+// Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+// Find the number of Friday 13th in the given year.
+// Input: Year in Gregorian calendar as integer.
+// Output: Number of Black Fridays in the year as an integer.
+// Examples:
+
+// unluckyDays(2015) == 3;
+// unluckyDays(1986) == 1;
+
+function unluckyDays(year) {
+  //your code here
+  let count = 0;
+  for (let i = 0; i < 12; i++) {
+    let date = new Date(year, i, 13);
+
+    if (date.getDay() === 5) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(unluckyDays(2015));
+console.log(unluckyDays(1986));
