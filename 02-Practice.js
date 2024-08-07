08cdbc2d25cbd70d35e0393b45615d715a0c62e1
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-08-07 09:36:27 +0100

    added new practice with solution

--- 02-Practice.js
+++ 02-Practice.js
@@ -265,10 +265,43 @@ function array(string) {
 //     });
 //   }
 
-console.log(array("")); //, null);
-console.log(array("1")); //, null);
-console.log(array("A1,B2")); //, null);
-console.log(array("1,2,3")); //, '2');
-console.log(array("1,2,3,4")); //, '2 3');
-console.log(array("A1,B2,C3,D4,E5")); //, 'B2 C3 D4');
-console.log(array("A,1,23,456,78,9,Z")); //, '1 23 456 78 9');
+// console.log(array("")); //, null);
+// console.log(array("1")); //, null);
+// console.log(array("A1,B2")); //, null);
+// console.log(array("1,2,3")); //, '2');
+// console.log(array("1,2,3,4")); //, '2 3');
+// console.log(array("A1,B2,C3,D4,E5")); //, 'B2 C3 D4');
+// console.log(array("A,1,23,456,78,9,Z")); //, '1 23 456 78 9');
+
+// DESCRIPTION:
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+// Additionally, if the number is negative, return 0.
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+// Courtesy of projecteuler.net (Problem 1)
+
+// Test Cases:
+
+// describe("basic tests", function () {
+//   test(10, 23);
+// });
+
+function sumMultiples(limit) {
+  // find the numbers that are multiples of 3 or 5 below 'limit';
+  let multiples = [];
+  for (let i = 0; i < limit; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      multiples.push(i);
+    }
+  }
+
+  console.log(multiples);
+  // return the total
+  return multiples.reduce((sum, num) => sum + num, 0);
+}
+
+console.log(sumMultiples(10));
