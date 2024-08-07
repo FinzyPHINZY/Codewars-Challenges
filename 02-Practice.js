7dc99fc41557d3534d691cdb8d362853c5ad8a00
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-08-07 09:19:14 +0100

    added solution to kata

--- 02-Practice.js
+++ 02-Practice.js
@@ -222,11 +222,53 @@ var Ghost = function () {
 
 // describe("Ghost", () => {
 //   it("should be one of the specified colors", () => {
-let colors = ["white", "yellow", "purple", "red"];
+// let colors = ["white", "yellow", "purple", "red"];
 //     Test.expect(colors.includes(new Ghost().color));
 //   });
 // });
 
-const ghostColor = new Ghost().color;
+// const ghostColor = new Ghost().color;
 
-console.log(colors.includes(ghostColor));
+// console.log(colors.includes(ghostColor));
+
+// DESCRIPTION:
+// This is a spin off of my first kata.
+// You are given a string containing a sequence of character sequences separated by commas.
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+// Examples
+
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+
+function array(string) {
+  // TODO
+  string = string.split(",");
+  if (string.length < 3) return null;
+
+  const result = string
+    .filter((char, i) => i !== 0 && i !== string.length - 1)
+    .join(" ");
+
+  return result;
+}
+
+// describe('Sample Tests', function() {
+//   function test(string, expected) {
+//     it(`string='${string}'`, function() {
+//       assert.strictEqual(array(string), expected);
+//     });
+//   }
+
+console.log(array("")); //, null);
+console.log(array("1")); //, null);
+console.log(array("A1,B2")); //, null);
+console.log(array("1,2,3")); //, '2');
+console.log(array("1,2,3,4")); //, '2 3');
+console.log(array("A1,B2,C3,D4,E5")); //, 'B2 C3 D4');
+console.log(array("A,1,23,456,78,9,Z")); //, '1 23 456 78 9');
