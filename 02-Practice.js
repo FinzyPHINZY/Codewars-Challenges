3ee99ef6801abcb9815f1b64e84261d7f1f0ac4b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-09-05 10:20:51 +0100

    added new practice and solution

--- 02-Practice.js
+++ 02-Practice.js
@@ -1120,6 +1120,54 @@ function nb_year(p0, percent, aug, p) {
   return count
 }
 
-console.log(nb_year(1000, 2, 50, 1200), 3)
-console.log(nb_year(1500, 5, 100, 5000), 15)
-console.log(nb_year(1500000, 2.5, 10000, 2000000), 10)
+// console.log(nb_year(1000, 2, 50, 1200), 3)
+// console.log(nb_year(1500, 5, 100, 5000), 15)
+// console.log(nb_year(1500000, 2.5, 10000, 2000000), 10)
+
+// Your job is to write a function which increments a string, to create a new string.
+
+//     If the string already ends with a number, the number should be incremented by 1.
+//     If the string does not end with a number. the number 1 should be appended to the new string.
+
+// Examples:
+
+// foo -> foo1
+
+// foobar23 -> foobar24
+
+// foo0042 -> foo0043
+
+// foo9 -> foo10
+
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+function incrementString(str) {
+  const match = str.match(/(\d+)$/)
+
+  if (match) {
+    const numStr = match[0]
+    const prefix = str.slice(0, match.index)
+
+    const num = (parseInt(numStr, 10) + 1).toString()
+    const preceedingZeros = numStr.length - num.length
+
+    const newNumStr =
+      preceedingZeros > 0 ? '0'.repeat(preceedingZeros) + num : num
+
+    return prefix + newNumStr
+  } else {
+    return str + 1
+  }
+}
+
+console.log(incrementString('foobar000'), 'foobar001')
+console.log(incrementString('foobar999'), 'foobar1000')
+console.log(incrementString('foobar00999'), 'foobar01000')
+console.log(incrementString('foo'), 'foo1')
+console.log(incrementString('foobar001'), 'foobar002')
+console.log(incrementString('foobar1'), 'foobar2')
+console.log(incrementString('1'), '2')
+console.log(incrementString('009'), '010')
+console.log(incrementString('fo99obar99'), 'fo99obar100')
