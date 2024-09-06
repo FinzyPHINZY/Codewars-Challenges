b9ed1395bca01e0246831125c3db1720413ce5ec
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-09-06 11:19:07 +0100

    added new practice and solution

--- 02-Practice.js
+++ 02-Practice.js
@@ -1181,12 +1181,75 @@ const sortme = function (names) {
   return names.sort((a, b) => a.localeCompare(b))
 }
 
-console.log(sortme(['Hello', 'there', "I'm", 'fine']), [
-  'fine',
-  'Hello',
-  "I'm",
-  'there',
-])
-console.log(sortme(['C', 'd', 'a', 'B']), ['a', 'B', 'C', 'd'])
-console.log(sortme(['CodeWars']), ['CodeWars'])
-console.log(sortme([]), [])
+// console.log(sortme(['Hello', 'there', "I'm", 'fine']), [
+//   'fine',
+//   'Hello',
+//   "I'm",
+//   'there',
+// ])
+// console.log(sortme(['C', 'd', 'a', 'B']), ['a', 'B', 'C', 'd'])
+// console.log(sortme(['CodeWars']), ['CodeWars'])
+// console.log(sortme([]), [])
+
+// If we write out the digits of "60" as English words we get "sixzero"; the number of letters in "sixzero" is seven. The number of letters in "seven" is five. The number of letters in "five" is four. The number of letters in "four" is fou we have reached a stable equilibrium.
+
+// Note: for integers larger than 9, write out the names of each digit in a single word (instead of the proper name of the number in English). For example, write 12 as "onetwo" (instead of twelve), and 999 as "nineninenine" (instead of nine hundred and ninety-nine).
+
+// For any integer between 0 and 999, return an array showing the path from that integer to a stable equilibrium:
+// Examples
+
+// numbersOfLetters(60) --> ["sixzero", "seven", "five", "four"]
+// numbersOfLetters(1) --> ["one", "three", "five", "four"]
+
+function numbersOfLetters(integer) {
+  // code here...
+  const numberWords = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ]
+
+  let result = []
+
+  const numToWords = (num) =>
+    num
+      .toString()
+      .split('')
+      .map((val) => numberWords[val])
+      .join('')
+
+  let currentWord = numToWords(integer)
+
+  if (currentWord === 'four') {
+    result.push(currentWord)
+    return result
+  } else {
+    result.push(currentWord)
+
+    while (true) {
+      let nextWord = numToWords(currentWord.length)
+      result.push(nextWord)
+
+      if (nextWord === 'four') {
+        break
+      }
+      currentWord = nextWord
+    }
+    return result
+  }
+}
+
+console.log(numbersOfLetters(4))
+
+console.log(numbersOfLetters(1)) //["one", "three", "five", "four"]);
+console.log(numbersOfLetters(12)) // ["onetwo", "six", "three", "five", "four"]);
+console.log(numbersOfLetters(37)) // ["threeseven", "onezero", "seven", "five", "four"]);
+console.log(numbersOfLetters(311)) //, ["threeoneone", "oneone", "six", "three", "five", "four"]);
+console.log(numbersOfLetters(999)) //, ["nineninenine", "onetwo", "six", "three", "five", "four"]);
