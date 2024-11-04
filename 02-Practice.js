a30420f386d52d9e8845893c8c62e789914e001d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-04 05:16:41 +0100

    added new practice and solution

--- 02-Practice.js
+++ 02-Practice.js
@@ -2031,6 +2031,38 @@ let exampleTests = [
   ['The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog', 3635],
 ];
 
-exampleTests.forEach((input) => {
-  console.log(sumOfIntegersInString(input));
-});
+// exampleTests.forEach((input) => {
+//   console.log(sumOfIntegersInString(input));
+// });
+
+// DESCRIPTION:
+// This is a spin off of my first kata.
+// You are given a string containing a sequence of character sequences separated by commas.
+// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+// Examples
+
+// "1,2,3"      =>  "2"
+// "1,2,3,4"    =>  "2 3"
+// "1,2,3,4,5"  =>  "2 3 4"
+
+// ""     =>  NULL
+// "1"    =>  NULL
+// "1,2"  =>  NULL
+
+function array(string) {
+  // TODO
+  string = string.split(',');
+
+  if (string.length < 3) return null;
+
+  return string.slice(1, -1).join(' ');
+}
+
+console.log(array(''), null);
+console.log(array('1'), null);
+console.log(array('A1,B2'), null);
+console.log(array('1,2,3'), '2');
+console.log(array('1,2,3,4'), '2 3');
+console.log(array('A1,B2,C3,D4,E5'), 'B2 C3 D4');
+console.log(array('A,1,23,456,78,9,Z'), '1 23 456 78 9');
