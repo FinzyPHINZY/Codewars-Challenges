a2c0ede53d717634d99a11b70fdec6a9f0cb0051
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-04 05:47:51 +0100

    added new kata and solution

--- 02-Practice.js
+++ 02-Practice.js
@@ -2091,4 +2091,29 @@ class Ghostt {
 
 let newGhost = new Ghostt();
 
-console.log(newGhost);
+// console.log(newGhost);
+
+// DESCRIPTION:
+// Task
+// You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
+
+// Note: the scores will always be unique (so no duplicate values)
+
+// Examples
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+
+// takes in an object
+// returns an array of languages sorted by their value in descending order.
+
+function myLanguages(results) {
+  return Object.entries(results)
+    .sort((a, b) => b[1] - a[1])
+    .filter((lang) => lang[1] >= 60)
+    .map((lang) => lang[0]);
+}
+
+console.log(myLanguages({ Java: 10, Ruby: 80, Python: 65 }));
+console.log(myLanguages({ Hindi: 60, Greek: 71, Dutch: 93 }));
+console.log(myLanguages({ 'C++': 50, ASM: 10, Haskell: 20 }));
