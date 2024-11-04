4f817e4edd71663e9316d09dd5b241abc8cf050b
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-04 05:20:33 +0100

    added new practice and solution

--- 02-Practice.js
+++ 02-Practice.js
@@ -2059,10 +2059,36 @@ function array(string) {
   return string.slice(1, -1).join(' ');
 }
 
-console.log(array(''), null);
-console.log(array('1'), null);
-console.log(array('A1,B2'), null);
-console.log(array('1,2,3'), '2');
-console.log(array('1,2,3,4'), '2 3');
-console.log(array('A1,B2,C3,D4,E5'), 'B2 C3 D4');
-console.log(array('A,1,23,456,78,9,Z'), '1 23 456 78 9');
+// console.log(array(''), null);
+// console.log(array('1'), null);
+// console.log(array('A1,B2'), null);
+// console.log(array('1,2,3'), '2');
+// console.log(array('1,2,3,4'), '2 3');
+// console.log(array('A1,B2,C3,D4,E5'), 'B2 C3 D4');
+// console.log(array('A,1,23,456,78,9,Z'), '1 23 456 78 9');
+
+// Color Ghost
+
+// Create a class Ghost
+
+// Ghost objects are instantiated without any arguments.
+
+// Ghost objects are given a random color attribute of 'white' or 'yellow'
+
+// or 'purple' or 'red when instantiated.
+
+// ghost = new Ghost();
+
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+
+class Ghostt {
+  constructor() {
+    this.color = ['white', 'yellow', 'purple', 'red'][
+      Math.floor(Math.random() * 4)
+    ];
+  }
+}
+
+let newGhost = new Ghostt();
+
+console.log(newGhost);
