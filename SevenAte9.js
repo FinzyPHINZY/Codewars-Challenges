3b1458e7fcd1ce282988c6482fb83c5352bc2acc
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-10 11:19:24 +0100

    added solution to kata

--- SevenAte9.js
+++ SevenAte9.js
@@ -7,15 +7,37 @@
 
 function sevenAte9(str) {
   // code here
-  return str;
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char !== '9') {
+      result += char;
+    } else {
+      if (str[i - 1] === '7' && str[i + 1] === '7') {
+        continue;
+      } else {
+        result += char;
+      }
+    }
+  }
+  return result;
 }
 
 const tests = [
   ['797', '77'],
   ['7979797', '7777'],
   ['165561786121789797', '16556178612178977'],
+  ['9573486979797979797979797779797979797', '957348697777777777777777'],
 ];
 
 tests.forEach((test) => {
-  console.log(sevenAte9(test[0]), test[1]);
+  const result = sevenAte9(test[0]);
+  console.log('Result: ', result);
+  console.log(result === test[1]);
 });
+
+// takes in a string of numbers
+// returns the input number string excluding each digit that is = 9 and has a 7 before and after it
+// it has to pass 9. only exclude 9 that has 7 before and after
+// if char is not a 9 that has 7 before and after
