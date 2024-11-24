53c653d3fc86ce8ac5ee6b786bb410ab80a67de5
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-11-24 16:34:12 +0100

    added new practice

--- 02-Practice.js
+++ 02-Practice.js
@@ -2143,11 +2143,33 @@ function cleanString(s) {
   return output.join('');
 }
 
-console.log(cleanString('abc#d##c'), 'ac');
-console.log(cleanString('abc####d##c#'), '');
+// console.log(cleanString('abc#d##c'), 'ac');
+// console.log(cleanString('abc####d##c#'), '');
 
 // takes in a string of characters including '#'
 // returns the string after applying each '#' as a backspace
 // iterate through the string.
 // if the char isnt '#', add char to output
 // else, remove last char in output
+
+// If we write out the digits of "60" as English words we get "sixzero"; the number of letters in "sixzero" is seven. The number of letters in "seven" is five. The number of letters in "five" is four. The number of letters in "four" is fou we have reached a stable equilibrium.
+
+// Note: for integers larger than 9, write out the names of each digit in a single word (instead of the proper name of the number in English). For example, write 12 as "onetwo" (instead of twelve), and 999 as "nineninenine" (instead of nine hundred and ninety-nine).
+
+// For any integer between 0 and 999, return an array showing the path from that integer to a stable equilibrium:
+// Examples
+
+// numbersOfLetters(60) --> ["sixzero", "seven", "five", "four"]
+// numbersOfLetters(1) --> ["one", "three", "five", "four"]
+
+function numbersOfLetters(integer) {
+  // code here...
+}
+
+// console.log(numbersOfLetters(4));
+
+// console.log(numbersOfLetters(1)); //["one", "three", "five", "four"]);
+// console.log(numbersOfLetters(12)); // ["onetwo", "six", "three", "five", "four"]);
+// console.log(numbersOfLetters(37)); // ["threeseven", "onezero", "seven", "five", "four"]);
+// console.log(numbersOfLetters(311)); //, ["threeoneone", "oneone", "six", "three", "five", "four"]);
+// console.log(numbersOfLetters(999)); //, ["nineninenine", "onetwo", "six", "three", "five", "four"]);
