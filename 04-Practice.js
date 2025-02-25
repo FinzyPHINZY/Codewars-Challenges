f4dd360743206a3d8ef7ea505a1b31ddc68f2f51
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-02-25 09:38:37 +0100

    added new practice and solution

--- 04-Practice.js
+++ 04-Practice.js
@@ -132,4 +132,33 @@ function partlist(arr) {
   return result;
 }
 
-console.log(partlist(['az', 'toto', 'picaro', 'zone', 'kiwi']));
+// console.log(partlist(['az', 'toto', 'picaro', 'zone', 'kiwi']));
+
+// Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+// Note: only positive integers will be tested.
+
+function sumOfIntegersInString(s) {
+  const match = s.match(/\d+/g);
+
+  if (!match) return 0;
+  return match.map(Number).reduce((sum, num) => sum + num, 0); // what the function name says
+}
+
+let = exampleTests = [
+  ['12.4', 16],
+  ['h3ll0w0rld', 3],
+  ['2 + 3 = ', 5],
+  [
+    'Our company made approximately 1 million in gross revenue last quarter.',
+    1,
+  ],
+  ['The Great Depression lasted from 1929 to 1939.', 3868],
+  ['Dogs are our best friends.', 0],
+  ['C4t5 are 4m4z1ng.', 18],
+  ['The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog', 3635],
+];
+
+exampleTests.forEach((test) => {
+  console.log(sumOfIntegersInString(test[0]) === test[1]);
+});
