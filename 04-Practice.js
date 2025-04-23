e4e082650f7a985a91879c5aa256575e4ede1346
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-04-23 10:57:13 +0100

    added new practice and solution

--- 04-Practice.js
+++ 04-Practice.js
@@ -570,9 +570,36 @@ function shorterReverseLonger(a, b) {
   return `${shorter}${longer.split('').reverse().join('')}${shorter}`;
 }
 
-console.log(shorterReverseLonger('first', 'abcde'), 'abcdetsrifabcde');
-console.log(shorterReverseLonger('hello', 'bau'), 'bauollehbau');
-console.log(shorterReverseLonger('fghi', 'abcde'), 'fghiedcbafghi');
-console.log(shorterReverseLonger('hello', ''), 'olleh');
-console.log(shorterReverseLonger('', 'bau'), 'uab');
-console.log(shorterReverseLonger('', ''), '');
+// console.log(shorterReverseLonger('first', 'abcde'), 'abcdetsrifabcde');
+// console.log(shorterReverseLonger('hello', 'bau'), 'bauollehbau');
+// console.log(shorterReverseLonger('fghi', 'abcde'), 'fghiedcbafghi');
+// console.log(shorterReverseLonger('hello', ''), 'olleh');
+// console.log(shorterReverseLonger('', 'bau'), 'uab');
+// console.log(shorterReverseLonger('', ''), '');
+
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+function likeSystem(arr) {
+  if (arr.length === 0) return 'no one likes this';
+  if (arr.length === 1) return `${arr[0]} likes this`;
+  if (arr.length === 2) return `${arr[0]} and ${arr[1]} like this`;
+  if (arr.length === 3) return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+
+  return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+}
+
+console.log(likeSystem([]));
+console.log(likeSystem(['Peter']));
+console.log(likeSystem(['Jacob', 'Alex']));
+console.log(likeSystem(['Max', 'John', 'Mark']));
+console.log(likeSystem(['Alex', 'Jacob', 'Mark', 'Max']));
+console.log(likeSystem(['Jacob', 'Alex', 'Alex', 'Jacob', 'Mark', 'Max']));
