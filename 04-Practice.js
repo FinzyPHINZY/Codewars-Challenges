361b900beb93a548ab42947bb4f5bcfabd7e7fa0
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-04-23 10:22:45 +0100

    added new practice and solution

--- 04-Practice.js
+++ 04-Practice.js
@@ -404,5 +404,113 @@ function greatestSequenceDigits(number, query) {
   return Math.max(...arr);
 }
 
-console.log(greatestSequenceDigits(1234567890, 5));
-console.log(greatestSequenceDigits(283910, 2));
+// console.log(greatestSequenceDigits(1234567890, 5));
+// console.log(greatestSequenceDigits(283910, 2));
+
+// DESCRIPTION:
+// A balanced number is a number where the sum of digits to the left of the middle digit(s) and the sum of digits to the right of the middle digit(s) are equal.
+
+// If the number has an odd number of digits, then there is only one middle digit. (For example, 92645 has one middle digit, 6.) Otherwise, there are two middle digits. (For example, the middle digits of 1301 are 3 and 0.)
+
+// The middle digit(s) should not be considered when determining whether a number is balanced or not, e.g. 413023 is a balanced number because the left sum and right sum are both 5.
+
+// The task
+// Given a number, find if it is balanced, and return the string "Balanced" or "Not Balanced" accordingly. The passed number will always be positive.
+
+// Examples
+// 7 ==> return "Balanced"
+// Explanation:
+// middle digit(s): 7
+// sum of all digits to the left of the middle digit(s) -> 0
+// sum of all digits to the right of the middle digit(s) -> 0
+// 0 and 0 are equal, so it's balanced.
+// 295591 ==> return "Not Balanced"
+// Explanation:
+// middle digit(s): 55
+// sum of all digits to the left of the middle digit(s) -> 11
+// sum of all digits to the right of the middle digit(s) -> 10
+// 11 and 10 are not equal, so it's not balanced.
+// 959 ==> return "Balanced"
+// Explanation:
+// middle digit(s): 5
+// sum of all digits to the left of the middle digit(s) -> 9
+// sum of all digits to the right of the middle digit(s) -> 9
+// 9 and 9 are equal, so it's balanced.
+// 27102983 ==> return "Not Balanced"
+// Explanation:
+// middle digit(s): 02
+// sum of all digits to the left of the middle digit(s) -> 10
+// sum of all digits to the right of the middle digit(s) -> 20
+// 10 and 20 are not equal, so it's not balanced.
+
+// Test Cases:
+
+// describe("Check less than thousand", function () {
+//   it("Check balanced number", function () {
+//     Test.assertEquals(balancedNum(7), "Balanced");
+//     Test.assertEquals(balancedNum(959), "Balanced");
+//     Test.assertEquals(balancedNum(13), "Balanced");
+//     Test.assertEquals(balancedNum(432), "Not Balanced");
+//     Test.assertEquals(balancedNum(424), "Balanced");
+//   });
+//   it("Check Larger number", function () {
+//     Test.assertEquals(balancedNum(1024), "Not Balanced");
+//     Test.assertEquals(balancedNum(66545), "Not Balanced");
+//     Test.assertEquals(balancedNum(295591), "Not Balanced");
+//     Test.assertEquals(balancedNum(1230987), "Not Balanced");
+//     Test.assertEquals(balancedNum(56239814), "Balanced");
+//   });
+// });
+
+function balancedNum(num) {
+  // get middle number index
+
+  const numStr = num.toString();
+
+  const length = numStr.length;
+
+  if (length < 3) return 'Balanced';
+
+  // function to sum numbers in a string;
+  const sumNums = (n) => {
+    // console.log(n.split('').map(Number).reduce(()=>));
+    return n
+      .split('')
+      .map(Number)
+      .reduce((sum, num) => {
+        return sum + num;
+      }, 0);
+  };
+
+  // get sum of left and right numbers
+
+  if (length % 2 !== 0) {
+    const left = numStr.slice(0, length / 2);
+    const right = numStr.slice(-length / 2, length);
+
+    const leftSum = sumNums(left);
+    const rightSum = sumNums(right);
+
+    return leftSum === rightSum ? 'Balanced' : 'Not Balanced';
+  }
+
+  const left = numStr.slice(0, length / 2 - 1);
+  const right = numStr.slice(-(length / 2) + 1, length);
+
+  const leftSum = sumNums(left);
+  const rightSum = sumNums(right);
+
+  return leftSum === rightSum ? 'Balanced' : 'Not Balanced';
+}
+
+console.log(balancedNum(7));
+console.log(balancedNum(959));
+console.log(balancedNum(13));
+console.log(balancedNum(432));
+console.log(balancedNum(424));
+
+console.log(balancedNum(1024));
+console.log(balancedNum(66545));
+console.log(balancedNum(295591));
+console.log(balancedNum(1230987));
+console.log(balancedNum(56239814));
