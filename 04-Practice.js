a5a7cfddb6b931867b4eec67883c28aec2e73dca
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-04-28 04:53:30 +0100

    added new practice and solution

--- 04-Practice.js
+++ 04-Practice.js
@@ -621,6 +621,38 @@ const filter_list = (arr) => {
   return arr.filter((char) => typeof char === 'number');
 };
 
-console.log(filter_list([1, 2, 'a', 'b']));
-console.log(filter_list([1, 'a', 'b', 0, 15]));
-console.log(filter_list([1, 2, 'aasf', '1', '123', 123]));
+// console.log(filter_list([1, 2, 'a', 'b']));
+// console.log(filter_list([1, 'a', 'b', 0, 15]));
+// console.log(filter_list([1, 2, 'aasf', '1', '123', 123]));
+
+// DESCRIPTION:
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+// Example:
+
+// input:
+//  [[ 0, 1, 2, 3, 4 ],
+//   [ 10,11,12,13,14 ],
+//   [ 20,21,22,23,24 ],
+//   [ 30,31,32,33,34 ]]
+
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+
+// Array's length > 2.
+
+function toCsvText(array) {
+  // good luck
+  return array.join('\n');
+}
+
+console.log(
+  toCsvText([
+    [0, 1, 2, 3, 4],
+    [10, 11, 12, 13, 14],
+    [20, 21, 22, 23, 24],
+    [30, 31, 32, 33, 34],
+  ])
+);
