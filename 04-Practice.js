5896aeebbb7516bd8600ac07c83541708dd15df3
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-04-29 04:48:38 +0100

    added new practice and solution

--- 04-Practice.js
+++ 04-Practice.js
@@ -819,5 +819,55 @@ function unluckyDays(year) {
   return dayCount;
 }
 
-console.log(unluckyDays(2015));
-console.log(unluckyDays(1986));
+// console.log(unluckyDays(2015));
+// console.log(unluckyDays(1986));
+
+// Your job is to write a function which increments a string, to create a new string.
+
+//     If the string already ends with a number, the number should be incremented by 1.
+//     If the string does not end with a number. the number 1 should be appended to the new string.
+
+// Examples:
+
+// foo -> foo1
+
+// foobar23 -> foobar24
+
+// foo0042 -> foo0043
+
+// foo9 -> foo10
+
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+function incrementString(str) {
+  const match = str.match(/(\d+)$/);
+
+  if (!match) {
+    return `${str}1`;
+  }
+
+  const matchStr = str.slice(0, match.index);
+  const matchNum = str.slice(match.index);
+
+  const formattedNum = (Number(matchNum) + 1).toString();
+
+  const zeroCount = matchNum.length - formattedNum.length;
+
+  const preceedingZeros = '0'.repeat(zeroCount > 0 ? zeroCount : 0);
+
+  const result = matchStr + preceedingZeros + formattedNum;
+
+  return result;
+}
+
+console.log(incrementString('foobar000'), 'foobar001');
+console.log(incrementString('foobar999'), 'foobar1000');
+console.log(incrementString('foobar00999'), 'foobar01000');
+console.log(incrementString('foo'), 'foo1');
+console.log(incrementString('foobar001'), 'foobar002');
+console.log(incrementString('foobar1'), 'foobar2');
+console.log(incrementString('1'), '2');
+console.log(incrementString('009'), '010');
+console.log(incrementString('fo99obar99'), 'fo99obar100');
