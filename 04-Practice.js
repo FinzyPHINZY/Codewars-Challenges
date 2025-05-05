d12e8405642d417a106fc2805932c884d5a75947
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-05-05 04:23:48 +0100

    added new practice and solution

--- 04-Practice.js
+++ 04-Practice.js
@@ -909,35 +909,76 @@ function dup(s) {
   return result;
 }
 
-console.log(
-  dup(['ccooddddddewwwaaaaarrrrsssss', 'piccaninny', 'hubbubbubboo']),
-  ['codewars', 'picaniny', 'hubububo']
-);
-console.log(dup(['abracadabra', 'allottee', 'assessee']), [
-  'abracadabra',
-  'alote',
-  'asese',
-]);
-console.log(dup(['kelless', 'keenness']), ['keles', 'kenes']);
-console.log(dup(['Woolloomooloo', 'flooddoorroommoonlighters', 'chuchchi']), [
-  'Wolomolo',
-  'flodoromonlighters',
-  'chuchchi',
-]);
-console.log(dup(['adanac', 'soonness', 'toolless', 'ppellee']), [
-  'adanac',
-  'sones',
-  'toles',
-  'pele',
-]);
-console.log(dup(['callalloo', 'feelless', 'heelless']), [
-  'calalo',
-  'feles',
-  'heles',
-]);
-console.log(dup(['putteellinen', 'keenness']), ['putelinen', 'kenes']);
-console.log(dup(['kelless', 'voorraaddoosspullen', 'achcha']), [
-  'keles',
-  'voradospulen',
-  'achcha',
-]);
+// console.log(
+//   dup(['ccooddddddewwwaaaaarrrrsssss', 'piccaninny', 'hubbubbubboo']),
+//   ['codewars', 'picaniny', 'hubububo']
+// );
+// console.log(dup(['abracadabra', 'allottee', 'assessee']), [
+//   'abracadabra',
+//   'alote',
+//   'asese',
+// ]);
+// console.log(dup(['kelless', 'keenness']), ['keles', 'kenes']);
+// console.log(dup(['Woolloomooloo', 'flooddoorroommoonlighters', 'chuchchi']), [
+//   'Wolomolo',
+//   'flodoromonlighters',
+//   'chuchchi',
+// ]);
+// console.log(dup(['adanac', 'soonness', 'toolless', 'ppellee']), [
+//   'adanac',
+//   'sones',
+//   'toles',
+//   'pele',
+// ]);
+// console.log(dup(['callalloo', 'feelless', 'heelless']), [
+//   'calalo',
+//   'feles',
+//   'heles',
+// ]);
+// console.log(dup(['putteellinen', 'keenness']), ['putelinen', 'kenes']);
+// console.log(dup(['kelless', 'voorraaddoosspullen', 'achcha']), [
+//   'keles',
+//   'voradospulen',
+//   'achcha',
+// ]);
+
+// DESCRIPTION:
+// Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+//     Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+//     Each part will be in a string
+//     Elements of a pair must be in the same order as in the original array.
+
+// Examples of returns in different languages:
+
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]]
+// or
+//  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+// {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or
+// a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+
+function partlist2(arr) {
+  // your code
+  const result = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    const part1 = arr.slice(0, i + 1);
+    const part2 = arr.slice(i + 1);
+
+    const combined = [part1.join(' '), part2.join(' ')];
+    result.push(combined);
+  }
+
+  return result;
+}
+
+console.log(partlist2(['az', 'toto', 'picaro', 'zone', 'kiwi']));
