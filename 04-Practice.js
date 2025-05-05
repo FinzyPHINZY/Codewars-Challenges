c22e90815db55e38c2a7e77e1c2c5d7f1a087bf8
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-05-05 04:36:26 +0100

    added new practice and solution

--- 04-Practice.js
+++ 04-Practice.js
@@ -981,4 +981,77 @@ function partlist2(arr) {
   return result;
 }
 
-console.log(partlist2(['az', 'toto', 'picaro', 'zone', 'kiwi']));
+// console.log(partlist2(['az', 'toto', 'picaro', 'zone', 'kiwi']));
+
+// If we write out the digits of "60" as English words we get "sixzero"; the number of letters in "sixzero" is seven. The number of letters in "seven" is five. The number of letters in "five" is four. The number of letters in "four" is fou we have reached a stable equilibrium.
+
+// Note: for integers larger than 9, write out the names of each digit in a single word (instead of the proper name of the number in English). For example, write 12 as "onetwo" (instead of twelve), and 999 as "nineninenine" (instead of nine hundred and ninety-nine).
+
+// For any integer between 0 and 999, return an array showing the path from that integer to a stable equilibrium:
+// Examples
+
+// numbersOfLetters(60) --> ["sixzero", "seven", "five", "four"]
+// numbersOfLetters(1) --> ["one", "three", "five", "four"]
+
+function numbersOfLetters2(integer) {
+  // code here...
+
+  const map = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+  };
+
+  const result = [];
+
+  const numToString = (num) => {
+    return num
+      .toString()
+      .split('')
+      .map((x) => map[x])
+      .join('');
+  };
+
+  // check if current value is an equilibrium.
+  // if yes; return the result.
+
+  // else, push to the result array
+  // create a while loop. if the current value is an equilibrium, return the result.
+  // else, convert to newword, push to array and run again.
+
+  let currentWord = numToString(integer);
+
+  result.push(currentWord);
+
+  if (currentWord === 'four') {
+    return result;
+  }
+
+  while (true) {
+    const newWord = numToString(currentWord.length);
+    result.push(newWord);
+
+    if (newWord === 'four') {
+      break;
+    }
+
+    currentWord = newWord;
+  }
+
+  return result;
+}
+
+console.log(numbersOfLetters2(4));
+
+console.log(numbersOfLetters2(1)); //["one", "three", "five", "four"]);
+console.log(numbersOfLetters2(12)); // ["onetwo", "six", "three", "five", "four"]);
+console.log(numbersOfLetters2(37)); // ["threeseven", "onezero", "seven", "five", "four"]);
+console.log(numbersOfLetters2(311)); //, ["threeoneone", "oneone", "six", "three", "five", "four"]);
+console.log(numbersOfLetters2(999)); //, ["nineninenine", "onetwo", "six", "three", "five", "four"]);
