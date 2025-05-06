fce5c65dae60c0842cfb8bb505b409627c62d03f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-05-06 09:12:26 +0100

    added new practice and solution

--- 04-Practice.js
+++ 04-Practice.js
@@ -1103,6 +1103,43 @@ function meeting(x) {
   return 'None available!';
 }
 
-console.log(meeting(['X', 'O', 'X'])); //, 1);
-console.log(meeting(['O', 'X', 'X', 'X', 'X'])); //, 0);
-console.log(meeting(['X', 'X', 'X', 'X', 'X'])); //, 'None available!');
+// console.log(meeting(['X', 'O', 'X'])); //, 1);
+// console.log(meeting(['O', 'X', 'X', 'X', 'X'])); //, 0);
+// console.log(meeting(['X', 'X', 'X', 'X', 'X'])); //, 'None available!');
+
+// Sort an array by value and index
+// Your task is to sort an array of integer numbers by the product of the value and the index of the positions.
+
+// For sorting the index starts at 1, NOT at 0!
+// The sorting has to be ascending.
+// The array will never be null and will always contain numbers.
+
+// Example:
+
+// Input: 23, 2, 3, 4, 5
+// Product of value and index:
+// 23 => 23 * 1 = 23  -> Output-Pos 4
+//  2 =>  2 * 2 = 4   -> Output-Pos 1
+//  3 =>  3 * 3 = 9   -> Output-Pos 2
+//  4 =>  4 * 4 = 16  -> Output-Pos 3
+//  5 =>  5 * 5 = 25  -> Output-Pos 5
+
+// Output: 2, 3, 4, 23, 5
+
+// takes in an array of numbers
+// return the array sorted by the product of the value and the index of the positions
+// keep track of the value and product of the value and index
+
+function sortByValueAndIndex2(array) {
+  const map = array.map((val, index) => ({
+    value: val,
+    product: val * (index + 1),
+  }));
+
+  return map.sort((a, b) => a.product - b.product).map((val) => val.value);
+}
+
+console.log(sortByValueAndIndex2([1, 2, 3, 4, 5])); // [ 1, 2, 3, 4, 5 ];
+console.log(sortByValueAndIndex2([23, 2, 3, 4, 5])); // [ 2, 3, 4, 23, 5 ];
+console.log(sortByValueAndIndex2([26, 2, 3, 4, 5])); // [ 2, 3, 4, 5, 26 ];
+console.log(sortByValueAndIndex2([9, 5, 1, 4, 3])); // [ 1, 9, 5, 3, 4 ];
