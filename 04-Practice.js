89562bd435a9131b37b2402384160998f8f7433f
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-05-07 05:45:48 +0100

    added new practice and solution

--- 04-Practice.js
+++ 04-Practice.js
@@ -1169,5 +1169,36 @@ function cleanString2(s) {
   return result.join('');
 }
 
-console.log(cleanString2('abc#d##c'), 'ac');
-console.log(cleanString2('abc####d##c#'), '');
+// console.log(cleanString2('abc#d##c'), 'ac');
+// console.log(cleanString2('abc####d##c#'), '');
+
+// DESCRIPTION:
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Examples
+
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+  // ...
+
+  if (!words.length) {
+    return '';
+  }
+
+  const sortedWords = words
+    .split(' ')
+    .sort((a, b) => a.match(/\d/) - b.match(/\d/))
+    .join(' ');
+  return sortedWords;
+}
+
+console.log(order('is2 Thi1s T4est 3a'), 'Thi1s is2 3a T4est');
+console.log(
+  order('4of Fo1r pe6ople g3ood th5e the2'),
+  'Fo1r the2 g3ood 4of th5e pe6ople'
+);
+console.log(order(''), '', 'empty input should return empty string');
